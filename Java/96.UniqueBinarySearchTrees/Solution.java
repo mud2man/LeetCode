3577f78612fe13eed2dc8ce7a3578c95abd3b640
Namo.Lu <chih-hung.lu@columbia.edu>
2017-07-06 20:51:51 -0700

    LeetCode#96 refine

--- Java/96.UniqueBinarySearchTrees/Solution.java
+++ Java/96.UniqueBinarySearchTrees/Solution.java
@@ -15,20 +15,14 @@ import java.util.*;
 
 public class Solution {
     public int numTrees(int n) {
-        int[] dp;
+        int[] dp = new int[n + 1];
         
-        if(n == 0 || n == 1){
-            return 1;
-        }
-        
-        dp = new int[n + 1];
         dp[0] = 1;
         dp[1] = 1;
         
         for(int i = 2; i <= n; ++i){
-            dp[i] = 0;
-            for(int j = 0; j < i; ++j){
-                dp[i] = dp[i] + dp[j] * dp[i - j - 1];
+            for(int j = i - 1; j >= 0; --j){
+                dp[i] += dp[j] * dp[i - j - 1];
             }
         }
         return dp[n];
