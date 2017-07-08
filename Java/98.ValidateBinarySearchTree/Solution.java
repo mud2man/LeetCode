91f85fad173a1bc9222dcf1cacba6acf10e4a7d5
Namo.Lu <chih-hung.lu@columbia.edu>
2017-07-07 19:59:12 -0700

    LeetCode#98 refine

--- Java/98.ValidateBinarySearchTree/Solution.java
+++ Java/98.ValidateBinarySearchTree/Solution.java
@@ -7,77 +7,75 @@ import java.util.*; // Stack
 /* Definition for binary tree */
 class TreeNode 
 {
-	int val;
-	TreeNode left;
-	TreeNode right;
-	TreeNode(int x) { val = x; }
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
 }
 
 public class Solution {
-    public boolean dfs(TreeNode root, long lb, long ub){
-        boolean isValid;
-        
+    public boolean dfs(TreeNode root, TreeNode lb, TreeNode hb){
         if(root == null){
             return true;
         }
         
-        isValid = true;
-        
-        if(root.left != null){
-            if(((long)root.left.val >= (long)root.val) || ((long)root.left.val <= lb)){
-                return false;
-            }
-            else{
-                isValid = dfs(root.left, lb, (long)root.val);
-            }
-        }
-        
-        if(isValid == false){
+        boolean isValid = true;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+          
+        if(left != null && left.val >= root.val){
             return false;
         }
         
-        if(root.right != null){
-            if(((long)root.right.val <= (long)root.val) || ((long)root.right.val >= ub)){
-                return false;
-            }
-            else{
-                isValid = dfs(root.right, (long)root.val, ub);
-            }
+        if(left != null && lb != null && left.val <= lb.val){
+            return false;
         }
-        return isValid;
+        
+        if(dfs(left, lb, root) == false){
+            return false;
+        }
+        
+        if(right != null && right.val <= root.val){
+            return false;
+        }
+
+        if(right != null && hb != null && right.val >= hb.val){
+            return false;
+        }
+        
+        return dfs(right, root, hb);  
     }
     
     public boolean isValidBST(TreeNode root) {
-        
-        return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        return dfs(root, null, null);
     }
-    
+  
     public static void main(String[] args)
     {
         TreeNode root;
-		Solution sol;
+        Solution sol;
         
-		/* Generate a input tree
-		 *     8
-		 *    / \
-		 *   3   10
-		 *  / \   \
-		 * 1   6   14
-		 *    / \  /
-		 *   4   7 13 
-		 */
-		root = new TreeNode(8);
-		root.left = new TreeNode(3);
-		root.right = new TreeNode(10);
-		root.left.left = new TreeNode(1);
-		root.left.right = new TreeNode(6);
-		root.right.right = new TreeNode(14);
-		root.left.right.left = new TreeNode(4);
-		root.left.right.right = new TreeNode(7);
-		root.right.right.left = new TreeNode(13);
+        /* Generate a input tree
+         *     8
+         *    / \
+         *   3   10
+         *  / \   \
+         * 1   6   14
+         *    / \  /
+         *   4   7 13 
+         */
+        root = new TreeNode(8);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(10);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(6);
+        root.right.right = new TreeNode(14);
+        root.left.right.left = new TreeNode(4);
+        root.left.right.right = new TreeNode(7);
+        root.right.right.left = new TreeNode(13);
 
-		sol = new Solution();
+        sol = new Solution();
         
         System.out.println("isValid: " + sol.isValidBST(root));
-	}
+    }
 }
