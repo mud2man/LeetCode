70bb3c6a94ebddd360e2d769f13b509b0b166a37
Namo.Lu <chih-hung.lu@columbia.edu>
2017-08-20 00:06:56 +0800

    LeetCode#240 refine

--- Java/240.Search2DMatrixII/Solution.java
+++ Java/240.Search2DMatrixII/Solution.java
@@ -1,43 +1,34 @@
-/* Use Dynamic Programing: O(n*m), m = primes.length
- * 1. Declare the pointer in the ugly number list, every pointer refer to the associated prime
- * 2. u(n) = min(u(ptr0)*p0, u(ptr1)*p1, ...)
+/* Search: O(n + m)
+ * 1. Startting from the first column, traverse from the last element until matrix[y][x] < target
+ * 2. Repeat step.1 until target found, or run out of all columns
  */
 
 import java.util.*;
 
 public class Solution{
     public boolean searchMatrix(int[][] matrix, int target) {
-        int colNum;
-        int rowNum;
-        int y;
-        int x;
-        
-        rowNum = matrix.length;
-        if(rowNum == 0){
+        if(matrix.length == 0){
             return false;
         }
-        colNum = matrix[0].length;
         
-        y = rowNum - 1;
-        x = 0;
-        while((y >= 0) && (x < colNum)){
-            if(matrix[y][x] == target){
-                return true;
-            }
-            else if(matrix[y][x] < target){
-                ++x;
-            }
-            else{
-                --y;
+        int y = matrix.length - 1;
+        for(int x = 0; x < matrix[0].length; ++x){
+            for(y = y; y >= 0; --y){
+                if(matrix[y][x] == target){
+                    return true;
+                }
+                else if(matrix[y][x] < target){
+                    break;
+                }
             }
         }
         return false;
     }
     
     public static void main(String[] args){
-		int i;
-		int j;
-		Solution sol;
+        int i;
+        int j;
+        Solution sol;
         int target;
         int[][] matrix = {
                           {1,   4,  7, 11, 15},
@@ -46,19 +37,16 @@ public class Solution{
                           {10, 13, 14, 17, 24},
                           {18, 21, 23, 26, 30}
                           };
-       	target = 17;
-		sol = new Solution();
+           target = 17;
+        sol = new Solution();
  
         System.out.println("matrix[][]:");
-		for(i = 0; i < matrix.length; ++i){
-			for(j = 0; j < matrix[i].length; ++j){
-        		System.out.print(matrix[i][j] + ", ");
-			}
-        	System.out.println("");
-		}
+        for(i = 0; i < matrix.length; ++i){
+            System.out.println(Arrays.toString(matrix[i]));
+        }
 
         System.out.println("target: " + target);
         System.out.println("isFound:" + sol.searchMatrix(matrix, target));
 
-	}
+    }
 }
