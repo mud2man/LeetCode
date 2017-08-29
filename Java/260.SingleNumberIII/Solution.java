968c819c17572da385e1e0532278519427d8c387
Namo.Lu <chih-hung.lu@columbia.edu>
2017-08-29 09:58:46 +0900

    LeetCode#260 refine

--- Java/260.SingleNumberIII/Solution.java
+++ Java/260.SingleNumberIII/Solution.java
@@ -1,6 +1,6 @@
 /* Xor bit manupulation: O(n)
  * 1. Xor all the numbers
- * 2. Use diff ^ -diff to get the last set bit
+ * 2. Get the last set bit selector
  * 3. Use this last set bit to divide numbers into two groups, one has the single number a, and the other one has single number b
  * 4. Xor all the number based on the last set bit, because the pair number will appear in the same group
  * 5. Therefore, pairs in each group will be zeros after xor
@@ -11,25 +11,31 @@ import java.util.*; // Stack
 
 public class Solution {
     public int[] singleNumber(int[] nums) {
-        int[] singles;
-        int diff;
-        
-        singles = new int[2];
-        diff = 0;
+        int xorResult = 0;
         for(int num: nums){
-            diff ^= num;
+            xorResult = xorResult ^ num;
         }
         
         // get the last set bit
-        diff = diff & -diff;
+        int selector = 1;
+        for(int offset = 0; offset < 32; offset++){
+            if((selector & xorResult) != 0){
+                break;
+            }
+            selector = selector << 1;
+        }
         
         //dispatch to two group, one has the last set bit, and the other has not 
+        int[] singles = new int[2];
         for(int num: nums){
-            if((diff & num) == 0)
-                singles[0] ^= num;
-            else
-                singles[1] ^= num;
+            if((num & selector) != 0){
+                singles[0] = singles[0] ^ num;
+            }
+            else{
+                singles[1] = singles[1] ^ num;
+            }
         }
+
         return singles;
     }
 
