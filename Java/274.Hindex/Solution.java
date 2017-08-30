8f653764163ba12a615194d1ea6308ea88dbfdc8
Namo.Lu <chih-hung.lu@columbia.edu>
2017-08-30 10:09:51 -0400

    LeetCode#274 refine

--- Java/274.Hindex/Solution.java
+++ Java/274.Hindex/Solution.java
@@ -1,54 +1,45 @@
-/* Sorting first and Check: O(nlgn)
- * 1. Sort and check from the largest 
+/* Bucket sort: O(n)
+ * 1. buckets[i] = number of paper with citation count = i, buckets[size] = number of paper with citation count >= size
+ * 2. Check h from h = size until count >= h 
  */
 
 import java.util.*; // Stack
 
 public class Solution{
+    public int hIndex(int[] citations) {
+        int size = citations.length;
+        int[] buckets = new int[size + 1];
+        
+        for(int citation: citations){
+            if(citation >= size){
+                buckets[size]++;
+            }
+            else{
+                buckets[citation]++;
+            }
+        }
+        
+        int h = size;
+        int count = 0;
+        while(h >= 0){
+            count += buckets[h];
+            if(count >= h){
+                break;
+            }
+            --h;
+        }
+        return h;
+    }
 
-    Solution(){
-	}
-
-	public int hIndex(int[] citations){
-		int idx;
-		int arraySize;
-		int hValue;
-		int maxhValue;
-		int hCount;
-		
-		maxhValue = 0;
-
-		Arrays.sort(citations);
-		arraySize = citations.length;
-
-		for(idx = arraySize - 1; idx >= 0; idx--){
-			
-			hValue = arraySize - idx;
-
-			if(hValue <= citations[idx]){
-				if(idx > 0){
-					if(citations[idx - 1] <= hValue){
-						maxhValue =  hValue;
-					}
-				}
-				else{
-					maxhValue =  hValue;
-				}
-			}
-		}
-
-		return maxhValue;
-	}
-
-	public static void main(String[] args){
-		
-		Solution sol;
-		int hIndex;
-		int[] citations = new int[] {1, 1};
-		
-		sol = new Solution();
-		hIndex = sol.hIndex(citations);
-		
-		System.out.println("hIndex: " +  hIndex);
-	}
+    public static void main(String[] args){
+        
+        Solution sol;
+        int hIndex;
+        int[] citations = new int[] {1, 1};
+        
+        sol = new Solution();
+        hIndex = sol.hIndex(citations);
+        
+        System.out.println("hIndex: " +  hIndex);
+    }
 }
