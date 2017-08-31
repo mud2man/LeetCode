14f7d0ab3bf1ac7a2ddd3dad7e924c5be08b0b8a
Namo.Lu <chih-hung.lu@columbia.edu>
2017-08-31 09:13:34 -0400

    LeetCode#275 using Java

---
+++ Java/275.HindexII/Solution.java
@@ -0,0 +1,43 @@
+/* Binary search: O(logn)
+ * 1. If citations[mid] == (size - mid), set lb = mid, hb = lb - 1, and break
+ * 2. If citations[mid] < (size - mid), set lb = (mid + 1)
+ * 3. Otherwise, set hb = mid - 1;
+ */
+
+import java.util.*; // Stack
+
+public class Solution{
+    public int hIndex(int[] citations) {
+        int size = citations.length;
+        int lb = 0;
+        int hb = size - 1;
+        
+        while(lb <= hb){
+            int mid = (lb + hb) / 2;
+            
+            if(citations[mid] == (size - mid)){
+                lb = mid;
+                hb = lb - 1;
+            }
+            else if(citations[mid] < (size - mid)){
+                lb = mid + 1;
+            }
+            else {
+                hb = mid - 1;
+            }
+        }
+        return size - lb;
+    }
+
+    public static void main(String[] args){
+        
+        Solution sol;
+        int hIndex;
+        int[] citations = new int[] {0, 1, 3, 6, 5};
+        
+        sol = new Solution();
+        hIndex = sol.hIndex(citations);
+        
+        System.out.println("hIndex: " +  hIndex);
+    }
+}
