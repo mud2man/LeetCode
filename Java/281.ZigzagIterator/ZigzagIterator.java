0e8f765e7343dd9225e4d8dedee6f2fd89b9cdca
Namo.Lu <chih-hung.lu@columbia.edu>
2017-08-31 21:51:54 -0400

    LeetCode#281 refine

--- Java/281.ZigzagIterator/ZigzagIterator.java
+++ Java/281.ZigzagIterator/ZigzagIterator.java
@@ -1,75 +1,71 @@
 /* O(n)
- * 1. Use ptr1 and ptr2 to track the current positions of v1 and v2
+ * 1. Use a list of iterators to record the current index of all lists
+ * 2. Delete the iterator if the index reach to the end
  */
 
 import java.util.*; // Stack
 
 public class ZigzagIterator {
-    private int ptr1;
-    private int ptr2;
-    private int owner; //v1: 1, v2: 2
-    List<Integer> v1;
-    List<Integer> v2;
-
+    private class ListIterator{
+        List<Integer> list;
+        int length;
+        int currIdx;
+        ListIterator(List<Integer> l, int len, int c){list = l; length = len; currIdx = c;}
+    }
+    private List<ListIterator> listIterators;
+    private Iterator<ListIterator> listIterator;
+    
     public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
-        this.v1 = v1;
-        this.v2 = v2;
-        this.ptr1 = -1;
-        this.ptr2 = -1;
-        this.owner = 1;
+        listIterators = new ArrayList<ListIterator>();
+        if(!v1.isEmpty()){
+            listIterators.add(new ListIterator(v1, v1.size(), 0));
+        }
+        
+        if(!v2.isEmpty()){
+            listIterators.add(new ListIterator(v2, v2.size(), 0));
+        }
+        
+        listIterator = listIterators.iterator();
     }
 
     public int next() {
-        
-        while(true){
-            if(owner == 1){
-                owner = 2;
-                if((ptr1 + 1) < v1.size()){
-                    return v1.get(++ptr1);
-                }
-            }
-        
-            if(owner == 2){
-                owner = 1; 
-                if((ptr2 + 1) < v2.size()){
-                    return v2.get(++ptr2);
-                }
-            }
+        if(!listIterator.hasNext()){
+            listIterator = listIterators.iterator();
         }
+        
+        ListIterator itr = listIterator.next();
+        int nextVal = itr.list.get(itr.currIdx++);
+        if(itr.currIdx == itr.length){
+            listIterator.remove();
+        }
+        return nextVal;
     }
 
     public boolean hasNext() {
-        if((ptr1 + ptr2 + 2) < (v1.size() + v2.size())){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return !(listIterators.isEmpty());
     }
 
     public static void main(String[] args){
-		ZigzagIterator sol;
-		List<Integer> v1;
-		List<Integer> v2;
+        ZigzagIterator sol;
+        List<Integer> v1;
+        List<Integer> v2;
 
-		v1 = new ArrayList<Integer>();
-		v2 = new ArrayList<Integer>();
-		ZigzagIterator i = new ZigzagIterator(v1, v2);
-			
-		v1.add(1);
-		v1.add(2);
-		v2.add(3);
-		v2.add(4);
-		v2.add(5);
-		v2.add(6);
-		
-		System.out.println("v1: " + v1);
-		System.out.println("v2: " + v2);
+        v1 = new ArrayList<Integer>();
+        v2 = new ArrayList<Integer>();
+        v1.add(1);
+        v1.add(2);
+        v2.add(3);
+        v2.add(4);
+        v2.add(5);
+        v2.add(6);
+        System.out.println("v1: " + v1);
+        System.out.println("v2: " + v2);
 
-		System.out.println("iterate: ");
- 		while (i.hasNext()){
-			System.out.print(i.next() + ", ");
-		}
-		System.out.println("");
-	}
+        System.out.println("iterate: ");
+        ZigzagIterator i = new ZigzagIterator(v1, v2);
+         while (i.hasNext()){
+            System.out.print(i.next() + ", ");
+        }
+        System.out.println("");
+    }
 }
