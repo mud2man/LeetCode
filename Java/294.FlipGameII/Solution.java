1eadf4f4f483164dfcb7b96b21fd96ec752c9d28
Namo.Lu <chih-hung.lu@columbia.edu>
2017-09-02 00:07:27 -0400

    LeetCode#294 refine

--- Java/294.FlipGameII/Solution.java
+++ Java/294.FlipGameII/Solution.java
@@ -1,40 +1,67 @@
-/* Recursive: O(n!)
- * 1. If any case can cuase opponent lose in the next match, the I win 
- * 2. If opponent can win under all the cases in the next matches, then I lose 
+/* Devide and counquer with memory: O(n!)
+ * 1. If any case can make sure I win in the next match, the I win 
+ * 2. If I win under no matter how opponent move in the next matches, then I win
  */
 
 import java.util.*;
 
 public class Solution{
-    public boolean canWin(String s) {
-        int idx;
-        String str;
-        
-        idx = s.indexOf("++");
-        if(s == null || s.length() < 2){
-            return false;
+    private boolean helper(HashSet<String> mustWinSet, HashSet<String> mayLoseSet, String s){
+        if(mustWinSet.contains(s)){
+            return true;
         }
         
-        //System.out.println("s:" + s);
-        for(idx = 0; idx < s.length(); ++idx){
-            if(s.startsWith("++", idx)){
-                str = s.substring(0, idx) + "--" +  s.substring(idx + 2);
-                if(!canWin(str)){
-                    return true;
+        if(mayLoseSet.contains(s)){
+            return false;
+        }
+
+        boolean canWin = false;
+        for(int i = 0; i < (s.length() - 1); ++i){
+            // if any step can cause I winm then I win
+            if(s.startsWith("++", i)){
+                canWin = true;
+                String firstS = s.substring(0, i) + "--" + s.substring(i + 2);
+                // must win under all the situations moved by opponent, then I win
+                for(int j = 0; j < (firstS.length() - 1); ++j){
+                    if(firstS.startsWith("++", j)){
+                        String secondS = firstS.substring(0, j) + "--" + firstS.substring(j + 2);
+                        if(!helper(mustWinSet, mayLoseSet, secondS)){
+                            mayLoseSet.add(secondS);
+                            canWin = false;
+                            break;
+                        }
+                    }
+                }
+                if(canWin == true){
+                    break;
                 }
             }
         }
-        return false;
+        
+        if(canWin){
+            mustWinSet.add(s);
+            return true;
+        }
+        else{
+            mayLoseSet.add(s);
+            return false;
+        }
+    }
+    
+    public boolean canWin(String s) {
+        HashSet<String> mustWinSet = new HashSet<String>();
+        HashSet<String> mayLoseSet = new HashSet<String>();
+        return helper(mustWinSet, mayLoseSet, s);
     }
 
-	public static void main(String[] args){
-		Solution sol;
-		String s;
+    public static void main(String[] args){
+        Solution sol;
+        String s;
 
-		s = "++++";
-		sol = new Solution();
+        s = "++++";
+        sol = new Solution();
 
-		System.out.println("s: " + s);
-		System.out.println("canWin: " + sol.canWin(s));
-	}
+        System.out.println("s: " + s);
+        System.out.println("canWin: " + sol.canWin(s));
+    }
 }
