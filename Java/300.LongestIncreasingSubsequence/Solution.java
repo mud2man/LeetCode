84ec3e004349cca8845803cfa3db6e1b745a7d05
Namo.Lu <chih-hung.lu@columbia.edu>
2017-09-02 20:27:41 -0400

    LeetCode#300 using my own way

--- Java/300.LongestIncreasingSubsequence/Solution.java
+++ Java/300.LongestIncreasingSubsequence/Solution.java
@@ -1,13 +1,18 @@
-/* Dynamic programing + binary search: O(nlogn)
- * 1. Creaet dp[] to store the smallest tail of increasing sequence with length i+1 on dp[i+1]
+/* Binary search: O(nlogn)
+ * 1. Have anincreasing sequence increaseList
  * 2. Iterate nums and binary search the index for nums[i], 
- * 3. Update dp[i]
+ * 3. If the index is equal to the size of increaseList, increase maxLen
+ * 4. Otherwise, replace nums[index] with num
  *
  * ex: [10, 9, 2, 5, 3, 7, 101, 18]
- *    dp[0]: 10->9->2 (10) (9) (2) (2)    (2)    (2)       (2)            (2)
- *    dp[1]: 5->3     ()   ()  ()  (2->5) (2->3) (2->3)    (2->3)         (2->3)
- *    dp[2]: 7        ()   ()  ()  ()     ()     (2->3->7) (2->3->7)      (2->3->7)
- *    dp[3]: 101->18  ()   ()  ()  ()     ()     ()        (2->3->7->101) (2->3->7->18)
+ * time[0]: increaseList: 10
+ * time[1]: increaseList: 9
+ * time[2]: increaseList: 2
+ * time[3]: increaseList: 2->5
+ * time[4]: increaseList: 2->3
+ * time[5]: increaseList: 2->3->7
+ * time[6]: increaseList: 2->3->7->101
+ * time[7]: increaseList: 2->3->7->18
  *
  */
 
@@ -15,47 +20,40 @@ import java.util.*;
 
 public class Solution{
     public int lengthOfLIS(int[] nums) {
-        int[] dp;
-        int size;
-        int i;
-        int len;
-        int pos;
-        
-        size = nums.length;
-        dp = new int[size];
-        
-        if(size == 0){
-            return 0;
-        }
-        
-        dp[0] = nums[0];
-        len = 1;
-        for(i = 1; i < size; ++i){
-            pos = Arrays.binarySearch(dp, 0, len, nums[i]);
-            if(pos < 0){
-                pos = 0 - (pos + 1);
-                dp[pos] = nums[i];
-                if(pos == len){
-                    len++;
+        LinkedList<Integer> increaseList = new LinkedList<Integer>();
+        int maxLen = 0;
+
+        for(int num: nums){
+            int idx = Collections.binarySearch(increaseList, num);
+            if(idx < 0){
+                idx = -idx - 1;
+                int size = increaseList.size();
+                if(idx == size){
+                    increaseList.add(num);
+                    maxLen = size + 1;
+                }
+                else{
+                    increaseList.add(idx, num);
+                    increaseList.remove(idx + 1);
                 }
             }
         }
-        
-        return len;
+
+        return maxLen;
     }
 
-	public static void main(String[] args){
-		int i;
-		Solution sol;
-		int len;
-		int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
-		
-		sol = new Solution();
+    public static void main(String[] args){
+        int i;
+        Solution sol;
+        int len;
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        
+        sol = new Solution();
 
-		System.out.println("nums: " + Arrays.toString(nums));
+        System.out.println("nums: " + Arrays.toString(nums));
 
-		len = sol.lengthOfLIS(nums);
-		
-		System.out.println("maximum increasing length: " + len);
-	}
+        len = sol.lengthOfLIS(nums);
+        
+        System.out.println("maximum increasing length: " + len);
+    }
 }
