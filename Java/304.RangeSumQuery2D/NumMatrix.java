4f2ec8f48b0d09841e7bf8999c2d2a32b401ae45
Namo.Lu <chih-hung.lu@columbia.edu>
2017-09-03 09:23:17 -0400

    LeetCode#304 refine

--- Java/304.RangeSumQuery2D/NumMatrix.java
+++ Java/304.RangeSumQuery2D/NumMatrix.java
@@ -1,103 +1,77 @@
-/* Use Dynamic Programing: O(n^2)
+/* Dynamic Programing: O(n*m)
  * 1. Create a 2D array to store partual sum
- * 2. sum(i, j) = sum(i, j-1) + sum(i-1, j) + x(i, j) - sum(i-1, j-1)
- * 3. regionSum(row1, col1, row2, col2) = sum(row2, col2) - sum(row2, col1 - 1 )
-      - sum(row1, col2 - 1) + sum(row1 - 1, col1 - 1)
+ * 2. sumMatrix(i, j) = sumMatrix(i, j-1) + sumMatrix(i-1, j) + x(i, j) - sumMatrix(i-1, j-1)
+ * 3. regionSum(row1, col1, row2, col2) = sumMatrix(row2, col2) - sumMatrix(row2, col1 - 1 ) - 
+                                          sumMatrix(row1, col2 - 1) + sumMatrix(row1 - 1, col1 - 1)
  */
 
 import java.util.*;
 
 public class NumMatrix{
-
-	/* partual sum */
-	List<List<Integer> > sums;
-	
+    private int[][] sumMatrix;
+    
     public NumMatrix(int[][] matrix) {
-		int colSize;
-		int rowSize;
-		int x;
-		int y;
-		int upSum;
-		int leftSum;
-		int upLeftSum;
-		int sum;
-		List<Integer> row;
+        if(matrix.length == 0){
+            return;
+        }
+        
+        sumMatrix = new int[matrix.length][matrix[0].length];
+        
+        for(int y = 0; y < sumMatrix.length; ++y){
+            for(int x = 0; x < sumMatrix[0].length; ++x){
+                int sum = 0;
+                if(y > 0){
+                    sum = sum + sumMatrix[y - 1][x];
+                }
+                
+                if(x > 0){
+                    sum = sum + sumMatrix[y][x - 1];
+                }
+                
+                if(y > 0 && x > 0){
+                    sum  = sum - sumMatrix[y - 1][x - 1];
+                }
+                sumMatrix[y][x] = sum + matrix[y][x];
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = sumMatrix[row2][col2];
+        
+        if(row1 > 0){
+            sum = sum - sumMatrix[row1 - 1][col2];
+        }
+        
+        if(col1 > 0){
+            sum = sum - sumMatrix[row2][col1 - 1];
+        }
+        
+        if(row1 > 0 && col1 > 0){
+            sum = sum + sumMatrix[row1 - 1][col1 - 1];
+        }
+        return sum;
+    } 
 
-		sums = new ArrayList<List<Integer>>();
-		rowSize = matrix.length;
-		
-		if(rowSize != 0){
-			colSize = matrix[0].length;
-		}
-		else{
-			return;
-		}
+    public static void main(String[] args){
+        NumMatrix sol;
+        int[][] matrix = {{3, 0, 1, 4, 2},
+                          {5, 6, 3, 2, 1},
+                          {1, 2, 0, 1, 5},
+                          {4, 1, 0, 1, 7},
+                          {1, 0, 3, 0, 5}};
+    
+        System.out.println("matrix:");
+        for(int[] row: matrix){
+            System.out.println(Arrays.toString(row));
+        }
+        System.out.println("");
 
-		/* Acquire sums in 0th row*/
-		row = new ArrayList<Integer>();
-		row.add(matrix[0][0]);
-		for(x = 1; x < colSize; x++){
-			leftSum = row.get(x - 1);
-			sum = leftSum + matrix[0][x];
-			row.add(sum);
-		}
-		sums.add(row);
+        sol = new NumMatrix(matrix);
+        System.out.println("(2, 1, 4, 3) = " + sol.sumRegion(2, 1, 4, 3));
+        System.out.println("(1, 1, 2, 2) = " + sol.sumRegion(1, 1, 2, 2));
+        System.out.println("(1, 2, 2, 4) = " + sol.sumRegion(1, 2, 2, 4));
+        System.out.println("(1, 2, 1, 2) = " + sol.sumRegion(1, 2, 1, 2));
 
-		/* Acquire sums in 0th colum*/
-		for(y = 1; y < rowSize; y++){
-			row = new ArrayList<Integer>();
-			upSum = sums.get(y - 1).get(0);
-			sum = upSum + matrix[y][0];
-			row.add(sum);
-			sums.add(row);
-		}
-
-		/* Acquire sums in the rest column and row*/
-		for(y = 1; y < rowSize; y++){
-			for(x = 1; x < colSize; x++){
-				upSum = sums.get(y - 1).get(x);
-				leftSum = sums.get(y).get(x - 1);
-				upLeftSum = sums.get(y - 1).get(x - 1);
-				sum = upSum + leftSum + matrix[y][x] - upLeftSum;
-				sums.get(y).add(sum);
-			}
-		}
-	}
-
-	public int sumRegion(int row1, int col1, int row2, int col2) {
-		int upSum;
-		int leftSum;
-		int upLeftSum;
-	
-		if(sums.size() == 0){
-			return 0;
-		}
-
-		upSum = (row1 > 0)? sums.get(row1 - 1).get(col2): 0 ;
-		leftSum =(col1 > 0)? sums.get(row2).get(col1 - 1): 0;
-		upLeftSum = ((col1 > 0) && (row1 > 0))? sums.get(row1 - 1).get(col1 - 1): 0;
-
-		return sums.get(row2).get(col2) - upSum - leftSum + upLeftSum;
-	}
-
-	public void dump(){
-		System.out.println(sums);
-	}
-
-	public static void main(String[] args){
-		NumMatrix sol;
-		int[][] matrix = {{3, 0, 1, 4, 2},
-						  {5, 6, 3, 2, 1},
-						  {1, 2, 0, 1, 5},
-						  {4, 1, 0, 1, 7},
-						  {1, 0, 3, 0, 5}};
-		
-		sol = new NumMatrix(matrix);
-
-		System.out.println("(2, 1, 4, 3) = " + sol.sumRegion(2, 1, 4, 3));
-		System.out.println("(1, 1, 2, 2) = " + sol.sumRegion(1, 1, 2, 2));
-		System.out.println("(1, 2, 2, 4) = " + sol.sumRegion(1, 2, 2, 4));
-		System.out.println("(1, 2, 1, 2) = " + sol.sumRegion(1, 2, 1, 2));
-
-	}
+    }
 }
