354c71c74c1a4eca649999c19566d590c267cdc9
Namo.Lu <chih-hung.lu@columbia.edu>
2017-09-03 21:05:04 -0400

    LeetCode#313 refine

--- Java/313.SuperUglyNumber/Solution.java
+++ Java/313.SuperUglyNumber/Solution.java
@@ -1,80 +1,47 @@
-/* Use Dynamic Programing: O(n*m), m = primes.length
+/* Dynamic Programing: O(n*m)
  * 1. Declare the pointer in the ugly number list, every pointer refer to the associated prime
- * 2. u(n) = min(u(ptr0)*p0, u(ptr1)*p1, ...)
+ * 2. dp(i) = min(dp(ptr0)*p0, dp(ptr1)*p1, ...)
  */
 
 import java.util.*;
 
 public class Solution{
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        int[] dp = new int[n];
+        int[] pointers = new int[primes.length];
+        
+        dp[0] = 1;
+        for(int i = 1; i < n; ++i){
+            int min = Integer.MAX_VALUE;
+            for(int j = 0; j < primes.length; ++j){
+                min = Math.min(min, primes[j] * dp[pointers[j]]);
+            }
+            
+            dp[i] = min;
+            
+            for(int j = 0; j < primes.length; ++j){
+                if(primes[j] * dp[pointers[j]] == min){
+                    pointers[j]++;
+                }
+            }
+        }
+        return dp[n - 1];
+    }
 
-	/* The set of ugly numbers and positions */
-	private List< Integer > uglys;
-	private List< Integer > ptrs;
-	
-    Solution(){
-		uglys = new ArrayList< Integer >();
-		ptrs = new ArrayList< Integer >();
-	}
-	
-	public int nthSuperUglyNumber(int n, int[] primes) {
-		int size;
-		int i;
-		int j;
-		int min;
-		int ptr;
+    public static void main(String[] args){
+        int i;
+        Solution sol;
+        int[] primes = {2, 7, 13, 19};
+        
+        System.out.println("Ugly numbers: ");
 
-		size = primes.length;
+        for(i = 1; i < 20; i++){
+            sol = new Solution();
+            System.out.print(sol.nthSuperUglyNumber(i, primes) +", ");
 
-		for(i = 0; i < size; i++){
-			ptrs.add(0);
-		}
-		uglys.add(1);
-
-		for(i = 1; i < n; i++){
-
-			/* Find the next ugly number */
-			min = primes[0] * uglys.get(ptrs.get(0));
-
-			for(j = 1; j < size; j++){
-				min = Math.min(min, primes[j] * uglys.get(ptrs.get(j)));
-			}
-
-			uglys.add(min);
-
-			/* Update all positions associated with all primes */
-			for(j = 0; j < size; j++){
-				if((uglys.get(ptrs.get(j)) * primes[j]) == min){
-					ptr = ptrs.get(j);
-					ptrs.set(j, ++ptr);
-				}
-			}
-		}
-
-		return uglys.get(n - 1);
-	}
-
-	public void dump() {
-		System.out.println("uglys: ");
-		System.out.println(uglys);
-		System.out.println("ptrs: ");
-		System.out.println(ptrs);
-	}
-
-	public static void main(String[] args){
-		int i;
-		Solution sol;
-		int[] primes = {2, 7, 13, 19};
-		
-
-		System.out.println("Ugly numbers: ");
-
-		for(i = 1; i < 20; i++){
-			sol = new Solution();
-			System.out.print(sol.nthSuperUglyNumber(i, primes) +", ");
-
-			if(i%5 == 4){
-				System.out.println();
-			}
-		}
-	}
+            if(i%5 == 4){
+                System.out.println();
+            }
+        }
+    }
 }
