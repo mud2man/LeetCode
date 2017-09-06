bd021a225c6cd2213f617a323ae8a43e130c875d
Namo.Lu <chih-hung.lu@columbia.edu>
2017-09-06 09:01:27 -0400

    LeetCode#334 refine

--- Java/334.IncreasingTripletSubsequence/Solution.java
+++ Java/334.IncreasingTripletSubsequence/Solution.java
@@ -1,46 +1,35 @@
-/* O(n)
- * 1. list[0] = the first element of list with length 2
- * 2. list[1] = the second element of list with length 2
- * 3. min = the minimum value as long as we traverse
- * 4. Maintain the relationship among list[0], list[1], min
- * 5. If num > list[1], return true;
- * ex: preorder = [4, 2, 1, 5, 3, 6]
+/* DynamicO programming: (n)
+ * 1. increasingList[0] = the smallest last element with length = 1
+ * 2. increasingList[1] = the smallest last element with length = 2
+ * ex: nums[] = [4, 2, 1, 5, 3, 6, 7]
  * 
- * time[0]: list = [max, max], min = max
- * time[1]: list = [max, max], min = 2
- * time[1]: list = [max, max], min = 1
- * time[1]: list = [1, 5], min = 1
- * time[1]: list = [1, 3], min = 1
- * time[1]: list = [1, 3], min = 1 => return true, since 6 > list[1] = 3
+ * time[0]: list = [4, max]
+ * time[1]: list = [2, max]
+ * time[2]: list = [1, max]
+ * time[3]: list = [1, 5]
+ * time[4]: list = [1, 3]
+ * time[5]: list = [1, 3] => return true, since 6 > list[1] = 3
  */
 
 import java.util.*; // Stack
 
 public class Solution {
     public boolean increasingTriplet(int[] nums) {
-        //list[0] = the minimum last element of list with length 1;
-        //list[1] = the minimum last element of list with length 2;
-        int[] list;
-        int min;
+        // increasingList[0] = the smallest last element with length = 1
+        // increasingList[1] = the smallest last element with length = 2
+        int[] increasingList = new int[2];
         
-        list = new int[3];
-        list[0] = Integer.MAX_VALUE;
-        list[1] = Integer.MAX_VALUE;
-        min = Integer.MAX_VALUE;
-        
+        increasingList[0] = Integer.MAX_VALUE;
+        increasingList[1] = Integer.MAX_VALUE;
         for(int num: nums){
-            if(num > list[1]){
+            if(num > increasingList[1]){
                 return true;
             }
-            else if((num > list[0]) && (num < list[1])){
-                list[1] = num;   
+            else if(num > increasingList[0] && num < increasingList[1]){
+                increasingList[1] = num;
             }
-            else if((num > min) &&(num < list[1])){
-                list[1] = num;
-                list[0] = min;
-            }
-            else if(num < list[0]){
-                min = Math.min(num, min);
+            else if(num <  increasingList[0]){
+                increasingList[0] = num;
             }
             else{
                 continue;
@@ -49,18 +38,14 @@ public class Solution {
         return false;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         Solution sol;
         boolean isExist;
-        int[] nums = {4, 2, 1, 5, 3, 6};
+        int[] nums = {4, 2, 1, 5, 3, 6, 7};
 
         sol = new Solution();
-
         System.out.println("nums: " + Arrays.toString(nums));
-
         isExist = sol.increasingTriplet(nums);
-        
         System.out.println("isExist: " + isExist);
-	}
+    }
 }
