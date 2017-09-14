d2c9742401a73f7be878c1e92c2092ffc9aa68e5
Namo.Lu <chih-hung.lu@columbia.edu>
2017-09-14 18:21:41 -0400

    LeetCode#424 update

--- Java/424.LongestRepeatingCharacterReplacement/Solution.java
+++ Java/424.LongestRepeatingCharacterReplacement/Solution.java
@@ -1,69 +1,54 @@
-/* O(n)
+/* Sliding Window: O(n)
  * 1. Devide the case into 26 characters
- * 2. Keep a posList to record the modified positions
- * 3. If str.charAt(idx) == target, keep scanning to right
- * 4. Else, take the head from the posList, and append to the tail
+ * 2. In getMaxLength, if charArray[end] != c, k-- if k > 0
+ * 3. Otherwise, add start until reach the char != c
+ * 4. Keep doing step2 and step3, until reach the the end
  * 
- * ex: str = "ABCABB", k = 2, assume target = B
- * time[0]: idx = 0, head = -1, posList=[0], str = "BBCABB", currLen = 1 
- * time[1]: idx = 1, head = -1, posList=[0], str = "BBCABB", currLen = 2
- * time[2]: idx = 2, head = -1, posList=[0, 2], str = "BBBABB", currLen = 3
- * time[3]: idx = 3, head = 0, posList=[2,3], str = "ABBBBB", currLen = 3
- * time[4]: idx = 4, head = 0, posList=[2,3], str = "ABBBBB", currLen = 4
- * time[5]: idx = 5, head = 0, posList=[2,3], str = "ABBBBB", currLen = 5
+ * ex: str = "ABCABB", k = 2, assume c = A
+ * time[0]: end = 0, start = 0, str = "ABCABB", maxLen = 1, k = 2
+ * time[1]: end = 1, start = 0, str = "AACABB", maxLen = 2, k = 1
+ * time[2]: end = 2, start = 0, str = "AAAABB", maxLen = 3, k = 0
+ * time[3]: end = 3, start = 0, str = "AAAABB", maxLen = 4, k = 0
+ * time[4]: end = 4, start = 2, str = "ABAAAB", maxLen = 4, k = 0
+ * time[5]: end = 5, start = 3, str = "ABCAAA", maxLen = 4, k = 0
  */
 
 import java.util.*; // Stack
 
 public class Solution {
-    public int getMaxRepeat(String s, char target, int k){
-        LinkedList<Integer> posList;
-        int idx, currLen, maxLen, head;
-        
-        posList = new LinkedList<Integer>();
-        maxLen = 0;
-        currLen = 0;
-        head = -1;
-        idx = 0;
-        
-        for(idx= 0 ; idx < s.length(); idx++){
-            if(s.charAt(idx) != target){
-                if(posList.size() < k){
-                    posList.add(idx);
+    private int getMaxLength(char[] charArray, char c, int k){
+        int maxLen = 0;
+        int start = 0;
+        for(int end = 0; end < charArray.length; ++end){
+            if(charArray[end] != c){
+                if(k > 0){
+                    --k;
                 }
                 else{
-                    if(!posList.isEmpty()){
-                        head = posList.poll();
-                        posList.add(idx);
+                    while(charArray[start] == c){
+                        start++;
                     }
-                    else{
-                        head = idx;
-                    }
+                    start++;
                 }
             }
-            currLen = idx - head;
-            maxLen = Math.max(maxLen, currLen);
+            maxLen = Math.max(maxLen, end - start + 1);
         }
         return maxLen;
     }
     
     public int characterReplacement(String s, int k) {
-        int maxLen, currLen;
-        char c;
+        int maxLen = 0;
+        char[] charArray = s.toCharArray();
         
-        maxLen = 0;
-        for(c = 'A'; c <= 'Z'; c++){
-            currLen = getMaxRepeat(s, c, k);
-            if(maxLen < currLen){
-                maxLen = currLen;
-            }
-        }
+        for(char c = 'A'; c <= 'Z'; ++c){
+            maxLen = Math.max(maxLen, getMaxLength(charArray, c, k));
+        }    
         return maxLen;
     }
  
     public static void main(String[] args){
         Solution sol;
-        String str = "AABABBA";
+        String str = "ABCABB";
         int k, maxLen;
 
         sol = new Solution();
