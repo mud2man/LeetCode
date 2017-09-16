17afa4d7ba896a64dbc2b9b0d11f5ba8681b740f
Namo.Lu <chih-hung.lu@columbia.edu>
2017-09-15 22:04:06 -0400

    LeetCode#435 refine

--- Java/435.NonOverlappingIntervals/Solution.java
+++ Java/435.NonOverlappingIntervals/Solution.java
@@ -1,8 +1,8 @@
 /* Sort and Greedy: O(nlogn)
- * 1. Sort intervals by end time
- * 2. Select the next index of interval without overlap
- * 3. Or, if the next interval with overlaping, but the start time is latter than the current one
- * 4. Replace the current idx with this one, and call "greedySelectNext" to select the next valid interval
+ * 1. The same as the problem: find the maximum number of intervals that are non-overlapping
+ * 2. Sort intervals by end time, and set the previous end time previousEnd as Integer.MIN_VALUE
+ * 3. Select the next index of interval without overlap, update previousEnd, and accumulate maxCount
+ * 4. Repeat step2 and step3, and retunr intervals.length - maxCount
  */
 
 import java.util.*; // Stack
@@ -15,44 +15,26 @@ class Interval {
     }
 
 public class Solution {
-    private class EndPointComparator implements Comparator<Interval>{
+    private class IntervalComparator implements Comparator<Interval>{
         @Override
-        public int compare(Interval o1, Interval o2) {
-            return o2.end - o1.end;
+        public int compare(Interval interval0, Interval interval1) {
+            return interval0.end - interval1.end;
         }
-    }
-    
-    public int greedySelectNext(Interval[] intervals, int idx){
-        int nextIdx;
-
-        nextIdx = idx + 1;
-        while(nextIdx < intervals.length && intervals[nextIdx].end > intervals[idx].start){
-            if(intervals[nextIdx].start > intervals[idx].start){
-                nextIdx = greedySelectNext(intervals, nextIdx);
-                break;
-            }
-            nextIdx++;
-        }
-        return nextIdx;
     }
     
     public int eraseOverlapIntervals(Interval[] intervals) {
-        int count, size, idx;
+        int maxCount = 0;
+        Arrays.sort(intervals, new IntervalComparator());
         
-        idx = 0;
-        count = 1;
-        size = intervals.length;
-        Arrays.sort(intervals, new EndPointComparator());
+        int previousEnd = Integer.MIN_VALUE;
+        for(Interval interval: intervals){
+            if(previousEnd <= interval.start){
+                maxCount++;
+                previousEnd = interval.end;
+            }
+        }
         
-        if(size == 0){
-            return 0;
-        }
-
-        while(idx < intervals.length ){
-            idx = greedySelectNext(intervals, idx);
-            count = (idx < intervals.length )? count + 1: count;
-        }
-        return (size - count); 
+        return intervals.length - maxCount;
     }
 
     public static void main(String[] args){
