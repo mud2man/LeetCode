7f430c82ac6255585262a365d95edb517a6dd3d6
Namo.Lu <chih-hung.lu@columbia.edu>
2017-09-16 11:04:40 -0400

    LeetCode#442 refine

--- Java/442.FindAllDuplicatesinArray/Solution.java
+++ Java/442.FindAllDuplicatesinArray/Solution.java
@@ -1,53 +1,47 @@
 /* O(n)
- * 1. If nums[idx] != idx + 1, put nums[idx] to nums[nums[idx] - 1], nums[idx] = -1,  and call recursiveSwap
+ * 1. Travserse nums from left to right, and call helper 
+ * 2. If nums[index] == 0, means index + 1 never meet before, set nums[index] = -1
+ * 3. If nums[index] == -1, means index + 1 already meet before, add index + 1 into dulpicates
+ * 4. If nums[index] > 0, means index + 1 never meet before, set nums[index] = -1, and call helper with index = nums[index] - 1
+
  *
  * ex: nums= {2, 3, 1, 3}
- * time = 0; nums= {-1, 3, 1, 3}, num = 2
- * time = 1; nums= {-1, 2, 1, 3}, num = 3
- * time = 2; nums= {-1, 2, 3, 3}, num = 1
- * time = 3; nums= {1, 2, 3, 3}
- * time = 4; nums= {1, 2, 3, -1}, num = 3 
- * time = 5; nums= {1, 2, 3, -1}, duplicates = [3] 
+ * time = 0; nums= {0, 3, 1, 3}, num = 2
+ * time = 1; nums= {0, -1, 1, 3}, num = 3
+ * time = 2; nums= {0, -1, -1, 3}, num = 1
+ * time = 3; nums= {-1, -1, -1, 3}
+ * time = 4; nums= {-1, -1, -1, 0}, num = 3 
+ * time = 5; nums= {-1, -1, -1, 0}, duplicates = [3] 
  */
 
 import java.util.*; // Stack
 
 public class Solution {
-    public void recursiveSwap(int[] nums, List<Integer> duplicates, int num){
-        int tmp;
-        
-        if(num == -1){
-            return;
-        }
-        else if(nums[num - 1] == num){
-            duplicates.add(num);
-        }
-        else{
-            if(nums[num - 1] == -1){
-                nums[num - 1] = num;
-            }
-            else{
-                tmp = nums[num - 1];
-                nums[num - 1] = num;
-                recursiveSwap(nums, duplicates, tmp);
-            }
+    private void helper(List<Integer> dulpicates, int[] nums, int index){
+        switch (nums[index]){
+            case -1:
+                dulpicates.add(index + 1);
+                break;
+            case 0:
+                nums[index] = -1;
+                break;
+            default:
+                int nextIndex = nums[index] - 1;
+                nums[index] = -1;
+                helper(dulpicates, nums, nextIndex);
         }
     }
     
     public List<Integer> findDuplicates(int[] nums) {
-        int idx, tmp;
-        List<Integer> duplicates;
-        
-        duplicates = new ArrayList<Integer>();
-        
-        for(idx = 0; idx < nums.length; idx++){
-            if(nums[idx] != idx + 1){
-                tmp = nums[idx];
-                nums[idx] = -1;
-                recursiveSwap(nums, duplicates, tmp);
-            }
+        List<Integer> dulpicates = new ArrayList<Integer>();
+        for(int i = 0; i < nums.length; ++i){
+            if(nums[i] != -1 && nums[i] != 0){
+                int startIndex = nums[i] - 1;
+                nums[i] = 0;
+                helper(dulpicates, nums, startIndex);
+            }   
         }
-        return duplicates;
+        return dulpicates;
     }
 
     public static void main(String[] args){
