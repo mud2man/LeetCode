83594352057e23a8661def5d0f4d46fba104d7c5
Namo.Lu <chih-hung.lu@columbia.edu>
2017-09-17 11:16:11 -0400

    LeetCode#451 refine

--- Java/451.SortCharactersByFrequency/Solution.java
+++ Java/451.SortCharactersByFrequency/Solution.java
@@ -1,60 +1,55 @@
-/* Use hashMap and sorting: O(n)
+/* Bucket sort and HashMap: O(n)
  * 1. Create a hashMap "freqMap" to record the frequency of every character
- * 2. Sort the hashMap and output the answer string
+ * 2. According the frequency f, and put it into the f-th slot of bucket
+ * 3. Iterate bucket from right, and append the character based on the frequency 
  */
 
 import java.util.*; // Stack
 
-class FreqComparator implements Comparator<Map.Entry<Character, Integer>>{
- 
-	@Override
-	public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2 )
-    {
-        return (o2.getValue()).compareTo(o1.getValue());
-    }
-}
-
 public class Solution {
     public String frequencySort(String s) {
-        int i;
-        char c;
-        HashMap<Character, Integer> freqMap;
-        List<Map.Entry<Character, Integer>> freqList;
-        StringBuilder sb;
+        HashMap<Character, Integer> frequencyMap = new HashMap<Character, Integer>();
         
-        freqMap = new HashMap<Character, Integer>();
-        sb = new StringBuilder("");
-        
-        for(i = 0; i < s.length(); ++i){
-            c = s.charAt(i);
-            if(freqMap.containsKey(c)){
-                freqMap.put(c, freqMap.get(c) + 1);
+        for(char c: s.toCharArray()){
+            if(!frequencyMap.containsKey(c)){
+                frequencyMap.put(c, 0);
             }
-            else{
-                freqMap.put(c, 1);
+            frequencyMap.put(c, frequencyMap.get(c) + 1);
+        }
+        
+        Set<Character>[] bucket = new HashSet[s.length() + 1];
+        for(Map.Entry<Character, Integer> entry: frequencyMap.entrySet()){
+            int frequency = entry.getValue();
+            char c = entry.getKey();
+            if(bucket[frequency] == null){
+                bucket[frequency] = new HashSet<Character>();
+            }
+            bucket[frequency].add(c);
+        }
+        
+        StringBuilder answer = new StringBuilder("");
+        for(int i = s.length(); i >= 0; --i){
+            int frequency = i;
+            if(bucket[i] != null){
+                for(char c: bucket[i]){
+                    for(int j = 0; j < frequency; ++j){
+                        answer.append(c);
+                    }
+                }
             }
         }
         
-        freqList = new LinkedList<Map.Entry<Character, Integer>>(freqMap.entrySet());
-        Collections.sort(freqList, new FreqComparator());
-
-        for(Map.Entry<Character, Integer> entry: freqList){
-            for(i = 0; i < entry.getValue(); ++i){
-                sb.append(entry.getKey());
-            }
-        }
-        
-        return sb.toString();
+        return answer.toString();
     }
     
     public static void main(String[] args){
-		Solution sol;
-		String s;
+        Solution sol;
+        String s;
 
-		s = "aAbb";
-		sol = new Solution();
-		
-		System.out.println("before sorting: " + s);
-		System.out.println("after sorting: " + sol.frequencySort(s));
-	}
+        s = "aAbb";
+        sol = new Solution();
+        
+        System.out.println("before sorting: " + s);
+        System.out.println("after sorting: " + sol.frequencySort(s));
+    }
 }
