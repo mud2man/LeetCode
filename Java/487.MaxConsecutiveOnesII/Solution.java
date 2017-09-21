0eba609937dd73073185651fa5bd944acaddd814
Namo.Lu <chih-hung.lu@columbia.edu>
2017-09-20 23:53:46 -0400

    LeetCode#487

--- Java/487.MaxConsecutiveOnesII/Solution.java
+++ Java/487.MaxConsecutiveOnesII/Solution.java
@@ -1,51 +1,36 @@
 /* Queue O(n)
- * 1. Use a queue to store the latest two index of zeros
- * 2. If nums[idx] == 1, accumulate current length with 1
- * 3. If nums[idx] == 0, current length is equal to (idx - the head of queue)
- * 4. In every step, compare the current length and maximum length, and select the bigger one
+ * 1. Use two pointers to store the latest two index of zeros
+ * 3. If nums[i] == 0, update ptr0 = ptr1, ptr1 = i
+ * 4. Update maxLen = max(maxLen, i - ptr0);
  *
  * ex: nums{1, 0, 1, 1, 0, 0, 1}
- * idx = 0, nums[idx] = 1, queue = {-1}, currLen = 1
- * idx = 1, nums[idx] = 0, queue = {-1, 1}, currLen = 1 - (-1) = 2
- * idx = 2, nums[idx] = 1, queue = {-1, 1}, currLen = 2 + 1 = 3
- * idx = 3, nums[idx] = 1, queue = {-1, 1}, currLen = 3 + 1 = 4
- * idx = 4, nums[idx] = 0, queue = {1, 4}, currLen = 4 - 1 = 3
- * idx = 5, nums[idx] = 0, queue = {4, 5}, currLen = 5 - 4 = 1
- * idx = 6, nums[idx] = 1, queue = {4, 5}, currLen = 1 + 1 = 1
+ * idx = 0, nums[idx] = 1, ptr0 = -1, ptr1 = -1, currLen = 1
+ * idx = 1, nums[idx] = 0, ptr0 = -1, ptr1 = 1, currLen = 1 - (-1) = 2
+ * idx = 2, nums[idx] = 1, ptr0 = -1, ptr1 = 1, currLen = 2 - (-1) = 3
+ * idx = 3, nums[idx] = 1, ptr0 = -1, ptr1 = 1, currLen = 3 - (-1) = 4
+ * idx = 4, nums[idx] = 0, ptr0 = 1, ptr1 = 4, currLen = 4 - 1 = 3
+ * idx = 5, nums[idx] = 0, ptr0 = 4, ptr1 = 5, currLen = 5 - 4 = 1
+ * idx = 6, nums[idx] = 1, ptr0 = 4, ptr1 = 5, currLen = 6 + 4 = 2
  */          
 
 import java.util.*; // Stack
 
 public class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int idx, maxLen, currLen;
-        Queue<Integer> queue;
+        int ptr0 = -1;
+        int ptr1 = -1;
+        int maxLen = 1;
         
-        queue = new LinkedList<Integer>();
-        maxLen = 0;
-        currLen = 0;
-        queue.offer(-1);
-        
-        for(idx = 0; idx < nums.length; idx++){
-            if(nums[idx] != 0){
-                currLen++;
+        for(int i = 0; i < nums.length; ++i){
+            if(nums[i] == 0){
+                ptr0 =  ptr1;
+                ptr1 = i;
             }
-            else{
-                if(queue.size() < 2){
-                    queue.offer(idx);
-                    currLen = idx - queue.peek();
-                }
-                else{
-                    queue.poll();
-                    queue.offer(idx);
-                    currLen = idx - queue.peek();
-                }
-            }
-            maxLen = Math.max(maxLen, currLen);
+            maxLen = Math.max(maxLen, i - ptr0);
         }
         
         return maxLen;
-    } 
+    }
 
     public static void main(String[] args){
         Solution sol;
