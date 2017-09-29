d39a0e35fa477ad78d6bc50acd702b1799dd2215
Namo.Lu <chih-hung.lu@columbia.edu>
2017-09-28 23:35:02 -0400

    Add namo's version

--- Java/221.MaximalSquare/Solution.java
+++ Java/221.MaximalSquare/Solution.java
@@ -1,6 +1,9 @@
 /* Dynamic Programming: O(n*m)
- * 1. dp[y][x] = the maximum area of square with bottom-rightest corner at (y, x)
- * 2. dp[y][x] = min(dp[y - 1][x] + 1, dp[y][x - 1] + 1, dp[y - 1][x - 1] + 1);
+ * 1. dp[y][x] = the length of biggest square with right-bottom cornet at (y, x)
+ * 2. Let upLength = dp[y - 1][x], leftLength = dp[y][x - 1]
+ * 3. If upLength != leftLength, dp[y][x] = min(upLength, leftLength) + 1
+ * 4. Otherwise, if matrix[y - upLength][x - leftLength] == '1', then dp[y][x] = upLength + 1
+ * 5. Otherwise, dp[y][x] = upLength
  */
 
 import java.util.*;
@@ -8,33 +11,44 @@ import java.util.*;
 
 public class Solution{
     public int maximalSquare(char[][] matrix) {
-        if(matrix.length == 0){
-            return 0;
+        int depth = matrix.length;
+        int width = (depth > 0)? matrix[0].length: 0;
+        int[][] dp = new int[depth][width];
+        
+        int maxLen = 0;
+        
+        for(int x = 0; x < width; ++x){
+            dp[0][x] = (matrix[0][x] == '1')? 1: 0;
+            maxLen = Math.max(maxLen, dp[0][x]);
         }
         
-        int[][] dp = new int[matrix.length][matrix[0].length];
-        int maxLen = 0;
-        for(int y = 0; y < matrix.length; ++y){
-            for(int x = 0; x < matrix[0].length; ++x){
-                char c = matrix[y][x];
-                if(c == '0'){
-                    dp[y][x] = 0;
-                    continue;
-                }
-                else{
-                    if(y > 0 && x > 0){
-                        dp[y][x] = Math.min(dp[y - 1][x], dp[y][x - 1]);
-                        dp[y][x] = Math.min(dp[y - 1][x - 1], dp[y][x]);
-                        dp[y][x]++;
+        for(int y = 1; y < depth; ++y){
+            dp[y][0] = (matrix[y][0] == '1')? 1: 0;
+            maxLen = Math.max(maxLen, dp[y][0]);
+        }
+        
+        for(int y = 1; y < depth; ++y){
+            for(int x = 1; x < width; ++x){
+                if(matrix[y][x] == '1'){
+                    int upLength = dp[y - 1][x];
+                    int leftLength = dp[y][x - 1];
+                    if(upLength != leftLength){
+                        int minLength = Math.min(upLength, leftLength);
+                        dp[y][x] = minLength + 1;
                     }
                     else{
-                        dp[y][x] = 1;
+                        if(matrix[y - upLength][x - leftLength] == '1'){
+                            dp[y][x] = upLength + 1;
+                        }
+                        else{
+                            dp[y][x] = upLength;
+                        }
                     }
-                    maxLen = Math.max(maxLen, dp[y][x]);               
+                    maxLen = Math.max(maxLen, dp[y][x]);
                 }
             }
         }
-        return maxLen * maxLen;
+        return (maxLen * maxLen);
     }
 
     public static void main(String[] args){
