83c9a57a14d0e8b2ae86e8983dfa94c179d41182
Namo.Lu <chih-hung.lu@columbia.edu>
2017-10-02 10:52:06 -0400

    LeetCode#547 refine

--- Java/547.FriendCircles/Solution.java
+++ Java/547.FriendCircles/Solution.java
@@ -1,49 +1,43 @@
-/* Union and Find: O(n^2*logn) But LeetCode has O(n^2) solution
+/* DFS: O(n^2)
+ * 1. Have a array visited to record if we meet the person
+ * 2. Iterate all person if we never meet (visited[person] == false), and call dfs to visit all his friends
+ * 3. In dfs, visit all the person's friend. If we didn't meet the friend visited[friend], the call dfs again
+ * 4. Therefore, when we invoke dfs to visit the person, we will visit all his friends
+ * 5. We only need to accumulate circle count on the first-level loop if visited[person] == false
  */
 
 import java.util.*;
 
 
 public class Solution{
-    private int find(int[] roots, int target){
-        int farther = roots[target];
-        int son = target;
-        while(farther != son){
-            // compression
-            roots[son] = roots[farther];
-            son = farther;
-            farther = roots[farther];
-        } 
-        return farther;
-    }
-    
-    public int findCircleNum(int[][] M) {
-        int size = M.length;
-        int[] roots = new int[size];
-        
-        for(int i = 0; i < size; ++i){
-            roots[i] = i;
-        }
-        
-        int circleNum = size;
-        for(int y = 0; y < size; ++y){
-            for(int x = y + 1; x < size; ++x){
-                if(M[y][x] == 1){
-                    int me = x;
-                    int friend = y;
-                    int myRoot = find(roots, me);
-                    int friendRoot = find(roots, friend);
-                    
-                    // join
-                    if(myRoot != friendRoot){
-                        circleNum--;
-                        roots[myRoot] = Math.min(myRoot, friendRoot);
-                        roots[friendRoot] = roots[me];   
-                    }
+    private void dfs(int person, boolean[] visited, int[][] matrix){
+        visited[person] = true;
+        int[] friendList = matrix[person];
+        for(int friend = 0; friend < friendList.length; ++friend){
+            if(friendList[friend] == 1){
+                if(visited[friend] == false){
+                    dfs(friend, visited, matrix);
                 }
             }
         }
-        return circleNum;
+    }
+    
+    public int findCircleNum(int[][] matrix) {
+        if(matrix == null || matrix.length == 0){
+            return 0;
+        }
+        
+        int size = matrix.length;
+        int circles = 0;
+        boolean[] visited = new boolean[size];
+        for(int person = 0; person < size; ++person){
+            if(visited[person] == false){
+                dfs(person, visited, matrix);
+                circles++;
+            }
+        }
+        
+        return circles;
     }
 
     public static void main(String[] args){
