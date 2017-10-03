b244617575c49c41566b069206bab8a53f003816
Namo.Lu <chih-hung.lu@columbia.edu>
2017-10-02 22:04:04 -0400

    LeetCode#294 refine

--- Java/294.FlipGameII/Solution.java
+++ Java/294.FlipGameII/Solution.java
@@ -1,57 +1,42 @@
 /* Backtrack with memorization: O(n!)
  * 1. If any case can make sure I win in the next match, the I win 
- * 2. If I win under no matter how opponent move in the next matches, then I win
+ * 2. iWin return the result if the current user can win when he get the initial string s no matter me or opponant
  */
 
 import java.util.*;
 
 public class Solution{
-    private boolean helper(HashSet<String> mustWinSet, HashSet<String> mayLoseSet, String s){
-        if(mustWinSet.contains(s)){
-            return true;
-        }
+    private boolean iWin(Set<String> winSet, Set<String> loseSet, String s){
+        if(winSet.contains(s)){return true;}
+        if(loseSet.contains(s)){return false;}
         
-        if(mayLoseSet.contains(s)){
-            return false;
-        }
-
-        boolean canWin = false;
-        for(int i = 0; i < (s.length() - 1); ++i){
-            // if any step can cause I winm then I win
-            if(s.startsWith("++", i)){
-                canWin = true;
-                String firstS = s.substring(0, i) + "--" + s.substring(i + 2);
-                // must win under all the situations moved by opponent, then I win
-                for(int j = 0; j < (firstS.length() - 1); ++j){
-                    if(firstS.startsWith("++", j)){
-                        String secondS = firstS.substring(0, j) + "--" + firstS.substring(j + 2);
-                        if(!helper(mustWinSet, mayLoseSet, secondS)){
-                            mayLoseSet.add(secondS);
-                            canWin = false;
-                            break;
-                        }
-                    }
+        int i = 0;
+        while(i < s.length()){
+            int nextIndex = s.indexOf("++", i);
+            if(nextIndex != -1){
+                StringBuilder sb = new StringBuilder(s);
+                sb.setCharAt(nextIndex++, '-');
+                sb.setCharAt(nextIndex, '-');
+                
+                if(!iWin(winSet, loseSet, sb.toString())){
+                    winSet.add(s);
+                    return true;
                 }
-                if(canWin == true){
-                    break;
-                }
+                i = nextIndex;
+            }
+            else{
+                break;
             }
         }
-        
-        if(canWin){
-            mustWinSet.add(s);
-            return true;
-        }
-        else{
-            mayLoseSet.add(s);
-            return false;
-        }
+
+        loseSet.add(s);
+        return false;
     }
     
     public boolean canWin(String s) {
-        HashSet<String> mustWinSet = new HashSet<String>();
-        HashSet<String> mayLoseSet = new HashSet<String>();
-        return helper(mustWinSet, mayLoseSet, s);
+        Set<String> winSet = new HashSet<String>();
+        Set<String> loseSet = new HashSet<String>();
+        return iWin(winSet, loseSet, s);
     }
 
     public static void main(String[] args){
