4d81baa30998752070adfefe8ac1d3ccb870aeda
Namo.Lu <chih-hung.lu@columbia.edu>
2017-10-11 22:54:48 -0400

    LeetCode#491 update

--- Java/491.IncreasingSubsequences/backTrack/Solution.java
+++ Java/491.IncreasingSubsequences/backTrack/Solution.java
@@ -1,29 +1,43 @@
-/* Backtracking: O(2^n)
+/* Backtrack + Hashset: time = O(2^n), space = O(n)stack + O(n^2)hashset
+ * 1. Traverse from the leftest position of nums, and put the number num into the new list subsequence, if num never be used
+ * 2. In the backtracker, if the length of subsequence > 1, add it into subsequences
+ * 3. Travese the from startIndex, and put the never-meet and increasing element into subsequence
  */
 
 import java.util.*;
 
 public class Solution{
-    public List<List<Integer>> findSubsequences(int[] nums) {
-        Set<List<Integer>> res= new HashSet<List<Integer>>();
-        List<Integer> holder = new ArrayList<Integer>();
-        findSequence(res, holder, 0, nums);
-        List result = new ArrayList(res);
-        return result;
-     }
-
-    public void findSequence(Set<List<Integer>> res, List<Integer> holder, int index, int[] nums) {
-        if (holder.size() >= 2) {
-            res.add(new ArrayList(holder));
+    private void backtracker(List<List<Integer>> subsequences, LinkedList<Integer> subsequence, int[] nums, int startIndex){
+        if(subsequence.size() > 1){
+            subsequences.add(new LinkedList(subsequence));
         }
-        for (int i = index; i < nums.length; i++) {
-            if(holder.size() == 0 || holder.get(holder.size() - 1) <= nums[i]) {
-                holder.add(nums[i]);
-                findSequence(res, holder, i + 1, nums);
-                holder.remove(holder.size() - 1);
+        
+        Set<Integer> used = new HashSet<Integer>();
+        for(int i = startIndex; i < nums.length; ++i){
+            if(subsequence.peekLast() <= nums[i] && !used.contains(nums[i])){
+                subsequence.add(nums[i]);
+                backtracker(subsequences, subsequence, nums, i + 1);
+                subsequence.pollLast();
+                used.add(nums[i]);
             }
         }
-    } 
+    }
+    
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        List<List<Integer>> subsequences = new LinkedList<List<Integer>>();
+        Set<Integer> used = new HashSet<Integer>();
+        
+        for(int i = 0; i < (nums.length - 1); ++i){
+            if(!used.contains(nums[i])){
+                LinkedList<Integer> subsequence = new LinkedList<Integer>();
+                subsequence.add(nums[i]);
+                backtracker(subsequences, subsequence, nums, i + 1);
+                used.add(nums[i]);
+            }
+        } 
+        
+        return subsequences;
+    }
 
     public static void main(String[] args){
         Solution sol;
