11edf75ac8798c6938042abe61aa78ffe0cf03d4
Namo.Lu <chih-hung.lu@columbia.edu>
2017-10-19 00:03:59 -0400

    LeetCode#127 refine

--- Java/127.WordLadder/Solution.java
+++ Java/127.WordLadder/Solution.java
@@ -1,9 +1,7 @@
-/* BFS: O(n^2*k), where k = length of word, because each pair only compare once
- * 1. Create currBeginWords to store the current beginning words
- * 2. Iterate every words in wordList, and compare them with the word in currBeginWords
- * 3. If only one letter changed, put it into nextBeginWords and remove it from wordList. 
- * 4. Also compare if it equals to endWord, if so, return
- * 5. Repeat step 2, 3, and 4, until currBeginWords is empty
+/* BFS: O(n*k*26), where k = length of word
+ * 1. Have a Hashset wordSet to store the un-visited node
+ * 2. Use BFS to visit the wordSet, and put the neighbors into the end of BFS queue
+ * 3. If encounter endWord, return distance
  */
 
 import java.util.*;
@@ -11,45 +9,54 @@ import java.util.*;
 
 //Definition for singly-linked list.
 public class Solution{
-    boolean isClosed(String sourceWord, String targetWord){
-        int count = 0;
-        for(int i = 0; i < sourceWord.length(); ++i){
-            if(sourceWord.charAt(i) != targetWord.charAt(i)){
-                count++;
-                if(count > 1){
-                    return false;
+        private List<String> getNeighbors(Set<String> wordSet, String currNode){
+        StringBuilder sb = new StringBuilder(currNode);
+        int length = currNode.length();
+        List<String> neighbors = new LinkedList<String>();
+        
+        for(int i = 0; i < length; ++i){
+            for(char c = 'a'; c <= 'z'; c++){
+                char tmp =  sb.charAt(i);
+                if(tmp == c){
+                    continue;
                 }
+                sb.setCharAt(i, c);
+                String neighbor = sb.toString();
+                if(wordSet.contains(neighbor)){
+                    neighbors.add(neighbor);
+                    wordSet.remove(neighbor);
+                }
+                sb.setCharAt(i, tmp);
             }
         }
-        return true;
+        return neighbors;
     }
     
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<String>(wordList);
         LinkedList<String> queue = new LinkedList<String>();
         queue.add(beginWord);
         
-        int length = 0;
+        if(!wordSet.contains(endWord)){
+            return 0;
+        }
+        
+        int distance = 1;
         while(!queue.isEmpty()){
-            System.out.println(queue);
             int size = queue.size();
-            length++;
             for(int i = 0; i < size; ++i){
-                String source = queue.pollFirst();
-                if(source.equals(endWord)){
-                    return length;
+                String currNode = queue.pollFirst();
+                if(currNode.equals(endWord)){
+                    return distance;
                 }
-                else{
-                    Iterator<String> itr = wordList.iterator();
-                    while( itr.hasNext() ){
-                        String target = itr.next();
-                        if(isClosed(source, target)){
-                            queue.add(target);
-                            itr.remove();
-                        }
-                    }
+                List<String> neighbors = getNeighbors(wordSet, currNode);
+                for(String neighbor: neighbors){
+                    queue.add(neighbor);
                 }
             }
+            distance++;
         }
+        
         return 0;
     }
 
