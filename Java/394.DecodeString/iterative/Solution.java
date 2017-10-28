9f3580265fd312e920fe458d9325bc549345ae4d
Namo.Lu <chih-hung.lu@columbia.edu>
2017-10-28 14:09:00 -0400

    LeetCode#394 update

--- Java/394.DecodeString/iterative/Solution.java
+++ Java/394.DecodeString/iterative/Solution.java
@@ -1,48 +1,47 @@
 /* Stack: O(n)
  * 1. Traverse the string from left to right
- * 2. If c is a digit, count.append(c)
- * 3. If c is '[', push the count into stack and reset count
- * 4. If c is ']', replicate top.sb with top.count time
- * 5. Otherwise, append the top of stack with c
+ * 2. If c is a digit, get the whole muliplication times, and update index with the index next to '['
+ * 3. If c is a letter. append it to the sb of top
+ * 4. If c is ']', append top.sb to the next top's sb with to.ptimes time
  */
 
 import java.util.*;
 
 public class Solution{
     private class StackNode{
+        int times;
         StringBuilder sb;
-        int count;
-        StackNode(int c){sb = new StringBuilder(""); count = c;}
+        StackNode(int t, StringBuilder s){times = t; sb = s;}
     }
     
     public String decodeString(String s) {
-        StringBuilder count = new StringBuilder("");
         Stack<StackNode> stack = new Stack<StackNode>();
-        stack.push(new StackNode(1));
+        StackNode top = new StackNode(1, new StringBuilder(""));
+        stack.push(top);
         
-        for(int i = 0; i < s.length(); ++i){
-            char c = s.charAt(i);
-            if(c >= '0' && c <= '9'){
-                count.append(c);
+        int index = 0;
+        while(index < s.length()){
+            char c = s.charAt(index);
+            if(Character.isDigit(c)){
+                int nextIndex = s.indexOf('[', index);
+                int times = Integer.parseInt(s.substring(index, nextIndex));
+                stack.push(new StackNode(times, new StringBuilder("")));
+                index = nextIndex + 1;
             }
-            else if(c == '['){
-                stack.push(new StackNode(Integer.parseInt(count.toString())));
-                count = new StringBuilder("");
-            }
-            else if(c == ']'){
-                StackNode top = stack.pop();
-                StringBuilder decodeSb = new StringBuilder("");
-                for(int j = 0; j < top.count; ++j){
-                    decodeSb.append(top.sb);
-                }
-                stack.peek().sb.append(decodeSb);
+            else if(Character.isLetter(c)){
+                stack.peek().sb.append(c);
+                index++;
             }
             else{
-                stack.peek().sb.append(c);
+                top = stack.pop();
+                String pattern = top.sb.toString();
+                for(int i = 0; i < top.times; ++i){
+                    stack.peek().sb.append(pattern);
+                }
+                index++;
             }
         }
-        
-        return stack.peek().sb.toString();
+        return stack.pop().sb.toString();
     }
 
     public static void main(String[] args){
