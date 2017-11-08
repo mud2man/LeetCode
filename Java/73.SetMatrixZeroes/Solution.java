f2eb1a236524dc4df011fe909cf1deb3d22e857c
Namo.Lu <chih-hung.lu@columbia.edu>
2017-11-08 18:32:04 -0500

    LeetCode#73 update

--- Java/73.SetMatrixZeroes/Solution.java
+++ Java/73.SetMatrixZeroes/Solution.java
@@ -2,104 +2,83 @@
  * 1. Check if the first row is zeros
  * 2. Check if the first column is zeros
  * 3. Check if zero among matrix[y][x], where x > 1, y > 1, 
- *	  and put the result in matrix[0][x] and matrix[y][0]
+ *      and put the result in matrix[0][x] and matrix[y][0]
  * 4. Reset matrix by matrix[0][x] and matrix[y][0] 
  */
 
 import java.util.*;
 
 public class Solution{
-	public void setZeroes(int[][] matrix) {
-        int y;
-        int x;
-        int rowNum;
-        int colNum;
-        boolean isZeroFirstRow;
-        boolean isZeroFirstCol;
+    public void setZeroes(int[][] matrix) {
+        int depth = matrix.length;
+        int width = (depth == 0)? 0: matrix[0].length;
+        boolean firstRowZeros = false;
+        boolean firstColumZeros = false;
         
-        rowNum = matrix.length;
-        colNum = 0;
-        if(rowNum > 0){
-           colNum =  matrix[0].length;
-        }
-        
-        isZeroFirstRow = false;
-        for(x = 0; x < colNum; ++x){
-            if(matrix[0][x] == 0){
-                isZeroFirstRow = true;
-                break;
-            }    
-        }
-        
-        isZeroFirstCol = false;
-        for(y = 0; y < rowNum; ++y){
+        for(int y = 0; y < depth; ++y){
             if(matrix[y][0] == 0){
-                isZeroFirstCol = true;
+                firstColumZeros = true;
                 break;
-            }    
+            }
         }
         
-        //record zeroed column and zerod row
-        for(y = 1; y < rowNum; ++y){
-            for(x = 1; x < colNum; ++x){
+        for(int x = 0; x < width; ++x){
+            if(matrix[0][x] == 0){
+                firstRowZeros = true;
+                break;
+            }
+        }
+        
+        for(int y = 1; y < depth; ++y){
+            for(int x = 1; x < width; ++x){
                 if(matrix[y][x] == 0){
                     matrix[0][x] = 0;
                     matrix[y][0] = 0;
                 }
-            }  
+            }
         }
         
-        //reset columns to zeros
-        for(x = 1; x < colNum; ++x){
-            if(matrix[0][x] == 0){
-                for(y = 1; y < rowNum; ++y){
+        for(int y = 1; y < depth; ++y){
+            for(int x = 1; x < width; ++x){
+                if(matrix[y][0] == 0 || matrix[0][x] == 0){
                     matrix[y][x] = 0;
                 }
             }
         }
         
-        //reset rows to zeros
-        for(y = 1; y < rowNum; ++y){
-            if(matrix[y][0] == 0){
-                for(x = 1; x < colNum; ++x){
-                    matrix[y][x] = 0;
-                }
-            }
-        }
-        
-        if(isZeroFirstRow == true){
-            for(x = 0; x < colNum; ++x){
-                matrix[0][x] = 0;
-            }
-        }
-        
-        if(isZeroFirstCol == true){
-            for(y = 0; y < rowNum; ++y){
+        for(int y = 0; y < depth; ++y){
+            if(firstColumZeros == true){
                 matrix[y][0] = 0;
+            }
+        }
+        
+        for(int x = 0; x < width; ++x){
+            if(firstRowZeros == true){
+                matrix[0][x] = 0;
             }
         }
     }
  
     public static void main(String[] args){
-		Solution sol;
-		int pathNum;
-		int i;
-		int[][] matrix = {{0, 1, 3},
-					      {1, 1, 4},
-					      {5, 0, 4}};
+        Solution sol;
+        int pathNum;
+        int i;
+        int[][] matrix = {{0, 1, 3},
+                          {1, 1, 4},
+                          {5, 0, 4}};
 
-		sol = new Solution();
-		
+        sol = new Solution();
+        
         System.out.println("before reset matrix[][]: ");
-		for(i = 0; i < matrix.length; ++i){
-        	System.out.println(Arrays.toString(matrix[i]));
-		}
+        for(i = 0; i < matrix.length; ++i){
+            System.out.println(Arrays.toString(matrix[i]));
+        }
 
-		sol.setZeroes(matrix);
-		
+        sol.setZeroes(matrix);
+        
         System.out.println("after reset matrix[][]: ");
-		for(i = 0; i < matrix.length; ++i){
-        	System.out.println(Arrays.toString(matrix[i]));
-		}
-	}
+        for(i = 0; i < matrix.length; ++i){
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+    }
 }
