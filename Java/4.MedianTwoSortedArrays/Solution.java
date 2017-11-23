fd711aec3f9670e144debfb1af52a6723337979b
Namo.Lu <chih-hung.lu@columbia.edu>
2017-11-23 15:39:08 -0500

    LeetCode#3 refine

--- Java/4.MedianTwoSortedArrays/Solution.java
+++ Java/4.MedianTwoSortedArrays/Solution.java
@@ -1,55 +1,49 @@
-/* Hash: Time:O(logn*logm) Space:O(1)
- * 1. Assume we have findKth API
- * 2. If (length1 + length2) is odd, then k = ((length1 + length2 - 1) / 2), retrun findKth(nums1, nums2, k)
- * 3. Otherwise, return (findKth(nums1, nums2, k) + findKth(nums1, nums2, k + 1)) / 2
- * 4. Implement findKth by getLessEqualCount, findKth may fail because the kth element may not in nums1
- * 5. Implement getLessEqualCount by Arrays.binarySearch, which return the number of elemnet less than or equal to target
- * 6. In the main function findMedianSortedArrays, if the invoke of findKth(nums1, nums2, k) fail, switch to findKth(nums2, nums1, k)
+/* Hash: Time:O(log(m + n)), Space:O(1)
  */
 
 import java.util.*;
 
 public class Solution{
-    private int getLessEqualCount(int[] nums1, int[] nums2, int target){
-        int count1 = Arrays.binarySearch(nums1, target);
-        if(count1 < 0){
-            count1 = -(count1 + 1);
-        }
-        else{
-            count1 = Arrays.binarySearch(nums1, target + 1);
-            count1 = (count1 < 0)? -(count1 + 1): count1;
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] big = (nums1.length > nums2.length)? nums1: nums2;
+        int[] small = (nums1.length <= nums2.length)? nums1: nums2;
+        int size = nums1.length + nums2.length;
+        
+        //corner case
+        if(small.length == 0){
+            if(big.length == 0){
+                return 0.0;
+            }
+            else if((big.length % 2) == 1){
+                return (double)big[big.length / 2];
+            }
+            else{
+                return (double)(big[big.length / 2] + big[(big.length / 2) - 1]) / 2.0;
+            }
         }
         
-        int count2 = Arrays.binarySearch(nums2, target);
-        if(count2 < 0){
-            count2 = -(count2 + 1);
+        int halfSize = size / 2;
+        int lbIndex = 0;
+        int hbIndex = 0;
+        if((size % 2) == 1){
+            hbIndex = (small[small.length - 1] > big[big.length - 1])? small.length - 2: small.length - 1;
         }
         else{
-            count2 = Arrays.binarySearch(nums2, target + 1);
-            count2 = (count2 < 0)? -(count2 + 1): count2;
+            hbIndex = small.length - 1;
         }
-        return (count1 + count2);
-    }
-    
-    public boolean findKth(int[] nums1, int[] nums2, int k, double[] target){
-        boolean isFound = false;
-        int hb = nums1.length - 1;
-        int lb = 0;
+        
+        //binary search
+        int lb = lbIndex;
+        int hb = hbIndex;
+        int bigIndex;
         while(lb <= hb){
             int midIndex = (lb + hb) / 2;
-            int lessEqualCount = getLessEqualCount(nums1, nums2, nums1[midIndex]);
-            int lessCount = getLessEqualCount(nums1, nums2, nums1[midIndex] - 1);
-            if(lessCount == k){
+            bigIndex = halfSize - midIndex - 2;
+            if(small[midIndex] <= big[bigIndex + 1] && (midIndex == hbIndex || big[bigIndex] <= small[midIndex + 1])){
                 hb = midIndex;
-                isFound = true;
                 break;
             }
-            else if(lessCount < k && lessEqualCount > k){
-                hb = midIndex;
-                isFound = true;
-                break;
-            }
-            else if(lessCount > k){
+            else if(small[midIndex] > big[bigIndex + 1]){
                 hb = midIndex - 1;
             }
             else{
@@ -57,54 +51,40 @@ public class Solution{
             }
         }
         
-        if(isFound == false){
-            return false;
+        int smallIndex = hb;
+        bigIndex = (smallIndex == -1)? halfSize: (halfSize - smallIndex - 2);
+
+        if(smallIndex == -1){
+            if((size % 2) == 1){
+                return (double)Math.min(small[0], big[halfSize]);
+            }
+            else{
+                if(big.length > halfSize){
+                    int median0 = big[halfSize - 1];
+                    int median1 = Math.min(big[halfSize], small[0]);
+                    return (double)(median0 + median1) / 2.0;
+                }
+                else{        
+                    return (double)(big[halfSize - 1] + small[0]) / 2.0;
+                }
+            }
         }
         else{
-            target[0] = (double)nums1[hb];
-            return true;
+            if((size % 2) == 1){
+                if(smallIndex < (small.length - 1)){
+                    return (double)Math.min(small[smallIndex + 1], big[bigIndex + 1]);
+                }
+                else{
+                    return (double)Math.max(small[smallIndex], big[bigIndex + 1]);
+                }
+            }
+            else{
+                int median0 = (bigIndex != - 1)? Math.max(small[smallIndex], big[bigIndex]): small[smallIndex];
+                int median1 = (smallIndex == (small.length - 1))? big[bigIndex + 1]: Math.min(small[smallIndex + 1], big[bigIndex + 1]);
+                return (double)(median0 + median1) / 2.0;
+            }
         }
     }
-    
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int length1 = nums1.length;
-        int length2 = nums2.length;
-        
-        if(((length1 + length2) % 2) == 1){
-            int k = (length1 + length2 - 1) / 2;
-            double[] target = new double[1];
-            if(findKth(nums1, nums2, k, target)){
-                return target[0];
-            }
-            else{
-                findKth(nums2, nums1, k, target);
-                return target[0];
-            }
-        }
-        else{
-            int k = (length1 + length2 - 1) / 2;
-            double[] target = new double[1];
-            double[] medians = new double[2];
-            if(findKth(nums1, nums2, k, target)){
-                medians[0] = target[0];
-            }
-            else{
-                findKth(nums2, nums1, k, target);
-                medians[0] = target[0];
-            }
-            
-            k++;
-            if(findKth(nums1, nums2, k, target)){
-                medians[1] = target[0];
-            }
-            else{
-                findKth(nums2, nums1, k, target);
-                medians[1] = target[0];
-            }
-            
-            return (medians[0] + medians[1]) / 2;
-        }
-    }   
  
     public static void main(String[] args){
         int[] nums1 = {1, 2};
