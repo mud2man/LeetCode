8a6261bdbd0dd31842b8aa5995e3dac0a7311336
Namo.Lu <chih-hung.lu@columbia.edu>
2017-11-27 22:05:08 -0500

    LeetCode#654 refine

--- Java/654.MaximumBinaryTree/Solution.java
+++ Java/654.MaximumBinaryTree/Solution.java
@@ -1,6 +1,7 @@
-/* O(n^2), but leetcode has O(n) solution
- * 1. Traverse nums from left to right
- * 2. Insert the new node into the current tree
+/* Stack: Time:O(n), Space:O(n)
+ * 1. Have a stack to record the right most children path, because the next tree node will only be added among them
+ * 2. Pop the stack if the value of top is less than current value
+ * 3. If the stack is not empty stack.peek().right = currNode
  */
 
 import java.util.*; // Stack
@@ -14,41 +15,29 @@ class TreeNode {
 }
 
 public class Solution {
-    private void insert(TreeNode root, TreeNode target){
-        while(root.right != null && root.right.val > target.val){
-            root = root.right;
-        }
-        
-        if(root.right == null){
-            root.right = target;
-        }
-        else{
-            TreeNode tmp = root.right;
-            root.right = target;
-            target.left = tmp;
-        }
-    }
-    
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        if(nums.length == 0){
-            return null;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        
+        for(int i = 0; i < nums.length; ++i){
+            TreeNode currNode = new TreeNode(nums[i]);
+            TreeNode top = null;
+            while(!stack.empty() && stack.peek().val < currNode.val){
+                top = stack.pop();
+            }
+            
+            if(!stack.empty()){
+                stack.peek().right = currNode;
+            }
+            
+            currNode.left = top;
+            stack.push(currNode);
         }
         
-        TreeNode root = new TreeNode(nums[0]);
-        
-        for(int i = 1; i < nums.length; ++i){
-            int num = nums[i];
-            if(num > root.val){
-                TreeNode newRoot = new TreeNode(num);
-                newRoot.left = root;
-                root = newRoot;
-            }
-            else{
-                insert(root, new TreeNode(num));
-            }
+        TreeNode top = null;
+        while(!stack.empty()){
+            top = stack.pop();
         }
-        
-        return root;
+        return top;
     }
 
     private void preOrder(TreeNode root){
