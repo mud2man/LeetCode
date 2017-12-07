108ea7a95be93253a5d1080bfebe666ecb10659d
Namo.Lu <chih-hung.lu@columbia.edu>
2017-12-06 21:26:59 -0500

    LeetCode#730 using Java

---
+++ Java/730.CountDifferentPalindromicSubsequences/Solution.java
@@ -0,0 +1,67 @@
+/* Dynamic Programming: Time:O(n^3), Space:O(n^2)
+ * 1. dp[y][x] = the count of different palindromic subsequences between S.substring[y][x]
+ * 2. dp[y][x] = dp[y - 1][x](without S.charAt(y)) + dp[y - 1][z](with S.charAt(y)) - dp[w][z](intersection), 
+      where S.charAt(z - 1) = target, S.charAt(w + 1) = target
+ */
+
+import java.util.*;
+
+public class Solution{
+    private int helper(String S, int[][] dp, int y, int x){
+        if(y == x){
+            return 1;
+        }   
+        
+        char target = S.charAt(y);
+        int countWithout = dp[y - 1][x];
+        int headIdx = x;
+        int tailIdx = y - 1;
+        
+        while(S.charAt(headIdx) != target && headIdx <= tailIdx){headIdx++;}
+        headIdx++;
+        int countWith = (headIdx <= tailIdx)? dp[tailIdx][headIdx] + 1: 1;
+        
+        if(headIdx > tailIdx){
+            return (countWithout + countWith) % 1000000007;
+        }
+        
+        while(S.charAt(tailIdx) != target && headIdx <= tailIdx){tailIdx--;}
+        tailIdx--;
+        
+        int countIntersect;
+        if(headIdx <= tailIdx){
+            countIntersect = dp[tailIdx][headIdx] + 1;
+        }
+        else if(S.charAt(headIdx) == target){
+            countIntersect = 1;
+        }
+        else{
+            countIntersect = 0;
+        }
+        
+        int count = (countWithout + countWith - countIntersect) % 1000000007;
+        count = (count < 0)? count += 1000000007: count;
+        return count;
+    }
+    
+    public int countPalindromicSubsequences(String S) {
+        int length = S.length();
+        int[][] dp = new int[length][length];
+        
+        for(int y = 0; y < length; ++y){
+            for(int x = y; x >= 0; --x){
+                dp[y][x] = helper(S, dp, y, x);
+            }
+        }
+        
+        return dp[length - 1][0];
+    }
+
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        String S = "bccb";
+        
+        System.out.println("S: " + S);
+        System.out.println("count:" + sol.countPalindromicSubsequences(S));
+    }
+}
