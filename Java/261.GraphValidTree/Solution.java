c41933d43f776626552db34847f96288981cc259
Namo.Lu <chih-hung.lu@columbia.edu>
2018-01-05 21:37:41 -0800

    LeetCode#261 using Java

---
+++ Java/261.GraphValidTree/Solution.java
@@ -0,0 +1,61 @@
+/* Union and Find: O(eloge), where e is the number of edges
+ * 1. Return false if edges.length != (n - 1)
+ * 2. Use union and find to check if the added edge belongs to the same component
+ */
+
+import java.util.*;
+
+public class Solution{
+        private int find(int[] roots, int node){
+        if(roots[node] == node){
+            return node;
+        }
+        else{
+            //compression
+            roots[node] = roots[roots[node]];
+            
+            return find(roots, roots[roots[node]]);
+        }
+    }
+    
+    public boolean validTree(int n, int[][] edges) {
+        if(edges.length != (n - 1)){
+            return false;
+        }
+        
+        int[] roots = new int[n];
+        for(int i = 0; i < n; ++i){
+            roots[i] = i;
+        }
+        
+        for(int[] edge: edges){
+            int root0 = find(roots, edge[0]);
+            int root1 = find(roots, edge[1]);
+            
+            if(root0 == root1){
+                return false;
+            }
+            else{
+                //union
+                roots[root1] = root0; 
+            }
+        }
+        
+        return true;
+    }
+
+    public static void main(String[] args){
+        Solution sol;
+        int[][] edges = {{0, 1}, {0, 2}, {0, 3}, {1, 4}};
+        int n = 5;
+
+        System.out.println("n: " + n);
+        System.out.println("edges: ");
+        for(int[] edge: edges){
+            System.out.println(Arrays.toString(edge));
+        }
+        
+        sol = new Solution();    
+        System.out.println("is valid tree: " + sol.validTree(n, edges));
+    }
+}
