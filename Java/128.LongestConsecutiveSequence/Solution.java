ea8b565fa908faeb6ce9852bea078f0240ed42c4
Namo.Lu <chih-hung.lu@columbia.edu>
2018-01-19 13:34:03 -0800

    Refine LeetCode#128

--- Java/128.LongestConsecutiveSequence/Solution.java
+++ Java/128.LongestConsecutiveSequence/Solution.java
@@ -1,56 +1,36 @@
 /* Hash: Time:O(n), Space:O(n). LeetCode has simpler solution
- * 1. Keep a hashMap "lengthsMap" to store the mapping between node and length
- * 2. Keep a hashMap "headsMap" to store the mapping between node and available head
- * 3. Keep a hashMap "tailsMap" to store the mapping between node and available tail
- * 4. In each loop, check if the new node can be attached to the beginning of existing sequence
- * 5. Then, check if the new sequence can be merged to the tail of a existing sequence
+ * 1. Keep a hashMap "lengthsMap" to store the mapping between node and length. e.g.,{1, 2, 3, 4, 5}, map.get(1) = 1, map.get(5) = 5
+ * 2. In each loop, check if the new node can be attached to the begin or end of existing sequence
+ * 3. Then, update lengthsMap and maxLength
+ * 4. Because we prevent duplicated, the nodes on the "leftLength" and "rightLength" will not overlap
  */         
 
 import java.util.*;
 
 public class Solution {
     public int longestConsecutive(int[] nums) {
-        int maxLength = (nums.length > 0)? 1: 0;
-        Set<Integer> used = new HashSet<Integer>();
+        int maxLength = 0;
         HashMap<Integer, Integer> lengthsMap = new HashMap<Integer, Integer>();
-        HashMap<Integer, Integer> headsMap = new HashMap<Integer, Integer>();
-        HashMap<Integer, Integer> tailsMap = new HashMap<Integer, Integer>();
         
         for(int num: nums){
-            if(used.contains(num)){
+            if(lengthsMap.containsKey(num)){
                 continue;
             }
             else{
-                used.add(num);
-            }
-            
-            if(headsMap.containsKey(num)){
-                int previousHead = headsMap.get(num);
-                int previousLength = lengthsMap.get(previousHead);
-                int previousTail = previousHead + previousLength;
-                headsMap.remove(previousHead - 1);
-                lengthsMap.remove(previousHead);
-                tailsMap.put(previousTail, num);
-                headsMap.put(num - 1, num);
-                lengthsMap.put(num, previousLength + 1);
-                maxLength = Math.max(maxLength, previousLength + 1);
-            }
-            else{
-                lengthsMap.put(num, 1);
-                tailsMap.put(num + 1, num);
-                headsMap.put(num - 1, num);
-            }
-            
-            //merge
-            if(tailsMap.containsKey(num)){
-                int firstHead = tailsMap.get(num);
-                int mergedLength = lengthsMap.get(num) + lengthsMap.get(firstHead);
-                headsMap.remove(num - 1);
-                tailsMap.remove(num);
-                lengthsMap.remove(num);
-                lengthsMap.put(firstHead, mergedLength);
-                tailsMap.put(firstHead + mergedLength, firstHead);
-                maxLength = Math.max(maxLength, mergedLength);
+                int leftLength = lengthsMap.containsKey(num - 1)? lengthsMap.get(num - 1): 0;
+                int rightLength = lengthsMap.containsKey(num + 1)? lengthsMap.get(num + 1): 0;
+                int sum = leftLength + rightLength + 1;
+                
+                if(leftLength == 0 || rightLength == 0){
+                    lengthsMap.put(num, sum);
+                }
+                else {
+                    lengthsMap.put(num, 0);
+                }
+
+                lengthsMap.put(num - leftLength, sum);
+                lengthsMap.put(num + rightLength, sum);
+                maxLength = Math.max(maxLength, sum);
             }
         }
         
