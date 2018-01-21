27dad7b5557b4e2bc404f4824f3170532ec37044
Namo.Lu <chih-hung.lu@columbia.edu>
2018-01-20 23:29:51 -0800

    LeetCode#139 refine

--- Java/139.WordBreak/Solution.java
+++ Java/139.WordBreak/Solution.java
@@ -1,65 +1,52 @@
-/* Use Dynamic Programing: O(n^2),
- * 1. dp[i + 1] = (dp[i] & wordDict.contains(tail)) | (dp[i - 1] & wordDict.contains(tail)) | ....
- * ex: s: "leetcode" wordDict:["leet", "code"]
- *     dp[4] = (dp[3]=0 & wordDict.contains("t")) | (dp[2]=0 & wordDict.contains("et")) \
- *             (dp[1]=0 & wordDict.contains("eet")) | (dp[0]=1 & wordDict.contains("leet"))
+/* Use Dynamic Programing: O(n*k*m), where n is the length of s, k is the size of wordDict, m is the length of longest word
+ * 1. dp[i + 1] = trew, means s.substring(0, i + 1) can be seperated
+ * 2. Only handle when dp[i] is true, 
+ * 3. If dp[size] is true, means s.substring(0, size) can be seperated
+ * ex: s: "leetcode", wordDict:["leet", "code"]
+ *     dp[] = {1, 0, 0, 0, 1, 0, 0, 0, 1}
  */
 
 import java.util.*;
 
 public class Solution{
-    public boolean wordBreak(String s, Set<String> wordDict) {
-        String tail;
-        int i;
-        int j;
-        boolean[] dp;
-        int len;
-        int maxLen;
-        
-        len = s.length();
-        
-        if(len == 0){
-            return true;
-        }
-        
-        maxLen = 0;
-        for(String word : wordDict){
-            if(maxLen < word.length()){
-                maxLen = word.length();
-            }
-        }
-        
-        dp = new boolean[len + 1];
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int size = s.length();
+        boolean[] dp = new boolean[size + 1];
         dp[0] = true;
-        for(i = 0; i < len; ++i){
-            for(j = 1; j <= Math.min(i + 1, maxLen); ++j){
-                tail = s.substring(i - j + 1, i + 1);
-                if(wordDict.contains(tail) && (dp[i - j + 1] == true)){
-                    dp[i + 1] = true;
-                    break;
+        
+        for(int i = 0; i < size; ++i){
+            if(dp[i] != true){
+                continue;
+            }
+            else{
+                for(String word: wordDict){
+                    if(s.startsWith(word, i)){
+                        dp[i + word.length()] = true; 
+                    }
                 }
             }
         }
-        return dp[len];
+        
+        return dp[size];
     }
     
     public static void main(String[] args){
-		Solution sol;
+        Solution sol;
         String s;
-		Set<String> wordDict;
-		boolean canBroken;
+        List<String> wordDict;
+        boolean canBroken;
 
-       	s = "leetcode";
-		sol = new Solution();
- 		wordDict = new HashSet <String>();
-		wordDict.add("leet");
-		wordDict.add("code");
+        s = "leetcode";
+        sol = new Solution();
+        wordDict = new ArrayList <String>();
+        wordDict.add("leet");
+        wordDict.add("code");
 
-		canBroken = sol.wordBreak(s , wordDict);
+        canBroken = sol.wordBreak(s , wordDict);
 
         System.out.println("s: " + s);
         System.out.println("wrodDict: " + wordDict);
         System.out.println("canBorken: " + canBroken);
-		
-	}
+        
+    }
 }
