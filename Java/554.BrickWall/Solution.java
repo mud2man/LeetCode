f3f17a2018a9bd6c11a4a428fcf290ee33d9535f
Namo.Lu <chih-hung.lu@columbia.edu>
2018-02-06 11:54:10 -0800

    LeetCode#554 using Java

---
+++ Java/554.BrickWall/Solution.java
@@ -0,0 +1,52 @@
+/* Hash: Time:O(n), Space:O(n), where n is the number of bricks
+ * 1. Have a hashmap "edgeMap" to record the occurance of accumulated length row by row
+ * 2. Keep track the most occurance maxCount
+ * 3. Return wall.size() - maxCount
+ */
+
+import java.util.*;
+
+public class Solution{
+    public int leastBricks(List<List<Integer>> wall) {
+        if(wall == null || wall.size() == 0){
+            return 0;
+        }
+        
+        int maxCount = 0;
+        HashMap<Integer, Integer> edgeMap = new HashMap<Integer, Integer>();
+        int rowLength = 0;
+        
+        for(Integer num: wall.get(0)){
+            rowLength += num;
+        }
+        
+        for(List<Integer> row: wall){
+            int length = 0;
+            for(Integer num: row){
+                length += num;
+                if(length < rowLength){
+                    edgeMap.putIfAbsent(length, 0);
+                    edgeMap.put(length, edgeMap.get(length) + 1);
+                    maxCount = Math.max(edgeMap.get(length), maxCount); 
+                }
+            }
+        }
+        
+        return wall.size() - maxCount;
+    }
+ 
+    public static void main(String[] args){
+        Solution sol;
+        List<List<Integer>> wall = new ArrayList<List<Integer>>();
+        wall.add(Arrays.asList(1, 2, 2, 1));
+        wall.add(Arrays.asList(3, 1, 2));
+        wall.add(Arrays.asList(1, 3, 2));
+        wall.add(Arrays.asList(2, 4));
+        wall.add(Arrays.asList(3, 1, 2));
+        wall.add(Arrays.asList(1, 3, 1, 1));
+        
+        sol = new Solution();
+        System.out.println("wall: " + wall);
+        System.out.println("the least number of crossing bricks: " + sol.leastBricks(wall));
+    }
+}
