8f14c801c34ecc6d48030fc2d98d6ce4f9c7d0e9
Namo.Lu <chih-hung.lu@columbia.edu>
2018-04-05 21:44:54 -0700

    LeetCode#752 using Java

---
+++ Java/752.OpenLock/Solution.java
@@ -0,0 +1,73 @@
+/* BFS: Time:O(1), Space:O(1)
+ * 1. Have a hashset "visited" to record the visited status and deadends
+ * 2. Use BFS to find the shortest distance, without revisiting
+ */
+
+import java.util.*; // Stack
+
+public class Solution {
+    private List<String> getNextStatus(String currentStatus, HashSet<String> visited){
+        List<String> ret = new LinkedList<String>();
+        StringBuilder sb = new StringBuilder(currentStatus);
+        int[] shifts = {1, -1};
+        for(int i = 0; i < 4; ++i){
+            char c = sb.charAt(i);
+            for(int shift: shifts){
+                int digit = (c - '0' + shift) % 10;
+                digit = (digit == -1)? 9: digit;
+                sb.setCharAt(i, (char)(digit + '0'));
+                String nextStatus = sb.toString();
+                if(!visited.contains(nextStatus)){
+                    visited.add(nextStatus);
+                    ret.add(nextStatus);
+                }
+            }
+            sb.setCharAt(i, c);
+        }
+        return ret;
+    }
+    
+    public int openLock(String[] deadends, String target) {
+        HashSet<String> visited = new HashSet<String>();
+        for(String deadend: deadends){
+            if(deadend.equals("0000")){
+                return -1;
+            }
+            else{
+                visited.add(deadend); 
+            }
+            
+        }
+        visited.add("0000");
+        int distance = 0;
+        LinkedList<String> queue = new LinkedList<String>();
+        queue.add("0000");
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; ++i){
+                String currentStatus = queue.pollFirst();
+                visited.add(currentStatus);
+                if(currentStatus.equals(target)){
+                    return distance;
+                }
+                else{
+                    List<String> nextStatus = getNextStatus(currentStatus, visited);
+                    queue.addAll(nextStatus);
+                }
+            }
+            distance++;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        Solution sol;
+        String[] deadends = {"0201", "0101", "0102", "1212", "2002"};
+        String target = "0202";
+
+        sol = new Solution();
+        System.out.println("deadends: " + Arrays.toString(deadends));
+        System.out.println("target: " + target);
+        System.out.println("shortest distance: " + sol.openLock(deadends, target));
+    }
+}
