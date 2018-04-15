ccb5c566d7d9c51f076444623d2b34b27154f364
Namo.Lu <chih-hung.lu@columbia.edu>
2018-04-15 10:23:23 -0700

    LeetCode#678 using Java

---
+++ Java/678.ValidParenthesisString/Solution.java
@@ -0,0 +1,69 @@
+/* Time:O(n), Space:O(1). LeetCodes has one shot solution
+ * 1. Check if ')' valid by traversing from left, if count < 0, we can use startCount to shift the error
+ * 2. Check if '(' valid by traversing from right, if count < 0, we can use startCount to shift the error
+ */         
+
+import java.util.*;
+
+public class Solution {
+    public boolean checkValidString(String s) {
+        int starCount = 0;
+        int count = 0;
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            switch (c){
+                case '(':
+                    count++;
+                    break;
+                case ')':
+                    count--;
+                    break;
+                case '*':
+                    starCount++;
+                    break;
+            }
+            if(count < 0){
+                starCount--;
+                count++;
+                if(starCount < 0){
+                    return false;
+                }
+            }
+        }
+        
+        starCount = 0;
+        count = 0;
+        for(int i = s.length() - 1; i >= 0; i--){
+            char c = s.charAt(i);
+            switch (c){
+                case '(':
+                    count--;
+                    break;
+                case ')':
+                    count++;
+                    break;
+                case '*':
+                    starCount++;
+                    break;
+            }
+            if(count < 0){
+                starCount--;
+                count++;
+                if(starCount < 0){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+
+    public static void main(String[] args){
+        Solution sol;
+        String s = "(*))";
+        sol = new Solution();
+
+        System.out.println("s: " + s);
+        System.out.println("is valid ?: " + sol.checkValidString(s));
+    }
+}
