671a9ee066ddc3d49e1fbf4c973e99163f5e3ee8
Namo.Lu <chih-hung.lu@columbia.edu>
2018-04-19 21:32:29 -0700

    LeetCode#380 refine

--- Java/380.InsertDeleteGetRandom/RandomizedSet.java
+++ Java/380.InsertDeleteGetRandom/RandomizedSet.java
@@ -1,6 +1,7 @@
-/* LinkedList and HashMap,
+/* ArrayList and HashMap,
  * 1. Use HashMap to record positions
  * 2. In "remove" case, swap the last element to the position, which is gonna be removed, to maintain consistence of position map
+ * 3. Have a variable validLength to keep the length of the list. Only append the list when validLength == set.size()
  */
 
 import java.util.*;
@@ -8,22 +9,27 @@ import java.util.*;
 
 public class RandomizedSet {
     HashMap<Integer, Integer> map;
-    LinkedList<Integer> set;
+    ArrayList<Integer> set;
+    int validLength;
 
     /** Initialize your data structure here. */
     public RandomizedSet() {
         map = new HashMap<Integer, Integer>();
-        set = new LinkedList<Integer>();
+        set = new ArrayList<Integer>();
+        int validLength = 0;
     }
     
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public boolean insert(int val) {
-        int idx;
-        
         if(!map.containsKey(val)){
-            idx = set.size();
-            map.put(val, idx);
-            set.add(val);
+            if(validLength == set.size()){
+                set.add(val);
+                map.put(val, validLength++);
+            }
+            else{
+                map.put(val, validLength);
+                set.set(validLength++, val);
+            }
             return true;
         }
         else{
@@ -33,23 +39,31 @@ public class RandomizedSet {
     
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
-        int idx;
-        
         if(map.containsKey(val)){
-            idx = map.get(val);
-            set.set(idx, set.peekLast());
-            map.remove(set.peekLast());
-            map.put(set.peekLast(), idx);
-            set.pollLast();
+            int idx = map.get(val);
+            if(idx == (validLength - 1)){
+                validLength--;
+            }
+            else{
+                int lastValue = set.get(--validLength);
+                set.set(idx, lastValue);
+                map.put(lastValue, idx);
+            }
             map.remove(val);
             return true;
         }
         else{
             return false;
         }
-        
     }
     
+    /** Get a random element from the set. */
+    public int getRandom() {
+        Random rand = new Random();
+        int idx = rand.nextInt(validLength);
+        return set.get(idx);
+    }
+ 
     /** Get a random element from the set. */
     public int getRandom() {
         int idx;
