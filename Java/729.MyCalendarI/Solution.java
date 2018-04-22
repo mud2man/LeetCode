9203d3086eea73e1277aa9710c4a226b94236ccd
Namo.Lu <chih-hung.lu@columbia.edu>
2018-04-21 22:41:33 -0700

    LeetCode#729 refine

--- Java/729.MyCalendarI/Solution.java
+++ Java/729.MyCalendarI/Solution.java
@@ -5,54 +5,21 @@
 import java.util.*;
 
 public class Solution{
-    private class TreeNode{
-        TreeNode left;
-        TreeNode right;
-        int[] interval;
-        boolean isInterval;
-        TreeNode(){left = null; right = null; isInterval = false; interval = new int[2];}
-    }
-    TreeNode root;
-    
+    TreeMap<Integer, Integer> calendar;
     public Solution() {
-        root = new TreeNode();
-        root.interval[0] = Integer.MIN_VALUE;
-        root.interval[1] = Integer.MAX_VALUE;
+        calendar = new TreeMap<>();
     }
-    
-    private boolean insert(TreeNode parent, int start, int end){
-        if(parent.isInterval || parent.interval[0] >= start || parent.interval[1] <= end){
-            return false;
-        }
-    
-        int[] leftInterval = (parent.left != null)? parent.left.interval: new int[]{Integer.MIN_VALUE, Integer.MIN_VALUE};
-        int[] rightInterval = (parent.right != null)? parent.right.interval: new int[]{Integer.MAX_VALUE, Integer.MAX_VALUE};
-        
-        if(start >= leftInterval[1] && end <= rightInterval[0]){
-            TreeNode node = new TreeNode();
-            node.interval[0] = start;
-            node.interval[1] = end;
-            node.isInterval = true;
-            TreeNode interNode = new TreeNode();
-            interNode.interval[0] = leftInterval[0];
-            interNode.interval[1] = end;
-            interNode.right = node;
-            interNode.left = parent.left;
-            parent.left = interNode;
-            return true;
-        }
-        else if(end <= rightInterval[0]){
-            return insert(parent.left, start, end);
-        }
-        else{
-            return insert(parent.right, start, end);
-        }
-    }
-    
+
     public boolean book(int start, int end) {
-        return insert(root, start, end);
+        Integer floorKey = calendar.floorKey(start);
+        if (floorKey != null && calendar.get(floorKey) > start) return false;
+        Integer ceilingKey = calendar.ceilingKey(start);
+        if (ceilingKey != null && ceilingKey < end) return false;
+
+        calendar.put(start, end);
+        return true;
     }
-    
+
     public static void main(String[] args){
         Solution sol = new Solution();
         int[][] intervals = {{10, 20}, {15, 25}, {20, 30}};
