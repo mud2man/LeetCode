c6f8d3501dbd7e6313592e8cc5ccc034a51dfa39
Namo.Lu <chih-hung.lu@columbia.edu>
2018-04-23 20:25:44 -0700

    LeetCode#533 refine

--- Java/533.LonelyPixelII/Solution.java
+++ Java/533.LonelyPixelII/Solution.java
@@ -1,79 +1,47 @@
-/* Time:O(n*m*n), Space:O(n). LeetCode has better solution
- * 1. countInRow[i] = j means the number of 'B' on column i is j
- * 2. countInColumn[i] = j means the number of 'B' on row i is j
- * 3. columnToRow[i] = j means the first row sees 'B' on column i is j-th row. -1: not assign; -2: invalid column; others: valid
- * 4. rowToRow[i] = j means i-th row is the same as j-th row 
- * 5. Get countInRow and countInColumn
- * 6. Get columnToRow to learn the valid column
- * 7. Accumulate count only if countInRow[x] == N && countInColumn[y] == N && columnToRow[x] >= 0 (valid column) 
+/* Time:O(n*m), Space:O(n*m)
+ * 1. Retrieve the count of black pixel per column numberInColumn, and the rowToCount
+ * 2. Only the row of with count of black pixel is equal to N can put into rowToCount
+ * 3. Accumulate blackPixelCount with N only if the count of the row == N, and the numberInColumn[x]
  */
 
 import java.util.*;
 
 public class Solution{
-    private boolean isSameRow(int source, int target, char[][] picture){
-        for(int x = 0; x < picture[0].length; ++x){
-            if(picture[source][x] != picture[target][x]){
-                return false;
-            }
-        }
-        return true;
-    }
-    
     public int findBlackPixel(char[][] picture, int N) {
         int depth = picture.length;
         int width = picture[0].length;
-        int[] countInRow = new int[width];
-        int[] countInColumn = new int[depth];
-        int[] columnToRow = new int[width];
-        int[] rowToRow = new int[depth];
-        
-        for(int x = 0; x < width; ++x){
-            columnToRow[x] = -1;
-            for(int y = 0; y < depth; ++y){
-                if(picture[y][x] == 'B'){
-                    countInRow[x]++;
-                    countInColumn[y]++;
-                    rowToRow[y] = -1;
-                }
-            }
-        }
+        int[] numberInColumn = new int [width]; 
+        HashMap<String, Integer> rowToCount = new HashMap<String, Integer>();
         
         for(int y = 0; y < depth; ++y){
-            rowToRow[y] = y;
+            StringBuilder row = new StringBuilder("");
+            int count = 0;
             for(int x = 0; x < width; ++x){
-                if(picture[y][x] == 'B'){
-                    if(columnToRow[x] == -1){
-                        columnToRow[x] = y;
-                        continue;
-                    }
-                    
-                    if(rowToRow[y] < y || columnToRow[x] == -2){
-                        continue;
-                    }
-                    
-                    int row = columnToRow[x];
-                    int parentRow = rowToRow[row];
-                    if(isSameRow(parentRow, y, picture)){
-                        rowToRow[y] = parentRow;
-                    }
-                    else{
-                        columnToRow[x] = -2;
-                    }   
-                }
+                char c = picture[y][x];
+                numberInColumn[x] += (c == 'B')? 1: 0;
+                count += (c == 'B')? 1: 0;
+                row.append(c);
             }
+            if(count == N){
+                rowToCount.putIfAbsent(row.toString(), 0);
+                rowToCount.put(row.toString(), rowToCount.get(row.toString()) + 1);
+            } 
         }
-
-        int count = 0;
-        for(int y = 0; y < depth; ++y){
-            for(int x = 0; x < width; ++x){
-                if(picture[y][x] == 'B' && countInRow[x] == N && countInColumn[y] == N && columnToRow[x] >= 0){
-                    count++;
+        
+        int blackPixelCount = 0;
+        for(Map.Entry<String, Integer> entry: rowToCount.entrySet()){
+            String row = entry.getKey();
+            if(entry.getValue() != N){
+                continue;
+            }
+            else{
+                for(int i = 0; i < row.length(); ++i){
+                    blackPixelCount += (row.charAt(i) == 'B' && numberInColumn[i] == N)? N: 0;
                 }
             }
         }
         
-        return count;
+        return blackPixelCount;
     }
  
     public static void main(String[] args){
