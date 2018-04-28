7c5fdf69ffadfef02d83eda48c36e770926fe3b9
Namo.Lu <chih-hung.lu@columbia.edu>
2018-04-28 10:56:37 -0700

    LeetCode#651 udpate

--- Java/651.4KeysKeyboard/Solution.java
+++ Java/651.4KeysKeyboard/Solution.java
@@ -4,7 +4,7 @@
  * 3. Because if there is any A in the final sequence of dp[i], we can move A to the beginning of the final sequence.
  * 4. Then we can produce a bigger solution, which contradict that dp[i] is the biggest number
  * 5. dp[i] = (count + 1) * dp[j], where j decrease from (i - 3), and count increase from 1
- * 6. In matmmatics, the curve y = (x)*(constant - x) has only one local maximum, and it is global maximun too.
+ * 6. In mathmatics, the curve y = (x)*(constant - x) has only one local maximum, and it is global maximun too.
  * 7. So we can decrease j until (count + 1) * dp[j] starts to decrease
  */
 
@@ -21,8 +21,8 @@ public class Solution{
         for(int i = 6; i <= N; ++i){
             int j = i - 3;
             int count = 1;
-            while(dp[i] <= (count + 1) * dp[j]){
-                dp[i] = (count + 1) * dp[j];
+            while(dp[i] <= (count * dp[j] + dp[j])){
+                dp[i] = count * dp[j] + dp[j];
                 j--;
                 count++;
             }
