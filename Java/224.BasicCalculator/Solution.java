cd0628e205b00073ae22dad9eace6570799548cd
Namo.Lu <chih-hung.lu@columbia.edu>
2018-05-05 12:21:56 -0700

    LeetCode#224 refine

--- Java/224.BasicCalculator/Solution.java
+++ Java/224.BasicCalculator/Solution.java
@@ -1,103 +1,81 @@
-/* Stack: Time:O(n), Space:O(n), LeetCode has a shorter answer
- * 1. Have currInteger to track the current result
- * 2. Have operator to track the current opeartor
- * 3. Have nextInteger to track the next integer
- * 4. Have a stack to store the previous result in the outer parathesis
- * 5. Have a helper method operation to do caculation
+/* Stack: Time:O(n), Space:O(n)
+ * 1. Have a stack to store the result for each level, which has a initial value {0, +}
+ * 2. Delete all space from s, and have a new string "expression"
+ * 3. Traverse expression, and update stack according the current char
+ *
+ * ex: input: '(1+(4+5+2)-3)+(6+8)'
+ * time[0]: stack:{{0, +}}
+ * time[1]: stack:{{0, +}, {0, +}}, c = '('
+ * time[2]: stack:{{0, +}, {1, +}}, c = '+'
+ * time[3]: stack:{{0, +}, {1, +}, {0, +}}, c = '('
+ * time[4]: stack:{{0, +}, {1, +}, {4, +}}, c = '+'
+ * time[5]: stack:{{0, +}, {1, +}, {9, +}}, c = '+'
+ * time[6]: stack:{{0, +}, {12, +}}, c = ')'
+ * time[7]: stack:{{0, +}, {12, -}}, c = '-'
+ * time[8]: stack:{{9, +}}, c = ')'
+ * time[9]: stack:{{9, +}, {0, +}}, c = '('
+ * time[10]: stack:{{9, +}, {6, +}}, c = '+'
+ * time[11]: stack:{{23, +}}, c = ')'
  */
 
 import java.util.*;
 
 public class Solution{
-    //after operation, reset operator and nextInteger
-    private StringBuilder operation(StringBuilder currInteger, StringBuilder operator, StringBuilder nextInteger){
-        StringBuilder answer;
-
-        if((operator.length() == 0 || nextInteger.length() == 0) && currInteger.length() > 0){
-            answer = currInteger;
-        }
-        else if(nextInteger.length() > 0 && currInteger.length() == 0){
-            answer = new StringBuilder(nextInteger);
-            nextInteger.delete(0, nextInteger.length());
-        }
-        else if(operator.length() == 0 && nextInteger.length() == 0 && currInteger.length() == 0){
-            answer = currInteger;
-        }
-        else{
-            int leftOperand = Integer.parseInt(currInteger.toString());
-            int rightOperand = Integer.parseInt(nextInteger.toString());
-            if(operator.toString().equals("+")){
-                int result = leftOperand + rightOperand;
-                answer = new StringBuilder(Integer.toString(result));
-            }
-            else{
-                int result = leftOperand - rightOperand;
-                answer = new StringBuilder(Integer.toString(result));
-            }
-            operator.delete(0, operator.length());
-            nextInteger.delete(0, nextInteger.length());
-        }
-        return answer;
+    private class Node{
+        int left;
+        char operator;
+        Node(int l, char o){left = l; operator = o;}
+    }
+    
+    private void operate(Stack<Node> stack, int right){
+        Node top = stack.peek();
+        top.left = (top.operator == '+')? top.left + right: top.left - right;
     }
     
     public int calculate(String s) {
-        Stack<String> stack = new Stack<String>();
-        StringBuilder nextInteger = new StringBuilder("");
-        StringBuilder currInteger = new StringBuilder("");
-        StringBuilder operator = new StringBuilder("");
-        
+        StringBuilder expression = new StringBuilder("");
         for(int i = 0; i < s.length(); ++i){
-            char c = s.charAt(i);
-            if(Character.isDigit(c)){
-                nextInteger.append(c);
-            }
-            else if(c == '+' || c == '-'){
-                if(currInteger.length() == 0){
-                    operator = new StringBuilder("");
-                    operator.append(c);
-                    currInteger = nextInteger;
-                    nextInteger = new StringBuilder("");
-                }
-                else{
-                    currInteger = operation(currInteger, operator, nextInteger);
-                    operator = new StringBuilder("");
-                    operator.append(c);
-                }
-            }
-            else if(c == ' '){
-                currInteger = operation(currInteger, operator, nextInteger);
-            }
-            else if(c == '('){
-                if(currInteger.length() != 0){
-                    stack.push(currInteger.toString());
-                    currInteger = new StringBuilder("");
-                }
-                
-                if(operator.length() != 0){
-                    stack.push(operator.toString());
-                    operator = new StringBuilder("");
-                } 
-            }
-            else{
-                currInteger = operation(currInteger, operator, nextInteger);
-                if(!stack.isEmpty()){
-                    nextInteger = currInteger;
-                    String top = stack.pop().toString();
-                    if(top.equals("+") || top.equals("-")){
-                        operator = new StringBuilder(top);
-                        currInteger = operation(new StringBuilder(stack.pop()), operator, nextInteger);
-                    }
-                    else{
-                        currInteger = new StringBuilder(top);
-                    } 
-                }
+            char c = s.charAt(i); 
+            if(c != ' '){
+                expression.append(c);
             }
         }
-
-        currInteger = operation(currInteger, operator, nextInteger);
-        return Integer.parseInt(currInteger.toString());
+        
+        Stack<Node> stack = new Stack<Node>();
+        stack.push(new Node(0, '+'));
+        int right = 0;
+        for(int i = 0; i < expression.length(); ++i){
+            char c = expression.charAt(i); 
+            switch (c){
+                case '(':
+                    stack.push(new Node(0, '+'));
+                    break;
+                case ')':
+                    operate(stack, right);
+                    right = stack.pop().left;
+                    break;
+                case '+':
+                    operate(stack, right);
+                    right = 0;
+                    stack.peek().operator = '+';
+                    break;
+                case '-':
+                    operate(stack, right);
+                    right = 0;
+                    stack.peek().operator = '-';
+                    break;
+                default:
+                    right = right * 10 + (c - '0');
+                    
+            }
+        }
+        
+        if(right != 0){
+            operate(stack, right);
+        }
+        return stack.pop().left;
     }
- 
+  
     public static void main(String[] args){
         Solution sol;
         String s = "(1+(4+5+2)-3)+(6+8)";
