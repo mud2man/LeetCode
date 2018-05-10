15b752b03876c42d9ebe5d4f65c231097f0b79c3
Namo.Lu <chih-hung.lu@columbia.edu>
2018-05-09 21:57:10 -0700

    LeetCode#689 refine

--- Java/689.MaximumSum3NonOverlappingSubarrays/Solution.java
+++ Java/689.MaximumSum3NonOverlappingSubarrays/Solution.java
@@ -1,16 +1,9 @@
 /* Dynamic programming: Time:O(n), Space:O(n)
- * 1. Compute the array "subArrays" of k-subarray's sum
- * 2. Compute the "maxLeftSubArray", where maxLeftSubArrays[i] = max(subArrays[0], subArrays[1], ..., subArrays[i])
- * 3. Compute the "maxRightSubArrays", where maxRightSubArrays[i] = max(subArrays[i], subArrays[i + 1], ..., subArrays[length - 1])
- * 4. Traverse all the valid 2nd subarrays, get the maximum three sums, and determine the 2nd index of subarray
- * 5. Now, we have the left sum and the right sum. So we can traverse from left to find the lexicographically smallest index
+ * 1. dp[i][j] = the maximum sum of i size-k subarray
+ * 2. record[i][j] = the starting index of i-th subarray, where has maximum sum considering the nums[0, 1, ..., j]
+ * 3. Udpate dp[i][j] if (sum + dp[i - 1][j - k] > dp[i][j - 1])
+ * 4. For convience, we use 4D array to avoid special case of first subarray
  * 
- * ex: nums[]              = {1, 2, 1, 2, 6, 7, 5, 1}, k = 2
- *     subArrays[]         = {3, 3, 3, 8, 13, 12, 6}
- *     maxLeftSubArrays[]  = {3, 3, 3, 8, 13, 13, 13}
- *     maxRightSubArrays[] = {13, 13, 13, 13, 13, 12, 6}
- *     valid index of 2nd sub-array is {2, 3, 4}
- *     maxThreeSum = max{(3 + 3 + 13), (3 + 8 + 12), (3 + 13 + 6)} => 2nd index is 3 
  */
 
 import java.util.*;
@@ -18,64 +11,42 @@ import java.util.*;
 public class Solution{
     public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
         int length = nums.length;
-        int[] subArrays = new int[length - k + 1];
+        int[][] dp = new int[4][length];
+        int[][] record = new int[4][length];
         
-        int sum = 0;
-        for(int i = 0; i < (k - 1); ++i){
-            sum += nums[i];
-        }
-        
-        for(int i = 0; i < subArrays.length; ++i){
-            sum += nums[k + i - 1];
-            subArrays[i] = sum;
-            sum -= nums[i];
-        }
-        
-        int[] maxLeftSubArrays = new int[length - k + 1];
-        int maxLeftSubArray = 0;
-        for(int i = 0; i < maxLeftSubArrays.length; ++i){
-            maxLeftSubArray = Math.max(maxLeftSubArray, subArrays[i]);
-            maxLeftSubArrays[i] = maxLeftSubArray;
-        }
-        
-        int[] maxRightSubArrays = new int[length - k + 1];
-        int maxRightSubArray = 0;
-        for(int i = maxRightSubArrays.length - 1; i >= 0; --i){
-            maxRightSubArray = Math.max(maxRightSubArray, subArrays[i]);
-            maxRightSubArrays[i] = maxRightSubArray;
-        }
-        
-        int maxThreeSum = 0;
-        int[] threeSubArrays = new int[3];
-        for(int i = k; i <= (length - 2 * k); ++i){
-            int currentThreeSum = subArrays[i] + maxLeftSubArrays[i - k] + maxRightSubArrays[i + k];
-            if(maxThreeSum < currentThreeSum){
-                threeSubArrays[0] = i - k;
-                threeSubArrays[1] = i;
-                threeSubArrays[2] = i + k;
-                maxThreeSum = currentThreeSum;
+        for(int i = 1; i <= 3; ++i){
+            int start = (i - 1) * k;
+            int end = i * k;
+            int sum = 0;
+            for(int j = start; j < end; ++j){
+                sum += nums[j];
+            }
+            
+            dp[i][end - 1] = (i > 1)? dp[i - 1][start - 1] + sum: sum; 
+            record[i][end - 1] = (i - 1) * k;
+            for(int j = end; j < length; ++j){
+                sum = sum - nums[j - k] + nums[j];  
+                if(sum + dp[i - 1][j - k] > dp[i][j - 1]){
+                    dp[i][j] = sum + dp[i - 1][j - k];
+                    record[i][j] = j - k + 1;
+                }
+                else{
+                    dp[i][j] = dp[i][j - 1];
+                    record[i][j] = record[i][j - 1];
+                }
             }
         }
-        
-        int left = maxLeftSubArrays[threeSubArrays[0]];
-        int right = maxRightSubArrays[threeSubArrays[2]];
-        for(int i = 0; i <= threeSubArrays[0]; ++i){
-            if(left == subArrays[i]){
-                threeSubArrays[0] = i;
-                break;
-            }
+      
+        int[] ranges = new int[3];
+        int index = length - 1;
+        for(int y = 3; y > 0; --y){
+            ranges[y - 1] = record[y][index];
+            index = record[y][index] - 1;
         }
         
-        for(int i = threeSubArrays[2]; i < subArrays.length; ++i){
-            if(right == subArrays[i]){
-                threeSubArrays[2] = i;
-                break;
-            }
-        }
-        
-        return threeSubArrays;
+        return ranges;
     }
- 
+  
     public static void main(String[] args){
         Solution sol;
         List<List<Integer>> subsets;
