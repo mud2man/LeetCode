b8079d746f22c6e4b5a0393fdf0d36e8d282a98c
Namo.Lu <chih-hung.lu@columbia.edu>
2018-05-10 21:46:21 -0700

    LeetCode#128 refine

--- Java/128.LongestConsecutiveSequence/Solution.java
+++ Java/128.LongestConsecutiveSequence/Solution.java
@@ -1,5 +1,5 @@
 /* Hash: Time:O(n), Space:O(n). LeetCode has simpler solution
- * 1. Keep a hashMap "lengthsMap" to store the mapping between node and length. e.g.,{1, 2, 3, 4, 5}, map.get(1) = 1, map.get(5) = 5
+ * 1. Keep a hashMap "lengthsMap" to store the mapping between node and length. e.g.,{1, 2, 3, 4, 5}, map.get(1) = 5, map.get(5) = 5
  * 2. In each loop, check if the new node can be attached to the begin or end of existing sequence
  * 3. Then, update lengthsMap and maxLength
  * 4. Because we prevent duplicated, the nodes on the "leftLength" and "rightLength" will not overlap
@@ -20,14 +20,7 @@ public class Solution {
                 int leftLength = lengthsMap.containsKey(num - 1)? lengthsMap.get(num - 1): 0;
                 int rightLength = lengthsMap.containsKey(num + 1)? lengthsMap.get(num + 1): 0;
                 int sum = leftLength + rightLength + 1;
-                
-                if(leftLength == 0 || rightLength == 0){
-                    lengthsMap.put(num, sum);
-                }
-                else {
-                    lengthsMap.put(num, 0);
-                }
-
+                lengthsMap.put(num, 0);
                 lengthsMap.put(num - leftLength, sum);
                 lengthsMap.put(num + rightLength, sum);
                 maxLength = Math.max(maxLength, sum);
