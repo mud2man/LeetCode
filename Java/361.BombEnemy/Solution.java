2bee619688feeb7992869598bf1aad9b65a06178
Namo.Lu <chih-hung.lu@columbia.edu>
2018-05-19 00:00:10 -0700

    LeetCode#361 refine

--- Java/361.BombEnemy/Solution.java
+++ Java/361.BombEnemy/Solution.java
@@ -1,106 +1,51 @@
-/* O(m*n)
- * 1. Caculate the enemy# from left, top, right, and bottom
- * 2. enemy[y][x] =  fromLeft[y][x - 1] + fromTop[y - 1][x] + fromRight[y][x + 1] + fromBottom[y + 1][x]
+/* Time:O(m*n), Space:O(m*n)
+ * 1. Have a 3D array dp, where dp[y][x][0] = row hit on (y, x), dp[y][x][1] = column hit on (y, x)
+ * 2. Because for every position (y, x), it only contributed on row count or column count once.
+ * 3. Therefore the amortized complexity is O(m*n)
  */
 
 import java.util.*;
 
 public class Solution{
     public int maxKilledEnemies(char[][] grid) {
-        int height = grid.length;
-        int width = (height > 0) ? grid[0].length: 0;
-        int[][] fromLeft = new int[height][width];
-        int[][] fromTop = new int[height][width];
-        int[][] fromRight = new int[height][width];
-        int[][] fromBottom = new int[height][width];
+        int depth = grid.length;
+        int width = (depth > 0)? grid[0].length: 0;
+        int[][][] dp = new int[depth][width][2];
+        int maxHit = 0;
         
-        // accumulate from left
-        for(int y = 0; y < height; ++y){
+        for(int y = 0; y < depth; ++y){
             for(int x = 0; x < width; ++x){
                 char c = grid[y][x];
-                switch (c){
-                    case 'W':
-                        fromLeft[y][x] = 0;
-                        break;
-                    case 'E':
-                        fromLeft[y][x] = (x > 0)? fromLeft[y][x - 1] + 1: 1;
-                        break;
-                    default:
-                        fromLeft[y][x] = (x > 0)? fromLeft[y][x - 1]: 0;
-                        break;
+                if(c == 'E' || c == '0'){
+                    if(dp[y][x][0] == 0){
+                        int rowHit = 0;
+                        for(int i = x; i < width && grid[y][i] != 'W'; ++i){
+                            rowHit += (grid[y][i] == 'E')? 1: 0;
+                        }
+                        
+                        for(int i = x; i < width && grid[y][i] != 'W'; ++i){
+                            dp[y][i][0] = rowHit;
+                        }
+                    }
+                    
+                    if(dp[y][x][1] == 0){
+                        int columnHit = 0;
+                        for(int i = y; i < depth && grid[i][x] != 'W'; ++i){
+                            columnHit += (grid[i][x] == 'E')? 1: 0;
+                        }
+                        
+                        for(int i = y; i < depth && grid[i][x] != 'W'; ++i){
+                            dp[i][x][1] = columnHit;
+                        }
+                    }
+                    
+                    if(c == '0'){
+                        maxHit = Math.max(dp[y][x][0] + dp[y][x][1], maxHit);
+                    }
                 }
             }
         }
-        
-        // accumulate from top
-        for(int x = 0; x < width; ++x){
-            for(int y = 0; y < height; ++y){
-                char c = grid[y][x];
-                switch (c){
-                    case 'W':
-                        fromTop[y][x] = 0;
-                        break;
-                    case 'E':
-                        fromTop[y][x] = (y > 0)? fromTop[y - 1][x] + 1: 1;
-                        break;
-                    default:
-                        fromTop[y][x] = (y > 0)? fromTop[y - 1][x]: 0;
-                        break;
-                }
-            }
-        }
-        
-        // accumulate from right
-        for(int y = 0; y < height; ++y){
-            for(int x = width - 1; x >= 0; --x){
-                char c = grid[y][x];
-                switch (c){
-                    case 'W':
-                        fromRight[y][x] = 0;
-                        break;
-                    case 'E':
-                        fromRight[y][x] = (x < width - 1)? fromRight[y][x + 1] + 1: 1;
-                        break;
-                    default:
-                        fromRight[y][x] = (x < width - 1)? fromRight[y][x + 1]: 0;
-                        break;
-                }
-            }
-        }
-        
-        // accumulate from bottom
-        for(int x = 0; x < width; ++x){
-            for(int y = height - 1; y >= 0; --y){
-                char c = grid[y][x];
-                switch (c){
-                    case 'W':
-                        fromBottom[y][x] = 0;
-                        break;
-                    case 'E':
-                        fromBottom[y][x] = (y < height - 1)? fromBottom[y + 1][x] + 1: 1;
-                        break;
-                    default:
-                        fromBottom[y][x] = (y < height - 1)? fromBottom[y + 1][x]: 0;
-                        break;
-                }
-            }
-        }
-        
-        int maxCount = 0;
-        for(int y = 0; y < height; ++y){
-            for(int x = 0; x < width; ++x){
-                char c = grid[y][x];
-                if(c == '0'){
-                    int countLeft = (x > 0)? fromLeft[y][x - 1]: 0;
-                    int countTop = (y > 0)? fromTop[y - 1][x]: 0;
-                    int countRight = (x < width - 1)? fromRight[y][x + 1]: 0;
-                    int countBottom = (y < height - 1)? fromBottom[y + 1][x]: 0;
-                    maxCount = Math.max(maxCount, countLeft + countTop + countRight + countBottom);
-                }
-            }
-        }
-        
-        return maxCount;
+        return maxHit;
     }
 
     public static void main(String[] args){
