468462217fc83e87f2e88f563a372b0b6a05f46a
Namo.Lu <chih-hung.lu@columbia.edu>
2018-05-19 15:30:48 -0700

    leetCode#548 using Java

---
+++ Java/548.SplitArrayEqualSum/Solution.java
@@ -0,0 +1,45 @@
+/* HashSet: Time:O(n), Space:O(1)
+ * 1. Iterate j, and iterate i, then put the equal sums on left part into the set "lefts"
+ * 2. Iterate k, if find equals sums, check if it in the lefts. If so return true
+ */         
+
+import java.util.*;
+
+public class Solution {
+    public boolean splitArray(int[] nums) {
+        int length = nums.length;
+        int[] sum = new int[length];
+        
+        for(int i = 0; i < length; ++i){
+            sum[i] = (i > 0)? nums[i] + sum[i - 1]: nums[i];
+        }
+        
+        for(int j = 3; j <= length - 4; ++j){
+            int leftSum = sum[j - 1];
+            HashSet<Integer> lefts = new HashSet<Integer>();
+            for(int i = 1; i < j - 1; ++i){
+                if(sum[i - 1] == (leftSum - sum[i])){
+                    lefts.add(sum[i - 1]);
+                }
+            }
+            
+            for(int k = j + 2; k < length - 1; ++k){
+                if((sum[k - 1] - sum[j]) == (sum[length - 1] - sum[k])){
+                    if(lefts.contains(sum[k - 1] - sum[j])){
+                        return true;
+                    }
+                }
+            }
+        }
+        
+        return false;
+    }
+
+    public static void main(String[] args){
+        Solution sol; 
+        int[] nums = {1, 2, 1, 2, 1, 2, 1};
+        sol = new Solution();
+        System.out.println("nums: " + Arrays.toString(nums));
+        System.out.println("can be splitted: " + sol.splitArray(nums));
+    }
+}
