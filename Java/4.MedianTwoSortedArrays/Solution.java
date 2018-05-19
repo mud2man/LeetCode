2d12b9e3540fac2c8d536d73146d356b62d44793
Namo.Lu <chih-hung.lu@columbia.edu>
2018-05-19 11:24:22 -0700

    LeetCode#4 Refine

--- Java/4.MedianTwoSortedArrays/Solution.java
+++ Java/4.MedianTwoSortedArrays/Solution.java
@@ -1,65 +1,58 @@
 /* binary search: Time:O(log(m + n)), Space:O(1)
- * 1. Let midIndex be the next index of of left part in small, and bigIndex the he next index of of left part in big
- * 2. Decrease hb if (midIndex > 0 && small[midIndex - 1] > big[bigIndex])
- * 3. Increase lb if (midIndex < small.length && big[bigIndex - 1] > small[midIndex])
- * 4. Otherwise, midIndex found, the find the maximun of the left part
- * 5. Then, find the minimum of the right part
- * 6. If size is odd, return maxLeft, otherwise return (maxLeft + minRight)/2
+ * 1. bigRight: the smallest element of right part in "big" array
+ * 2. bigLeft: the biggest element of left part in "big" array
+ * 3. smallRight: the smallest element of right part in "small" array
+ * 4. smallLeft: the biggest element of left part in "small" array
+ * 5. Increase lb if bigRight < smallLeft
+ * 6. Decrease hb if bigLeft > smallRight
+ * 7. Otherwise, pick the smaller between bigRight and smallRight, bigger between bigLeft and smallLeft
  */
 
 import java.util.*;
 
 public class Solution{
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] big = (nums1.length > nums2.length)? nums1: nums2;
-        int[] small = (nums1.length <= nums2.length)? nums1: nums2;
-        int size = nums1.length + nums2.length;
-        int halfSize = (size + 1) / 2;
+        int[] big = null;
+        int[] small = null;
+        if(nums1.length <= nums2.length){
+            big = nums1;
+            small = nums2;
+        }
+        else{
+            big = nums2;
+            small = nums1;
+        }
         
-        //binary search
         int lb = 0;
-        int hb = small.length;
+        int hb = big.length;
+        int halfSize = (big.length + small.length) / 2;
         while(lb <= hb){
-            int midIndex = (lb + hb) / 2;
-            int bigIndex = halfSize - midIndex;
-            if(midIndex > 0 && small[midIndex - 1] > big[bigIndex]){
-                hb = midIndex - 1;
+            int mid = (lb + hb) / 2;
+            int bigRight = (mid == big.length)? Integer.MAX_VALUE: big[mid];
+            int smallLeft = ((halfSize - mid - 1) < 0)? Integer.MIN_VALUE: small[halfSize - mid - 1];
+            int bigLeft = (mid > 0)? big[mid - 1]: Integer.MIN_VALUE;
+            int smallRight = ((halfSize - mid) == small.length)? Integer.MAX_VALUE: small[halfSize - mid];
+            
+            if(bigRight < smallLeft){
+                lb = mid + 1;
             }
-            else if(midIndex < small.length && big[bigIndex - 1] > small[midIndex]) {
-                lb = midIndex + 1;
+            else if(bigLeft > smallRight){
+                hb = mid - 1;
             }
             else{
-                int maxLeft = 0;
-                if(midIndex == 0){
-                    maxLeft = big[halfSize - 1];
-                }
-                else if(bigIndex == 0){
-                    maxLeft = small[halfSize - 1];
+                int right = Math.min(bigRight, smallRight);
+                int left = Math.max(bigLeft, smallLeft);
+                if((nums1.length + nums2.length) % 2 == 1){
+                    return (double)right;
                 }
                 else{
-                    maxLeft = Math.max(small[midIndex - 1], big[bigIndex - 1]);
+                    return (double)(right + left) / 2.0;
                 }
-                
-                if(size % 2 == 1){
-                    return maxLeft;
-                }
-                
-                int minRight = 0;
-                if(midIndex == small.length){
-                    minRight = big[halfSize - small.length];
-                }
-                else if(bigIndex == big.length){
-                    minRight = small[halfSize - big.length];
-                }
-                else{
-                    minRight = Math.min(small[midIndex], big[bigIndex]);
-                }
-                
-                return (double)(maxLeft + minRight) / 2.0;
             }
         }
-        return 0.0;   
-    }  
+        return 0.0;
+    }
+ 
     public static void main(String[] args){
         int[] nums1 = {1, 2};
         int[] nums2 = {3, 4};
