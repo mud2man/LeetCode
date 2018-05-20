cb5a1b0a63b466773c043ebca367aeeefa3617a1
Namo.Lu <chih-hung.lu@columbia.edu>
2018-05-19 18:21:59 -0700

    LeetCode#460 refine

--- Java/460.LFUCache/LFUCache.java
+++ Java/460.LFUCache/LFUCache.java
@@ -1,114 +1,72 @@
-/* Circular queue, HashMap: TIme:O(1), Space:O(n), where n is the capacity. Leetcodes has a shorter answer
- * 1. Have a circular queue to record all the node for eavery frequency
- * 2. Have a hashmap "frequency2Tail" to record the latest node for every frequency
- * 3. Have a hashmap "key2Index" to record the mapping between key and node index
- * 4. When adding, add the new node on the tail of the circular queue, which recorded by frequency2Tail
- * 5. When deleting, delete the head of the circular queue, which is the next of tail 
+/* Circular queue, HashMap: TIme:O(1), Space:O(n)
+ * 1. Have a hashmap "freq2Keys" to record all the keys for eavery frequency
+ * 2. Have a hashmap "key2Freq" to record key-frequency pair
+ * 3. Have a hashmap "key2Val" to record key-value pair
+ * 4. Have minFreq and remain to record the minimum frequecy, and remaining count
+ * 5. When delete the least recently element, we can use iterator to delete the first element
  */
 
 import java.util.*;
 
 public class LFUCache {
-    private class Node{
-        int next;
-        int previous;
-        int value;
-        int key;
-        int frequency;
-        Node(int k, int v){key = k; value = v;}
-    }
-    Node[] queues;
-    HashMap<Integer, Integer> key2Index;
-    HashMap<Integer, Integer> frequency2Tail;
-    int leastFrequency;
-    int availableIndex;
-    int capacity;
+    int minFreq;
+    int remain;
+    HashMap<Integer, Integer> key2Val;
+    HashMap<Integer, Integer> key2Freq;
+    HashMap<Integer, LinkedHashSet<Integer>> freq2Keys;
     
     public LFUCache(int capacity) {
-        this.capacity = capacity;
-        this.availableIndex = 0;
-        this.leastFrequency = -1;
-        this.frequency2Tail = new HashMap<Integer, Integer>();
-        this.key2Index = new HashMap<Integer, Integer>();
-        this.queues = new Node[capacity];
-    }
-    
-    private int delete(int key){
-        int index = key2Index.get(key);
-        if(queues[index].next == index){
-            leastFrequency = (leastFrequency == queues[index].frequency)? -1: leastFrequency;
-            frequency2Tail.remove(queues[index].frequency);
-        }
-        else{
-            if(index == frequency2Tail.get(queues[index].frequency)){
-                frequency2Tail.put(queues[index].frequency, queues[index].previous);
-            }
-            int previous = queues[index].previous;
-            int next = queues[index].next;
-            queues[previous].next = next;
-            queues[next].previous = previous;
-        }
-        key2Index.remove(key);
-        return index;
-    }
-    
-    private void add(int index, int key, int value, int frequency){
-        leastFrequency = (leastFrequency == -1)? frequency: Math.min(leastFrequency, frequency);
-        queues[index].key = key;
-        queues[index].value = value;
-        queues[index].frequency = frequency;
-        queues[index].next = index;
-        queues[index].previous = index;
-        key2Index.put(key, index);
-        frequency2Tail.putIfAbsent(frequency, index);
-        if(frequency2Tail.get(frequency) != index){
-            int previous = frequency2Tail.get(frequency);
-            int next = queues[previous].next;
-            queues[previous].next = index;
-            queues[next].previous = index;
-            queues[index].next = next;
-            queues[index].previous = previous;
-        }
-        frequency2Tail.put(frequency, index);
-    }
-    
-    private void update(int key){
-        int index = key2Index.get(key);
-        delete(key);
-        add(index, key, queues[index].value, queues[index].frequency + 1);
+        this.remain = capacity;
+        this.minFreq = 0;
+        this.key2Val = new HashMap<Integer, Integer>();
+        this.key2Freq = new HashMap<Integer, Integer>();
+        this.freq2Keys = new HashMap<Integer, LinkedHashSet<Integer>>();
     }
     
     public int get(int key) {
-        if(capacity > 0 && key2Index.containsKey(key)){
-            update(key);
-            return queues[key2Index.get(key)].value;
+        if(!key2Val.containsKey(key)){
+            return -1;
         }
         else{
-            return -1;
+            int freq = key2Freq.get(key);
+            key2Freq.put(key, freq + 1);
+            LinkedHashSet<Integer> keys = freq2Keys.get(freq);
+            keys.remove(key);
+            if(keys.isEmpty() && minFreq == freq){
+                minFreq = freq + 1;
+            }
+            freq++;
+            freq2Keys.putIfAbsent(freq, new LinkedHashSet<Integer>());
+            freq2Keys.get(freq).add(key);
+            return key2Val.get(key);
         }
     }
     
     public void put(int key, int value) {
-        if(capacity <= 0){
-            return;
-        }
-        
-        if(key2Index.containsKey(key)){
-            queues[key2Index.get(key)].value = value;
+        if(key2Val.containsKey(key)){
+            key2Val.put(key, value);
             get(key);
         }
         else{
-            if(availableIndex < capacity){
-                queues[availableIndex] = new Node(key, value);
-                add(availableIndex, key, value, 1);
-                availableIndex++;
+            if(this.remain > 0){
+                this.remain--;
             }
             else{
-                int tailIndex = frequency2Tail.get(leastFrequency);
-                int deleteKey = queues[queues[tailIndex].next].key;
-                int deleteIndex = delete(deleteKey);
-                add(deleteIndex, key, value, 1);
+                LinkedHashSet<Integer> keys = freq2Keys.get(this.minFreq);
+                if(keys == null || keys.isEmpty()){
+                    return;
+                }
+                Iterator<Integer> itr = keys.iterator();
+                int removeKey = itr.next();
+                itr.remove();
+                key2Val.remove(removeKey);
+                key2Freq.remove(removeKey);
             }
+            key2Val.put(key, value);
+            key2Freq.put(key, 1);
+            freq2Keys.putIfAbsent(1, new LinkedHashSet<Integer>());
+            freq2Keys.get(1).add(key);
+            this.minFreq = 1;
         }
     }
 
