b74fca62769ebbd15f141d3d161553ce21e26196
Namo.Lu <chih-hung.lu@columbia.edu>
2018-06-03 20:21:00 -0700

    LeetCode#845 using Java

--- Java/845.LongestMountainArray/Solution.java
+++ Java/845.LongestMountainArray/Solution.java
@@ -1,25 +1,32 @@
-/* Dynamic programming: Time:O(n), Space:O(n). However, LeetCode has one-shot O(1) space solution
- * 1. Get the longest left slope and store in dp[i][0], and right slope in dp[i][1]
- * 2. Traverse dp and get the longest mountain
+/* Time:O(n), Space:O(1)
+ * 1. Have a variable mode (0:flat, 1:increase, 2:decrease) to record previous status
+ * 2. Update left slope and right slop, then update "ret" only if (leftSlope > 0 && rightSlope > 0)
  */
 
 import java.util.*;
 
 public class Solution{
     public int longestMountain(int[] A) {
-        int length = A.length;
-        int[][] dp = new int[length][2];
-        for(int i = 1; i < length; ++i){
-            dp[i][0] = (A[i] > A[i - 1])? Math.max(2, dp[i - 1][0] + 1): 0;
-        }
-        
-        for(int i = length - 2; i > -1; --i){
-            dp[i][1] = (A[i] > A[i + 1])? Math.max(2, dp[i + 1][1] + 1): 0;
-        }
-        
         int ret = 0;
-        for(int i = 1; i < length - 1; ++i){
-            ret = (dp[i][0] > 0 && dp[i][1] > 0)? Math.max(ret, dp[i][0] + dp[i][1] - 1): ret;
+        int leftSlope = 0;
+        int rightSlope = 0;
+        int mode = 0; //0:flat, 1:increase, 2:decrease
+        for(int i = 1; i < A.length; ++i){
+            if(A[i] > A[i - 1]){
+                leftSlope = (mode != 1)? 2: leftSlope + 1;
+                rightSlope = 0;
+                mode = 1;
+            }
+            else if(A[i] < A[i - 1]){
+                rightSlope = (mode != 2)? 2: rightSlope + 1;
+                mode = 2;
+            }
+            else{
+                leftSlope = 0;
+                rightSlope = 0;
+                mode = 0;
+            }
+            ret = (leftSlope > 0 && rightSlope > 0)? Math.max(ret, leftSlope + rightSlope - 1): ret;
         }
         return ret;
     }
