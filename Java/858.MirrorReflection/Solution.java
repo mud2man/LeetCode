2901d08d96a18c192035cd70b156dafb9571a9ef
Namo.Lu <chih-hung.lu@columbia.edu>
2018-06-26 12:21:23 -0700

    LeetCode#858 update

--- Java/858.MirrorReflection/Solution.java
+++ Java/858.MirrorReflection/Solution.java
@@ -1,7 +1,7 @@
 /* Math: Time:O(logp), Space:O(1)
  * 1. Imagine the room has no ceil, the left wall and right wall extends infinitely
  * 2. When the ray hit the receiver, the distance in y direction must multiple of p
- * 3. So we can get the least common multiple between p and q
+ * 3. So we can get the least common multiple (L.C.M.) between p and q
  * 4. If (lcm / q) % 2 == 1, it mean the ray hit the receiver of right wall, then we can determine which one is hitted by (lcm / p) % 2 == 1
  * 5. Otherwise, it hit receiver#2
  */         
@@ -9,21 +9,21 @@
 import java.util.*;
 
 public class Solution {
-    private int getGcf(int p, int q){
+    private int getGcd(int p, int q){
         int big = (p > q)? p: q;
         int small = (p > q)? q: p;
         if(big % small == 0){
             return small;
         }
         else{
-            return getGcf(small, big % small);
+            return getGcd(small, big % small);
         }
     }
     
     public int mirrorReflection(int p, int q) {
-        int gcf = getGcf(p, q);
+        int gcd = getGcd(p, q);
         int num = 0;
-        int lcm = gcf * (p / gcf) * (q / gcf);
+        int lcm = gcd * (p / gcd) * (q / gcd);
         //right wall
         if((lcm / q) % 2 == 1){
             num = ((lcm / p) % 2 == 1)? 1: 0;
