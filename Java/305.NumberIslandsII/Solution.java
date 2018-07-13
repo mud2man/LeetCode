903d128b86d7befabec5576a8966f841bba36e41
Namo.Lu <chih-hung.lu@columbia.edu>
2018-07-12 22:03:56 -0700

    LeetCode#305

--- Java/305.NumberIslandsII/Solution.java
+++ Java/305.NumberIslandsII/Solution.java
@@ -1,6 +1,6 @@
 /* Union and find: Time:O(klog(m*n)), Space:O(k). LeetCode also apply union by rank, you should learn it
- * 1. Have a map child2Parent to store the child-parent mapping 
- * 2. Traverse "positions" and apply union and find to update islands count
+ * 1. Have a map child2Parent to store the child-parent mapping, and child2rank to store rank
+ * 2. Traverse "positions" and apply union and find (path compression and union by rank) to update islands count
  */
 
 import java.util.*;
@@ -21,6 +21,7 @@ public class Solution{
     public List<Integer> numIslands2(int m, int n, int[][] positions) {
         Set<Integer> islands = new HashSet<>();
         Map<Integer, Integer> child2Parent = new HashMap<>();
+        Map<Integer, Integer> child2rank = new HashMap<>();
         int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         int number = 0;
         List<Integer> ret = new ArrayList<>();
@@ -32,6 +33,7 @@ public class Solution{
             islands.add(pos);
             Set<Integer> roots = new HashSet<>();
             int root = -1;
+            int maxRank = -1;
             for(int[] dir: dirs){
                 int nextY = y + dir[0];
                 int nextX = x + dir[1];
@@ -40,20 +42,38 @@ public class Solution{
                     continue;
                 }
                 //find
-                root = find(child2Parent, nextPos);
-                roots.add(root);
+                int r = find(child2Parent, nextPos);
+                if(child2rank.get(r) > maxRank){
+                    maxRank = child2rank.get(r);
+                    root = r;
+                }
+                roots.add(r);
             }
             
-            //union
+            //union by rank
             for(int r: roots){
-                child2Parent.put(r, root);
+                if(r == root){
+                    continue;
+                }
+            
+                if(maxRank > child2rank.get(r)){
+                    child2Parent.put(r, root);
+                }
+                else{
+                    child2Parent.put(r, root);
+                    child2rank.put(root, ++maxRank);
+                }
             }
             
             if(root != -1){
                 child2Parent.put(pos, root);
+                if(maxRank == 0){
+                    child2rank.put(root, ++maxRank);
+                }
             }
             else{
                 child2Parent.put(pos, pos);
+                child2rank.put(pos, 0);
             }
             number -= (roots.size() - 1);
             ret.add(number);
