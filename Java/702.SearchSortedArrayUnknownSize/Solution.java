7c4fb8a44967756547573f5036f3a9e998d4f3d7
Namo.Lu <chih-hung.lu@columbia.edu>
2018-07-14 20:18:12 -0700

    LeetCode#702

---
+++ Java/702.SearchSortedArrayUnknownSize/Solution.java
@@ -0,0 +1,40 @@
+/* Binary search: Time:O(logn), Space:O(1)
+ * 1. Binary search the length of array, and then binary search the target
+ */
+
+import java.util.*;
+
+public class Solution{
+    public int search(ArrayReader reader, int target) {
+        int lb = 0;
+        int hb = 9999;
+        while(lb <= hb){
+            int mid = (lb + hb) / 2;
+            if(reader.get(mid) == Integer.MAX_VALUE){
+                hb = mid - 1;
+            }
+            else{
+                lb = mid + 1;
+            }
+        }
+        int length = lb;
+        
+        lb = 0;
+        hb = length - 1;
+        while(lb <= hb){
+            int mid = (lb + hb) / 2;
+            if(reader.get(mid) > target){
+                hb = mid - 1;
+            }
+            else{
+                lb = mid + 1;
+            }
+        }
+        
+        return (reader.get(hb) == target)? hb: -1;
+    }
+  
+    public static void main(String[] args){
+        System.out.println("No test cases");
+    }
+}
