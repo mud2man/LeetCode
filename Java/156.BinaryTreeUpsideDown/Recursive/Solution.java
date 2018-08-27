0140fcae762ae001c496224f143531e27958e09b
Namo.Lu <chih-hung.lu@columbia.edu>
2018-08-26 22:22:38 -0700

    LeetCode#156 refine

---
+++ Java/156.BinaryTreeUpsideDown/Recursive/Solution.java
@@ -0,0 +1,79 @@
+/* Inorder: O(n)
+ * 1. Traverse tree inorderly (flip the left subtree firstly), and return the new root
+ */
+
+import java.util.*; // Stack
+
+/* Definition for binary tree */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class Solution {
+    private TreeNode inorderFlip(TreeNode root){
+        if(root.left == null){
+            return root;
+        }
+        
+        TreeNode newRoot = inorderFlip(root.left);
+        newRoot.left = root.right;
+        newRoot.right = root;
+        root.left = null;
+        root.right = null;
+        return root; 
+    }
+    
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        
+        TreeNode newRoot = root;
+        while(newRoot.left != null){
+            newRoot = newRoot.left;
+        }
+        inorderFlip(root);
+        return newRoot;
+    }
+
+    private void preOrder(TreeNode root){
+        if(root == null){
+            return;
+        }
+
+        System.out.print(root.val + ", ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void main(String[] args){
+        TreeNode root;
+        Solution sol;
+        
+        /* Generate a input tree
+         *     1
+         *    / \
+         *   2   3
+         *  / \ 
+         * 4   5
+         */
+        root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        sol = new Solution();
+
+        System.out.println("Before upside down:");
+        sol.preOrder(root); 
+        System.out.println("");
+        TreeNode newRoot = sol.upsideDownBinaryTree(root);
+        System.out.println("After upside down:");
+        sol.preOrder(newRoot); 
+        System.out.println("");
+    }
+}
