963b27782ab10e76643b60d4290751914b2926d0
Namo.Lu <chih-hung.lu@columbia.edu>
2018-09-01 12:52:36 -0700

    LeetCode#255 update

--- Java/255.VerifyPreorderSequenceBinarySearchTree/Solution.java
+++ Java/255.VerifyPreorderSequenceBinarySearchTree/Solution.java
@@ -1,15 +1,16 @@
 /* Stack: O(n) complextity, O(1) space
  * 1. If preorder[i + 1] > preorder[i], which is always valid
  * 2. Abusing the given array for the stack
- * 2. Pop stack until stack.top > preorder[i], and update the lower bound under the case
- * 3. If preorder[i] > preorder[i + 1], check if preorder[i] > lower bound
+ * 3. Pop stack until stack.top > preorder[i], and update the lower bound under the case
+ * 4. Because in preorder, the left subtree tree and root (the nodes being visited earilier) is smaller than the nodes in right tree
+ * 5. If preorder[i] > preorder[i + 1], check if preorder[i] > lower bound
  * ex: preorder = [4, 2, 1, 5, 3]
  * 
  * time[0]: lb = -max, stack = [4]
  * time[1]: lb = -max, stack = [4, 2]
  * time[2]: lb = -max, stack = [4, 2, 1]
  * time[3]: lb = 4, stack = [5]
- * time[4]: lb = 4, stack = [5] => preorder[4]=3 < lb=4, return false
+ * time[4]: lb = 4, stack = [5] => preorder[4]=3 < lb = 4, return false
  */
 
 import java.util.*; // Stack
@@ -37,8 +38,7 @@ public class Solution {
         return true;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         Solution sol;
         boolean isValid;
         int[] preorder = {4, 2, 1, 5, 3};
@@ -46,9 +46,7 @@ public class Solution {
         sol = new Solution();
 
         System.out.println("preorder: " + Arrays.toString(preorder));
-
         isValid = sol.verifyPreorder(preorder);
-        
         System.out.println("isValid: " + isValid);
     }
 }
