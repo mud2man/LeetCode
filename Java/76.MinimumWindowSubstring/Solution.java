14247f2ffbc760524bb8e9ae24411ae6692bb052
Namo.Lu <chih-hung.lu@columbia.edu>
2018-09-09 13:10:16 -0700

    LeetCode#76 udpate

--- Java/76.MinimumWindowSubstring/Solution.java
+++ Java/76.MinimumWindowSubstring/Solution.java
@@ -1,61 +1,53 @@
 /* Sliding window: Time:O(n), Space:O(n)
  * 1. Create a hashmap with key = character of t, value = [character# in t, character# in s within window]
- * 2. In the loop, shift the left end "j" of window until "remain" = 0
- * 3. Then, shift the right end "i" until remain > 0
- * 4. Update "minLength" and "minSubstring" in the end of loop
+ * 2. In the loop, shift the right end "right", and update remain and update char2count
+ * 3. Then, shift the left end "left" as long as the window contains all char in t (remain = 0)
+ * 4. Update "minLength" and "ret"" in the end of loop
  */
 
 import java.util.*;
 public class Solution{
     public String minWindow(String s, String t) {
-        HashMap<Character, int[]> characterToCount = new HashMap<Character, int[]>();
-        
+        int remain = t.length();
+        Map<Character, int[]> char2count = new HashMap<>();
         for(int i = 0; i < t.length(); ++i){
             char c = t.charAt(i);
-            characterToCount.putIfAbsent(c, new int[2]);
-            characterToCount.get(c)[0]++;
+            char2count.putIfAbsent(c, new int[2]);
+            char2count.get(c)[0]++;
         }
         
-        int minLength = s.length() + 1;
-        String minSubstring = "";
-        int i = 0;
-        int j = 0;
-        int remain = t.length();
-        while(j < s.length()){
-            while(j < s.length() && remain > 0){
-                char c = s.charAt(j++);
-                if(characterToCount.containsKey(c)){
-                    int[] pair = characterToCount.get(c);
-                    if(pair[1] < pair[0]){
-                        remain--;
-                    }
-                    pair[1]++;
+        int left = 0;
+        int minLength = Integer.MAX_VALUE;
+        String ret = "";
+        for(int right = 0; right < s.length(); ++right){
+            char c = s.charAt(right);
+            if(char2count.containsKey(c)){
+                char2count.get(c)[1]++;
+                if(char2count.get(c)[0] >= char2count.get(c)[1]){
+                    remain--;
                 }
             }
             
-            boolean isCover = (remain == 0)? true: false;
-            
-            while(i < j && remain == 0){
-                char c = s.charAt(i++);
-                if(characterToCount.containsKey(c)){
-                    int[] pair = characterToCount.get(c);
-                    pair[1]--;
-                    if(pair[1] < pair[0]){
-                        remain++;
-                        break;
+            //window contains all char in t
+            if(remain == 0){
+                c = s.charAt(left);
+                while(!char2count.containsKey(c) || char2count.get(c)[0] < char2count.get(c)[1]){
+                    if(char2count.containsKey(c)){
+                        char2count.get(c)[1]--;
                     }
+                    c = s.charAt(++left);
                 }
-            }
-            
-            if(minLength > (j - i + 1) && isCover){
-                minSubstring = s.substring(i - 1, j);
-                minLength = (j - i + 1);
+                
+                if((right - left + 1) < minLength){
+                    ret = s.substring(left, right + 1);
+                    minLength = right - left + 1;
+                }
             }
         }
         
-        return minSubstring;
+        return ret;
     }
-
+ 
     public static void main(String[] args){
         Solution sol;
         String s = "ADOBECODEBANC";
