1593957a95e919aaf6deafa7cca7f4c848f5a8bd
Namo.Lu <chih-hung.lu@columbia.edu>
2018-09-10 22:52:08 -0700

    LeetCode#273

--- Java/273.IntegerEnglishWords/Solution.java
+++ Java/273.IntegerEnglishWords/Solution.java
@@ -6,53 +6,52 @@
 import java.util.*;
 
 public class Solution{
-    String[] digits = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten","Eleven", 
-                       "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen","Nineteen"};
-    String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    String[] threeDigitUnit = {"", "Thousand", "Million", "Billion"};
-    
-    private String threeDigit(int num){ 
-        String numStr = "";
-        int hundred = num / 100;
-        if(hundred > 0){
-            numStr += digits[hundred] + " ";
-            numStr += "Hundred ";
+   class Solution {
+    private String helper(int num){
+        if(num == 0){
+            return "";
         }
-
-        num = num % 100;
-        if(num < 20 && num > 0){
-            numStr += digits[num] + " ";
+        
+        String[] digits = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven"
+                           ,"Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        String[] twoDigits = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+        
+        String ret = "";
+        int tail = num % 100;
+        num = num / 100;
+        if(tail < 20){
+            ret = digits[tail];
         }
-        else if(num > 0){
-            int ten = num / 10;
-            numStr += tens[ten] + " ";
-            num = num % 10;
-            if(num > 0){
-                numStr += digits[num] + " ";
-            }
+        else{
+            ret = (tail % 10 > 0)? (twoDigits[tail / 10] + " " + digits[tail % 10]): twoDigits[tail / 10];
         }
-        return numStr;
+        
+        if(num > 0){
+            ret = (ret.length() > 0)? (digits[num] + " Hundred ") + ret: (digits[num] + " Hundred");
+        }
+        return ret;
     }
     
     public String numberToWords(int num) {
-        String numStr = (num == 0)? "Zero ": "";
-        int weight = 1000000000;
-        for(int i = 3; i >= 0; --i){
-            if((num / weight) > 0){
-                if(i > 0){
-                    numStr = numStr + threeDigit(num / weight) + threeDigitUnit[i] + " ";
-                }
-                else{
-                    numStr = numStr + threeDigit(num / weight);
-                }      
-            }
-            num = num % weight;
-            weight = weight / 1000;
+        if(num == 0){
+            return "Zero";
         }
         
-        return numStr.substring(0, numStr.length() - 1);
+        String[] weight = {"", "Thousand", "Million", "Billion"};
+        String word = "";
+        int weightIdx = 0;
+        while(num > 0){
+            int tail = num % 1000;
+            String tailResult = helper(tail);
+            if(tailResult.length() > 0){
+                word = (" " + tailResult + " " + weight[weightIdx]) + word;
+            }
+            num = num / 1000;
+            weightIdx++;
+        }
+        return word.trim();
     }
-
+ 
     public static void main(String[] args){
         int num = 1234567;
         Solution sol = new Solution();
