eba2d486aa18d70108f153bc648096258c0e8769
Namo.Lu <chih-hung.lu@columbia.edu>
2018-09-11 23:36:47 -0700

    LeetCode#68 update

--- Java/68.TextJustification/Solution.java
+++ Java/68.TextJustification/Solution.java
@@ -4,69 +4,72 @@
 import java.util.*;
 
 public class Solution{
-    private void addLine(String[] words, int startIdx, int endIdx, List<String> lines, int maxWidth, boolean noExtarSpace){
-        int charCount = 0;
-        int wordCount = endIdx - startIdx + 1;
-        String line = words[startIdx];
-        if(noExtarSpace || wordCount == 1){
-            for(int i = startIdx + 1; i <= endIdx; ++i){
-                line = line + " " + words[i];
+    private String getLine(String[] words, int maxWidth, int left, int right){
+        String line = "";
+        //last line
+        if(right == words.length - 1){
+            int space = maxWidth;
+            line = words[left];
+            space -= line.length();
+            for(int i = left + 1; i <= right; ++i){
+                line += (" " + words[i]);
+                space -= (1 + words[i].length());
             }
-            for(int i = line.length(); i < maxWidth; ++i){
+            for(int i = 0; i < space; ++i){
                 line += " ";
             }
-            lines.add(line);
-            return;
         }
-        
-        for(int i = startIdx; i <= endIdx; ++i){
-            charCount += words[i].length();
+        else{
+            int wordLen = 0;
+            for(int i = left; i <= right; ++i){
+                wordLen += words[i].length();
+            }
+            line = words[left];
+            int spaceLen = maxWidth - wordLen;
+            if(left == right){
+                for(int i = 0; i < spaceLen; ++i){
+                    line += " ";
+                }
+            }
+            else{
+                int space = spaceLen / (right - left);
+                int longSpaceCount = spaceLen - space * (right - left);
+                int count = 0;
+                for(int i = left + 1; i <= right; ++i){
+                    int len = (count < longSpaceCount)? space + 1: space;
+                    for(int j = 0; j < len; ++j){
+                        line += " ";
+                    }
+                    count++;
+                    line += words[i];
+                }
+            } 
         }
-        
-        int spaceCount = wordCount - 1;
-        int totalSpaceLength = maxWidth - charCount;
-        int shortSpaceLength = totalSpaceLength / spaceCount;
-        int longSpaceCount = totalSpaceLength - shortSpaceLength * spaceCount;
-        String shortSpace = "";
-        for(int i = 0; i < shortSpaceLength; ++i){
-            shortSpace += " ";
-        }
-
-        for(int i = 0; i < longSpaceCount; ++i){
-            line = line + shortSpace + " " + words[++startIdx];
-        }
-        
-        for(int i = 0; i < (spaceCount - longSpaceCount); ++i){
-            line = line + shortSpace + words[++startIdx];
-        }
-        lines.add(line);
+        return line;
     }
     
     public List<String> fullJustify(String[] words, int maxWidth) {
-        int startIdx = 0;
-        int endIdx = 0;
-        List<String> lines = new ArrayList<String>();
-        
-        while(endIdx < words.length){
-            int charCount = words[startIdx].length();
-            while(charCount < maxWidth && endIdx < (words.length - 1)){
-                charCount = charCount + words[++endIdx].length() + 1;
+        int left = 0;
+        List<String> lines = new ArrayList<>();   
+        while(left < words.length){
+            int count = words[left].length();
+            int right = left + 1;
+            while(right < words.length){
+                count += (1 + words[right].length());
+                if(count > maxWidth || right == words.length){
+                    break;
+                }
+                else{
+                    right++;
+                }
             }
-            
-            if(charCount > maxWidth){
-                charCount = charCount - words[endIdx].length() - 1;
-                addLine(words, startIdx, endIdx - 1, lines, maxWidth, (charCount == maxWidth));
-                startIdx = endIdx;
-            }
-            else{
-                addLine(words, startIdx, endIdx, lines, maxWidth, true);
-                startIdx = ++endIdx;
-            }
+            right--;
+            lines.add(getLine(words, maxWidth, left, right));
+            left = right + 1;
         }
-        
         return lines;
     }
-
+ 
     public static void main(String[] args){
         String[] words = {"This", "is", "an", "example", "of", "text", "justification."};
         int maxWidth = 16;
