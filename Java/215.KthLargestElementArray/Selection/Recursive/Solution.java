6f2f4a92217a8dc00ff0cad65a0b49ca22e98599
Namo.Lu <chih-hung.lu@columbia.edu>
2018-09-13 23:41:14 -0700

    LeetCode#215 refine

---
+++ Java/215.KthLargestElementArray/Selection/Recursive/Solution.java
@@ -0,0 +1,50 @@
+/* Selection algorithm: Time:O(n) amortized, Space:O(1)
+ * 1. Use select to find the k-th element
+ * 2. If ptr - lb == k - 1, return pivot;
+ * 3. If k-th element on left half, call select(lb, ptr - 1, nums, k)
+ * 3. If k-th element on right half, call select(ptr + 1, hb, nums, k - (ptr - lb + 1))
+ */         
+
+import java.util.*;
+
+public class Solution {
+    private int select(int lb, int hb, int[] nums, int k){
+        int pivot = nums[hb];
+        int ptr = lb - 1;
+        for(int i = lb; i < hb; ++i){
+            if(nums[i] > pivot){
+                int temp = nums[++ptr];
+                nums[ptr] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        nums[hb] = nums[++ptr];
+        nums[ptr] = pivot;
+
+        if(ptr - lb == k - 1){
+            return pivot;
+        }
+        else if((ptr - lb + 1) > k){
+            return select(lb, ptr - 1, nums, k);
+        }
+        else{
+            return select(ptr + 1, hb, nums, k - (ptr - lb + 1));
+        }
+    }
+    
+    public int findKthLargest(int[] nums, int k) {
+        return select(0, nums.length - 1, nums, k);
+    }
+ 
+    public static void main(String[] args){
+        Solution sol;
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+        
+        sol = new Solution();
+
+        System.out.println("nums: " + Arrays.toString(nums));
+        System.out.println("k: " + k);
+        System.out.println("Kth largest: " + sol.findKthLargest(nums, k));
+    }
+}
