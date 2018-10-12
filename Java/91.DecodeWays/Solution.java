1a9f5a3cabfc92a7590b77d77c37617049740295
Namo.Lu <chih-hung.lu@columbia.edu>
2018-10-11 21:55:31 -0700

    LeetCode#91 update

--- Java/91.DecodeWays/Solution.java
+++ Java/91.DecodeWays/Solution.java
@@ -1,44 +1,37 @@
 /* Dynamic programming: O(n)
  * 1. dp[i] = the number of decode way from 0-th character to i-th character
- * 2. if s.charAt(i) > 0, dp[i] += dp[i - 1]
- * 3. if s.charAt(i - 1) | s.charAt(i - 1) < 27 && > 9, dp[i] += dp[i - 2]
+ * 2. if s.charAt(i) != '0', dp[i] += dp[i - 1]
+ * 3. if s.charAt(i - 1) != '0' && s.substring(i - 1, i + 1) >= 26, dp[i] += dp[i - 2]
  *
- * ex: n = 1234, dp[0] = 1, dp[1] = 1
- * dp[2] = dp[1] + dp[0] = 2
- * dp[3] = dp[2] + dp[1] = 3
- * dp[4] = dp[3] = 3
+ * ex: n = 226, dp[0] = 1
+ * dp[1] = dp[0] + 1 = 2
+ * dp[2] = dp[1] + dp[0] = 3
  */         
 
 import java.util.*;
 
 public class Solution {
     public int numDecodings(String s) {
-        int[] dp = new int [s.length() + 1];
-        int tens, digits;
-        
-        if(s.length() == 0){
+        if(s == null || s.length() == 0 || s.charAt(0) == '0'){
             return 0;
         }
         
-        digits = Integer.parseInt(s.substring(0, 1));
-        if(digits == 0){
-            return 0;
-        }
-
+        int[] dp = new int [s.length()];
         dp[0] = 1;
-        dp[1] = 1;
         for(int i = 1; i < s.length(); ++i){
-            tens = Integer.parseInt(s.substring(i - 1, i + 1));
-            digits = Integer.parseInt(s.substring(i, i + 1));
-            if(tens >= 1 && tens <= 26 && s.charAt(i - 1) != '0'){
-                dp[i + 1] += dp[i - 1];
+            dp[i] = 0;
+            //single digit
+            if(s.charAt(i) != '0'){
+                dp[i] += dp[i - 1];
             }
             
-            if(digits > 0){
-                dp[i + 1] += dp[i];
+            //double digit
+            int num = Integer.parseInt(s.substring(i - 1, i + 1));
+            if(num <= 26 && s.charAt(i - 1) != '0'){
+                dp[i] += (i - 2 >= 0)? dp[i - 2]: 1;
             }
         }
-        return dp[s.length()];
+        return dp[s.length() - 1];
     }
  
     public static void main(String[] args){
