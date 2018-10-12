2c0a9852a0255e8ec6c0163a807304baef23e3c0
Namo.Lu <chih-hung.lu@columbia.edu>
2018-10-11 21:10:04 -0700

    LeetCode#42 refine

--- Java/42.TrappingRainWater/Solution.java
+++ Java/42.TrappingRainWater/Solution.java
@@ -1,83 +1,59 @@
 /* Monotonous stack: Time:O(n), Space:O(n)
- * 1. If the next height is lower than previous one, push it into stack
- * 2. If the next height is higher than previous one, accumulate volume and pop stack
- * 3. In the latter case, if stack.size > 1 do accumulating, oterwise replace the current top 
+ * 1. Virtual stack is {stack, top, nextTop}, where node = {height, index}, and maintain a decreasing order in terms of height
+ * 2. If the next height is lower than previous one, push it into stack
+ * 3. If the next height is equals to  previous one, update its index with top[1] = nextTop[1];
+ * 2. If the next height is higher than previous one, accumulate volume and pop stack, and push nexTop to stack if nextTop[0] < top[0]
  * 
- * ex: height[]={0,1,0,2,1,0,1,3}, vol = 0, stack ={}
- * idx = 1, vol = 0, stack = {(1, 1)}
- * idx = 2, vol = 0, stack = {(1, 1), (0, 2)}
- * idx = 3, vol = 1, stack = {(2, 3)}
- * idx = 4, vol = 1, stack = {(2, 3), (1, 4)}
- * idx = 5, vol = 1, stack = {(2, 3), (1, 4), (0. 5)}
- * idx = 6, vol = 2, stack = {(2, 3), (1, 6)}
- * idx = 6, vol = 5, stack = {(2, 3)}
- * idx = 6, vol = 5, stack = {(3, 6)}
+ * ex: height[]={0, 1, 0, 2, 1, 0, 1, 3}, water = 0, stack ={}
+ * idx = 0, water = 0, stack = {(0, 0)}
+ * idx = 1, water = 0, stack = {(1, 1)}
+ * idx = 2, water = 0, stack = {(1, 1), (0, 2)}
+ * idx = 3, water = 1, stack = {(2, 3)}
+ * idx = 4, water = 1, stack = {(2, 3), (1, 4)}
+ * idx = 5, water = 1, stack = {(2, 3), (1, 4), (0. 5)}
+ * idx = 6, water = 2, stack = {(2, 3), (1, 6)}
+ * idx = 7, water = 5, stack = {(3, 7)}
  */
 
 import java.util.*;
 
 public class Solution{
-    private class HeightPos{
-        int height;
-        int pos;
-        HeightPos(int h, int p){height = h; pos = p;}
-    }
-    
     public int trap(int[] height) {
-        LinkedList<HeightPos> stack;
-        int vol, pos, len, wth;
-        
-        stack = new LinkedList<HeightPos>();
-        
-        for(pos = 0; pos < height.length && height[pos] == 0; pos++);
-        
-        if(pos == height.length){
+        if(height == null || height.length == 0){
             return 0;
         }
         
-        vol = 0;
-        stack = new LinkedList<HeightPos>();
-        stack.add(new HeightPos(height[pos], pos));
-        for(pos = pos + 1; pos < height.length; pos++){
-            if(stack.getLast().height > height[pos]){
-                stack.add(new HeightPos(height[pos], pos));
+        int water = 0;
+        Deque<int[]> stack = new LinkedList<>();
+        int[] top = new int[]{height[0], 0};
+        for(int i = 1; i < height.length; ++i){
+            int[] nextTop = new int[]{height[i], i};
+            if(nextTop[0] < top[0]){
+                stack.add(top);
+                top = nextTop;
             }
-            else if(stack.getLast().height < height[pos]){
-                if(stack.size() > 1){
-                    if(stack.get(stack.size() - 2).height > height[pos]){
-                        len = pos - stack.get(stack.size() - 2).pos - 1;
-                        wth = height[pos] - stack.get(stack.size() - 1).height;
-                        vol = vol + len * wth;
-                        stack.getLast().height = height[pos];
-                        stack.getLast().pos = pos;
-                    }
-                    else{
-                        while(stack.size() > 1 && stack.getLast().height < height[pos]){
-                            len = pos - stack.get(stack.size() - 2).pos - 1;
-                            wth = Math.min(stack.get(stack.size() - 2).height, height[pos]) - stack.getLast().height;
-                            vol = vol + len * wth;
-                            stack.pollLast();
-                        }
-                        
-                        if(stack.getLast().height < height[pos]){
-                            stack.getLast().height = height[pos];
-                            stack.getLast().pos = pos;
-                        }
-                        else{
-                            stack.add(new HeightPos(height[pos], pos));
-                        }
-                    }
-                }
-                else{
-                    stack.getLast().height = height[pos];
-                    stack.getLast().pos = pos;
-                }
+            else if(nextTop[0] == top[0]){
+                top[1] = nextTop[1];
             }
             else{
-                stack.getLast().pos = pos;
+                while(!stack.isEmpty() && nextTop[0] > top[0]){
+                    water += (nextTop[1] - stack.peekLast()[1] - 1) * (Math.min(nextTop[0], stack.peekLast()[0]) - top[0]);
+                    top = stack.pollLast();
+                }
+                
+                if(nextTop[0] < top[0]){
+                    stack.add(top);
+                    top = nextTop;
+                }
+                else if(nextTop[0] == top[0]){
+                    top[1] = nextTop[1];
+                }
+                else{
+                    top = nextTop;
+                }
             }
         }
-        return vol;
+        return water; 
     }
 
     public static void main(String[] args){
