e7fb6d54633cb7cdba0bb4dbe2f251a5aa6e085f
Namo.Lu <chih-hung.lu@columbia.edu>
2018-10-13 17:56:13 -0700

    Leetcode#49 update

--- Java/49.GroupAnagrams/Solution.java
+++ Java/49.GroupAnagrams/Solution.java
@@ -1,4 +1,4 @@
-/* HashMap + BigInteger: O(n*k), where k = longest length of string 
+/* HashMap: Time:O(n*klogk), Space:O(n), where k = longest length of string. We can translate char count to string as key to reduce time complexity to O(n*k)
  * 1. Put the string with the same key into hashMap
  * 2. Convert hashmap to list
  */          
@@ -7,29 +7,18 @@ import java.util.*;
 import java.math.*;
 
 public class Solution {
-    public BigInteger getKey(int[] primes, String s){
-        BigInteger key, num;
-        
-        key = new BigInteger("1");
-        for(char c: s.toCharArray()){
-            num = new BigInteger(Integer.toString(primes[c - 'a']));
-            key = key.multiply(num);
-        }
-        return key;
+    public String getKey(String s){
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
     }
     
     public List<List<String>> groupAnagrams(String[] strs) {
-        int[] primes = {2,  3,  5,  7,  11,
-                        13, 17, 19 ,23 ,29,
-                        31, 37, 41, 43, 47,
-                        53, 59, 61, 67, 71, 
-                        73, 79, 83, 89, 97, 
-                        101}; 
-        HashMap<BigInteger, List<String>> anagramsHashMap = new HashMap<BigInteger, List<String>>();;
+        HashMap<String, List<String>> anagramsHashMap = new HashMap<String, List<String>>();;
         List<List<String>> anagramsList = new ArrayList<List<String>>();
         
         for(String str: strs){
-            BigInteger key = getKey(primes, str);
+            String key = getKey(str);
             if(anagramsHashMap.containsKey(key)){
                 anagramsHashMap.get(key).add(str);
             }
@@ -40,13 +29,13 @@ public class Solution {
             }
         }
         
-        for (Map.Entry<BigInteger, List<String>> entry : anagramsHashMap.entrySet()) {
+        for (Map.Entry<String, List<String>> entry : anagramsHashMap.entrySet()) {
             anagramsList.add(entry.getValue());
         }
         
         return anagramsList;
     }
- 
+  
     public static void main(String[] args){
         Solution sol;
         String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
