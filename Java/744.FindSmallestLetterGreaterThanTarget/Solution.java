82f7869f4f44c546cfde00abfb2e9e3093f43afd
Namo.Lu <chih-hung.lu@columbia.edu>
2018-10-26 21:08:43 -0700

    LeetCode#744

---
+++ Java/744.FindSmallestLetterGreaterThanTarget/Solution.java
@@ -0,0 +1,45 @@
+/* Binary search Time:O(logn), Space:O(1). 
+ * 1. Because letters wrap around, we need to consider both ends no matter what
+ * 2. Set the candidate as the first char, and use binary search to find the solution between first and last
+ * 3. Then compare it with the last again
+ */          
+
+import java.util.*; // Stack
+
+public class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int lb = 0;
+        int hb = letters.length - 1;
+        char candidate = letters[0];
+        
+        while(lb <= hb){
+            int mid = (lb + hb) / 2;
+            char c = letters[mid];
+            int midDiff = (candidate > target)? candidate - target: candidate - target + 26;
+            int diff = (c > target)? c - target: c - target + 26;
+            candidate = (midDiff > diff)? c: candidate;
+            
+            if(c > target){
+                hb = mid - 1;
+            }
+            else{
+                lb = mid + 1;
+            }
+        }
+        
+        char last = letters[letters.length - 1];
+        int midDiff = (candidate > target)? candidate - target: candidate - target + 26;
+        int diff = (last > target)? last - target: last - target + 26;
+        candidate = (midDiff > diff)? last: candidate;
+        return candidate;
+    }
+  
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'a';
+        System.out.println("letters: " + Arrays.toString(letters));
+        System.out.println("target: " + target);
+        System.out.println("next greatest letter: " + sol.nextGreatestLetter(letters, target));
+    }
+}
