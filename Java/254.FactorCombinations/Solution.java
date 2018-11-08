9238fc50863fbee9aa328e2acf1ecdbe1033ee59
Namo.Lu <chih-hung.lu@columbia.edu>
2018-11-07 22:20:28 -0800

    LeetCode#254 update

--- Java/254.FactorCombinations/Solution.java
+++ Java/254.FactorCombinations/Solution.java
@@ -1,6 +1,6 @@
 /* Backtrack: O((n^0.5)!), Most n^0.5 factors, so combination# is (n^0.5)!
- * 1. In the backtracker, if the last factor is smaller or equal to n, put it factor list
- * 2. Iterate all the possible factors from last factor to n^0.5
+ * 1. In the backtracker, add {path + remain} to factors, since we keep the remain > the last number of path
+ * 2. Iterate all the possible factors from start to n^0.5
  * 3. Before enter the next-level backtracker, push the factor into factors
  * 4. Afetr leave the next-level backtracker, pop the last factor from factors
  */
@@ -8,40 +8,32 @@
 import java.util.*;
 
 public class Solution{
-    private void backtracker(LinkedList<Integer> factors, List<List<Integer>> factorsList, int n){
-        int lastFactor = factors.peekLast();
+   private void backtrack(int start, int remain, Deque<Integer> path, List<List<Integer>> factors){
+        List<Integer> factor = new ArrayList<>(path);
+        factor.add(remain);
+        factors.add(factor);
         
-        if(lastFactor <= n){
-            LinkedList<Integer> addFactors = new LinkedList<Integer>(factors);
-            addFactors.add(n);
-            factorsList.add(addFactors);
-        }
-        
-        for(int factor = lastFactor; factor <= (int)Math.sqrt(n); ++factor){
-            if(n % factor == 0){
-                //push
-                factors.add(factor);
-                
-                backtracker(factors, factorsList , n / factor);
-                
-                //pop
-                factors.pollLast();
+        for(int i = start; i <= (int)Math.sqrt(remain); ++i){
+            if(remain % i == 0){
+                path.add(i);
+                backtrack(i, remain / i, path, factors);
+                path.pollLast();
             }
         }
     }
     
     public List<List<Integer>> getFactors(int n) {
-        List<List<Integer>> factorsList = new LinkedList<List<Integer>>();
-        for(int factor = 2; factor <= (int)Math.sqrt(n); ++factor){
-            if(n % factor == 0){
-                LinkedList<Integer> factors = new LinkedList<Integer>();
-                factors.add(factor);
-                backtracker(factors, factorsList, n / factor);
+        List<List<Integer>> factors = new ArrayList<>();
+        for(int i = 2; i <= (int)Math.sqrt(n); ++i){
+            if(n % i == 0){
+                Deque<Integer> path = new LinkedList<>();
+                path.add(i);
+                backtrack(i, n / i, path, factors);
             }
         }
-        return factorsList;
+        return factors;
     }
- 
+  
     public static void main(String[] args){
         Solution sol;
         int n = 32;
