357ac2e4d232d8c77049a8650f2da4995e45352a
Namo.Lu <chih-hung.lu@columbia.edu>
2018-11-07 18:38:27 -0800

    LeetCode#31 update

--- Java/31.NextPermutation/Solution.java
+++ Java/31.NextPermutation/Solution.java
@@ -8,53 +8,42 @@
 import java.util.*;
 
 public class Solution{
-    public void reverse(int[] nums, int fromIdx, int toIdx){
-        int tmp;
-        
-        while(fromIdx < toIdx){
-            tmp = nums[fromIdx];
-            nums[fromIdx] = nums[toIdx];
-            nums[toIdx] = tmp;
-            fromIdx++;
-            toIdx--;
+    public void reverse(int[] nums, int start){
+        int i = start;
+        int j = nums.length - 1;
+        while(i < j){
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+            ++i;
+            --j;
         }
     }
     
     public void nextPermutation(int[] nums) {
-        int lowIdx;
-        int highIdx;
-        int size;
-        int tmp;
-        
-        size = nums.length;
-        
-        //permutation, if decending gap found
-        for(lowIdx = size - 1; lowIdx >= 0; --lowIdx){
-            if((lowIdx > 0) && (nums[lowIdx] > nums[lowIdx - 1])){
-                reverse(nums, lowIdx, size - 1);
-                for(highIdx = lowIdx; highIdx < size; ++highIdx){
-                    if(nums[highIdx] > nums[lowIdx - 1]){
-                        tmp = nums[lowIdx - 1];
-                        nums[lowIdx - 1] = nums[highIdx];
-                        nums[highIdx] = tmp;
-                        return;
+        int start = 0;
+        for(int i = nums.length - 1; i > 0; --i){
+            if(nums[i] > nums[i - 1]){
+                start = i;
+                for(int j = i; j <= nums.length; ++j){
+                    if(j == nums.length || nums[j] <= nums[i - 1]){
+                        int tmp = nums[i - 1];
+                        nums[i - 1] = nums[j - 1];
+                        nums[j - 1] = tmp;
+                        break;
                     }
                 }
+                break;
             }
         }
-    
-        //reverse nums, because no decending gap found
-        reverse(nums, 0, size - 1);
+        reverse(nums, start);
     }
-
+ 
     public static void main(String[] args){
-		Solution sol;
-		int[] nums = {2, 3, 1};
-		
-		sol = new Solution();
-		
+        Solution sol = new Solution();
+        int[] nums = {2, 3, 1};
         System.out.println("before permutation: " + Arrays.toString(nums));
-		sol.nextPermutation(nums);
+        sol.nextPermutation(nums);
         System.out.println("after permutation: " + Arrays.toString(nums));
 	}
 }
