005ed662a28bcd9b6da180c756f115be49e4902a
Namo.Lu <chih-hung.lu@columbia.edu>
2018-11-11 12:03:12 -0800

    LeetCode#44 refine

--- Java/44.WildcardMatching/dp/Solution.java
+++ Java/44.WildcardMatching/dp/Solution.java
@@ -1,68 +1,57 @@
-/* Greedy: Time:O(n*m), Space:O(n*m)
+/* Dynmaic progamming: Time:O(n*m), Space:O(n*m)
+ * 1. dp[y][x] = if p.substring(0, y + 1) can match s.substring(0, x + 1)
+ * 2. In general case, if p.charAt(y) == '*',  dp[y][x] = dp[y - 1][x] | dp[y - 1][x - 1] | dp[y][x - 1]
+ * 3. If p.charAt(y) == '?' ||  p.charAt(y) = s.charAt(x), dp[y][x] = dp[y - 1][x - 1]
+ * 4. Otherwise, dp[y][x] = false
+ * 5. We need to seperate cases of starting point, first row, first column, and others
+ * 6. We need to a boolean variable seeCharInFirstColumn to handle the first column 
  */
 
 import java.util.*;
 
 public class Solution{
-    private boolean onlyStar(String p, int endIndex){
-        for(int i = 0; i <= endIndex; ++i){
-            if(p.charAt(i) != '*'){
-                return false;
-            }
-        }    
-        return true;
-    }
-    
     public boolean isMatch(String s, String p) {
-        if(s.length() == 0 || p.length() == 0){
-            if(s.length() == 0 && p.length() == 0){
-                return true;
-            }
-            else if(p.length() == 0){
-                return false;
-            }
-            else{
-                return onlyStar(p, p.length() - 1);
-            }
-        }
-        
-        int depth = p.length();
-        int width = s.length();
-        boolean[][] dp = new boolean[depth][width];
-        for(int y = 0; y < depth; ++y){
-            char patternChar = p.charAt(y);
-            for(int x = 0; x < width; ++x){
-                if(patternChar == '*'){
-                    if(y == 0 && x == 0){
-                        dp[y][x] = true;
-                    }
-                    else if(y == 0){
-                        dp[y][x] = true;
-                    }
-                    else if(x == 0){
+        s = s + "?";
+        p = p + "?";
+        boolean[][] dp = new boolean[p.length()][s.length()];
+        boolean seeCharInFirstColumn = false;
+        for(int y = 0; y < p.length(); ++y){
+            char pc = p.charAt(y);
+            for(int x = 0; x < s.length(); ++x){
+                char sc = s.charAt(x);
+                if(y == 0 && x == 0){
+                    dp[y][x] = (pc == '*' || pc == '?' || pc == sc)? true: false;
+                    seeCharInFirstColumn = (pc != '*' || seeCharInFirstColumn == true)? true: false;
+                }
+                else if(y == 0){
+                    dp[y][x] = (pc == '*')? true: false;
+                }
+                else if(x == 0){
+                    if(pc == '*'){
                         dp[y][x] = dp[y - 1][x];
                     }
+                    else if(pc == '?' || pc == sc){
+                        dp[y][x] = (!seeCharInFirstColumn)? dp[y - 1][x]: false;
+                    }
                     else{
-                        dp[y][x] = dp[y][x - 1] | dp[y - 1][x - 1] | dp[y - 1][x];
-                    }
-                }
-                else if(patternChar == '?' || s.charAt(x) == patternChar){
-                    if(y == 0 && x == 0){
-                        dp[y][x] = true;
-                    }
-                    else if(y == 0){
                         dp[y][x] = false;
                     }
-                    else if(x == 0){
-                        dp[y][x] = (onlyStar(p, y - 1))? true: false;
+                    seeCharInFirstColumn = (pc != '*' || seeCharInFirstColumn == true)? true: false;
+                }
+                else{
+                    if(pc == '*'){
+                        dp[y][x] = dp[y - 1][x] | dp[y - 1][x - 1] | dp[y][x - 1]; 
+                    }
+                    else if(pc == '?' || pc == sc){
+                        dp[y][x] = dp[y - 1][x - 1]; 
                     }
                     else{
-                        dp[y][x] = dp[y - 1][x - 1];
+                        dp[y][x] = false; 
                     }
                 }
             }
         }
-        return dp[depth - 1][width - 1];
+        return dp[p.length() - 1][s.length() - 1];
     }
 
     public static void main(String[] args){
