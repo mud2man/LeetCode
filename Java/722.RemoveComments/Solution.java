e8ac4887671c03aaa878a491fa9ec9f642342ffb
Namo.Lu <chih-hung.lu@columbia.edu>
2018-11-11 15:47:57 -0800

    LeetCode#722 refine

--- Java/722.RemoveComments/Solution.java
+++ Java/722.RemoveComments/Solution.java
@@ -1,64 +1,42 @@
 /* Time:O(n), Space:O(n). Need official answer, it's to lenthy
- * 1. Scan the line every two chars, and have a "state" to indicate if during comment "/*"
+ * 1. Scan the line every two chars, and have a "inBlock" to indicate if during comment "/*"
  * 2. Handle the case given state
- * */
+ */
 
 import java.util.*;
 
 public class Solution{
-    public List<String> removeComments(String[] source) {
-        int state = 0; //0:normal, 1:during comment
-        List<String> code = new ArrayList<>();
-        boolean append = false;
-        
+   public List<String> removeComments(String[] source) {
+        List<String> codes = new ArrayList<>();
+        boolean inBlock = false;
+        StringBuilder code = new StringBuilder("");
         for(String line: source){
-            StringBuilder sb = new StringBuilder("");
-            int i = 0;
-            while(i < line.length()){
-                StringBuilder token = new StringBuilder("");
-                token.append(line.charAt(i));
-                if(i + 1 < line.length()){
-                    token.append(line.charAt(i + 1));
+            char[] array = line.toCharArray();
+            for(int i = 0; i < line.length(); ++i){
+                if(!inBlock && i + 1 < line.length() && array[i] == '/' && array[i + 1] == '*'){
+                    inBlock = true;
+                    ++i;
                 }
-                
-                if(state == 0){
-                    if(token.toString().equals("/*")){
-                        state = 1;
-                        i += 2;
-                        append = (sb.length() > 0)? true: false;
-                    }
-                    else if(token.toString().equals("//")){
-                        break;
-                    }
-                    else{
-                        sb.append(line.charAt(i));
-                        ++i;
-                    }
+                else if(inBlock && i + 1 < line.length() && array[i] == '*' && array[i + 1] == '/'){
+                    inBlock = false;
+                    ++i;
                 }
-                else{
-                    if(token.toString().equals("*/")){
-                        //append only if current line "sb" is empty
-                        if(append && code.size() > 0 && sb.length() == 0){
-                            sb.append(code.get(code.size() - 1));
-                            code.remove(code.size() - 1);
-                        }
-                        append = false;
-                        state = 0;
-                        i += 2;
-                    }
-                    else{
-                        ++i;
-                    }
+                else if(!inBlock && i + 1 < line.length() && array[i] == '/' && array[i + 1] == '/'){
+                    break;
+                }
+                else if(!inBlock){
+                    code.append(array[i]);
                 }
             }
-                                            
-            if(sb.toString().length() > 0){
-                code.add(sb.toString());
+            
+            if(!inBlock && code.length() > 0){
+                codes.add(code.toString());
+                code = new StringBuilder("");
             }
         }
-        return code;
+        return codes;
     }
-
+ 
     public static void main(String[] args){
         Solution sol;
         String[] source = {"a/*comment", "line", "more_comment*/b"};
