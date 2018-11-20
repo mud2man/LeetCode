ac2eb8cc55dc1713d705b533b9145b4f0ca2e075
Namo.Lu <chih-hung.lu@columbia.edu>
2018-11-19 21:50:51 -0800

    LeetCode#451 update

--- Java/451.SortCharactersByFrequency/Solution.java
+++ Java/451.SortCharactersByFrequency/Solution.java
@@ -8,38 +8,33 @@ import java.util.*; // Stack
 
 public class Solution {
     public String frequencySort(String s) {
-        HashMap<Character, Integer> frequencyMap = new HashMap<Character, Integer>();
-        
+        Map<Character, Integer> char2Count = new HashMap<>();
         for(char c: s.toCharArray()){
-            if(!frequencyMap.containsKey(c)){
-                frequencyMap.put(c, 0);
-            }
-            frequencyMap.put(c, frequencyMap.get(c) + 1);
+            char2Count.putIfAbsent(c, 0);
+            char2Count.put(c, char2Count.get(c) + 1);
         }
         
-        Set<Character>[] bucket = new HashSet[s.length() + 1];
-        for(Map.Entry<Character, Integer> entry: frequencyMap.entrySet()){
-            int frequency = entry.getValue();
+        Map<Integer, Set<Character>> count2Chars = new HashMap<>();
+        for(Map.Entry<Character, Integer> entry: char2Count.entrySet()){
+            int count = entry.getValue();
             char c = entry.getKey();
-            if(bucket[frequency] == null){
-                bucket[frequency] = new HashSet<Character>();
-            }
-            bucket[frequency].add(c);
+            count2Chars.putIfAbsent(count, new HashSet<>());
+            count2Chars.get(count).add(c);
         }
         
-        StringBuilder answer = new StringBuilder("");
-        for(int i = s.length(); i >= 0; --i){
-            int frequency = i;
-            if(bucket[i] != null){
-                for(char c: bucket[i]){
-                    for(int j = 0; j < frequency; ++j){
-                        answer.append(c);
+        StringBuilder sb = new StringBuilder("");
+        for(int i = s.length(); i > 0; --i){
+            if(count2Chars.containsKey(i)){
+                int count = i;
+                Set<Character> chars = count2Chars.get(count);
+                for(char c: chars){
+                    for(int j = 0; j < count; ++j){
+                        sb.append(c);
                     }
                 }
             }
         }
-        
-        return answer.toString();
+        return sb.toString();
     }
     
     public static void main(String[] args){
