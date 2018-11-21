cf64cf4a8d5d4ed20219176b1af406cd54c084dc
Namo.Lu <chih-hung.lu@columbia.edu>
2018-11-20 19:35:02 -0800

    LeetCode#39

--- Java/39.CombinationSum/Solution.java
+++ Java/39.CombinationSum/Solution.java
@@ -1,31 +1,32 @@
-/* Backtrack: O(n!)
+/* Backtrack: Time:O(n!), Space:O(n)
+ * 1. Sort input array, and call backtrack recursively
  */
 
 import java.util.*; 
 
 public class Solution{
-	public void helper(int[] candidates, int residure, List<Integer> list, List<List<Integer>> lists, int start){
-        int idx;
-        
-        if(residure == 0){
-            lists.add(new ArrayList<Integer> (list));
+    public void backtrack(int[] candidates, int residure, Deque<Integer> path, List<List<Integer>> lists, int start){
+        if(residure <= 0){
+            if(residure == 0){
+                lists.add(new ArrayList<Integer> (path));
+            }
             return;
         }
         
-        for(idx = start; idx < candidates.length && candidates[idx] <= residure; idx++){
-            list.add(candidates[idx]);
-            helper(candidates, residure - candidates[idx], list, lists, idx);
-            list.remove(list.size() - 1);
+        for(int idx = start; idx < candidates.length && candidates[idx] <= residure; idx++){
+            path.addLast(candidates[idx]);
+            backtrack(candidates, residure - candidates[idx], path, lists, idx);
+            path.pollLast();
         }
     }
     
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> lists = new ArrayList<List<Integer>>();
-        helper(candidates, target, new ArrayList<Integer>(), lists, 0);
+        backtrack(candidates, target, new LinkedList<>(), lists, 0);
         return lists;
     }
- 
+  
     public static void main(String[] args){
         int[] candidates = {2, 3, 6, 7};
         int target = 7;
