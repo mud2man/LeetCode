28b4779da2d9b672f3a4ecb67c5bbb40cea435d4
Namo.Lu <chih-hung.lu@columbia.edu>
2018-11-21 20:55:03 -0800

    LeetCode#4

--- Java/4.MedianTwoSortedArrays/Solution.java
+++ Java/4.MedianTwoSortedArrays/Solution.java
@@ -3,8 +3,8 @@
  * 2. bigLeft: the biggest element of left part in "big" array
  * 3. smallRight: the smallest element of right part in "small" array
  * 4. smallLeft: the biggest element of left part in "small" array
- * 5. Increase lb if bigRight < smallLeft
- * 6. Decrease hb if bigLeft > smallRight
+ * 5. Increase lb if bigLeft > smallRight
+ * 6. Decrease hb if smallLeft > bigRight
  * 7. Otherwise, pick the smaller between bigRight and smallRight, bigger between bigLeft and smallLeft
  */
 
@@ -13,33 +13,33 @@ import java.util.*;
 public class Solution{
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int[] big = (nums1.length >= nums2.length)? nums1: nums2;
-        int[] small = (nums1.length < nums2.length)? nums1: nums2;  
-        int halfSize = (small.length + big.length) / 2;
-        int lb = halfSize - small.length; //count all nums in small
-        int hb = halfSize; //doesn't count any num in small
+        int[] small = (nums1.length < nums2.length)? nums1: nums2;
+        int len = nums1.length + nums2.length;
+        int lb = 0;
+        int hb = small.length;
         while(lb <= hb){
             int mid = (lb + hb) / 2;
-            int bigRight = (mid < big.length)? big[mid]: Integer.MAX_VALUE;
-            int bigLeft = (mid > 0)? big[mid - 1]: Integer.MIN_VALUE;
-            int smallIdx = halfSize - mid;
-            int smallRight = (smallIdx < small.length)?  small[smallIdx]: Integer.MAX_VALUE;
-            int smallLeft = (smallIdx > 0)? small[smallIdx - 1]: Integer.MIN_VALUE;
+            int smallLeft = (mid > 0)? small[mid - 1]: Integer.MIN_VALUE;
+            int smallRight = (mid < small.length)? small[mid]: Integer.MAX_VALUE;
+            int bigIdx = len / 2 - mid;
+            int bigLeft = (bigIdx > 0)? big[bigIdx - 1]: Integer.MIN_VALUE;
+            int bigRight = (bigIdx < big.length)? big[bigIdx]: Integer.MAX_VALUE;
             
-            if(bigRight >= smallLeft && smallRight >= bigLeft){
-                if((small.length + big.length) % 2 == 0){
-                    int right = Math.min(bigRight, smallRight); 
-                    int left = Math.max(bigLeft, smallLeft); 
-                    return (double)(right + left) / 2.0;
+            if(smallLeft <= bigRight && smallRight >= bigLeft){
+                if(len % 2 == 1){
+                    return (double)Math.min(bigRight, smallRight);
                 }
                 else{
-                    return (double)Math.min(bigRight, smallRight);   
+                    int left = Math.max(bigLeft, smallLeft);
+                    int right = Math.min(bigRight, smallRight);
+                    return (double)(left + right) / 2.0;
                 }
             }
-            else if(bigRight < smallLeft){
-                lb = mid + 1;
+            else if(smallLeft > bigRight){
+                hb = mid - 1;
             }
             else{
-                hb = mid - 1;
+                lb = mid + 1;
             }
         }
         return 0.0;
