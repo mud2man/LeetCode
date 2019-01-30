d9d5cc4cd4a25ba438958c019265eb793dffe0bc
Namo.Lu <chih-hung.lu@columbia.edu>
2019-01-29 20:48:49 -0800

    LeetCode#30 refine

--- Java/30.SubstringConcatenationAllWords/Solution.java
+++ Java/30.SubstringConcatenationAllWords/Solution.java
@@ -1,93 +1,58 @@
 /* Map + Slide Window: Time:O(n*k), Space:O(m*k), where n = s.length, k = word.length, m = words.length
- * 1. Have a map word2Id to store the mappting from word to id 
- * 2. Have a map diff to store the words need be covered
- * 3. Have a array "dp", where dp[i] store the id of word started with index i, -1 if there is no such word
- * 4. Given shift (0 to (wordLen - 1)), maintain a window with size of "dicLen", and traverse to right of s
- * 5. Update "localdDiff" after traverse, if localDiff is empty, it means we coverd each wordd at once in "words".
- * 6. Since every traverse take n/wordLen times and we take wordLen traverse, so the last loop is O(n)
- * 7. Hence, time complexity is bound by the second loop, which takes O(n*k)
+ * 1. Have a map "wordMap" to record the word count in words
+ * 2. Call find "wordLen" times with shifted string s, e.g., s = "barfoothefoobarman", call "find" given "barfoothefoobar", "arfoothefoobar", "rfoothefoobar"
+ * 3. In find, add "addWord" from the tail of window and delete "deleteWord" from the head of window, and update "wordMap" and "matchCount"
+ * 4. Add index(i + offset + wordLen - windowLen) to indexs if matchCount == wordCount
  */
 
 import java.util.*;
 
 public class Solution {
-    private void updateDiff(int id, Map<Integer, Integer> diffMap, int diffVal){
-        if(id != -1){
-            if(diffMap.containsKey(id)){
-                diffMap.put(id, diffMap.get(id) + diffVal);
-                if(diffMap.get(id) == 0){
-                    diffMap.remove(id);
+    private void find(String s, int offset, Map<String, int[]> wordMap, List<Integer> indexs, int wordLen, int wordCount){
+        int matchCount = 0;
+        int windowLen = wordLen * wordCount;
+        for(int i = 0; i < s.length(); i += wordLen){
+            String addWord = ((i + wordLen) <= s.length())? s.substring(i, i + wordLen): null;
+            String deleteWord = ((i - windowLen) >= 0)? s.substring(i - windowLen, i - windowLen + wordLen): null;
+            if(addWord != null && deleteWord != null && addWord.equals(deleteWord)){
+                if(matchCount == wordCount){
+                    indexs.add(i + offset + wordLen - windowLen);
                 }
+                continue;
             }
-            else{
-                diffMap.put(id, diffVal);
+
+            if(addWord != null && wordMap.containsKey(addWord)){
+                wordMap.get(addWord)[1]++;
+                matchCount += (wordMap.get(addWord)[0] >= wordMap.get(addWord)[1])? 1: 0;
+            }
+            if(deleteWord != null && wordMap.containsKey(deleteWord)){
+                wordMap.get(deleteWord)[1]--;
+                matchCount -= (wordMap.get(deleteWord)[0] > wordMap.get(deleteWord)[1])? 1: 0;
+            }
+            if(matchCount == wordCount){
+                indexs.add(i + offset + wordLen - windowLen);
             }
         }
     }
     
     public List<Integer> findSubstring(String s, String[] words) {
-        List<Integer> indices = new ArrayList<>();
-        Map<String, Integer> word2Id = new HashMap<>();
-        Map<Integer, Integer> diff = new HashMap<>();
-        int dicLen = words.length;
-        if(dicLen == 0){
-            return indices;
-        }
-        int wordLen = words[0].length();
-        int id = 0;
+        Map<String, int[]> wordMap = new HashMap<>();
         for(String word: words){
-            if(word.length() != wordLen){
-                return indices;
-            }
-            if(word2Id.containsKey(word)){
-                diff.put(word2Id.get(word), diff.get(word2Id.get(word)) + 1);
-            }
-            else{
-                word2Id.put(word, id);
-                diff.put(id++, 1);
-            }
+            wordMap.putIfAbsent(word, new int[2]);
+            wordMap.get(word)[0]++;
         }
         
-        int[] dp = new int[s.length()];
-        for(int i = 0; i < s.length(); ++i){
-            if(i + wordLen <= s.length()){
-                String word = s.substring(i, i + wordLen);
-                if(word2Id.containsKey(word)){
-                    dp[i] = word2Id.get(word);
-                }
-                else{
-                    dp[i] = -1;
-                }
+        List<Integer> indexs = new ArrayList<>();
+        int wordLen = (words.length > 0)? words[0].length(): 0;
+        for(int i = 0; i < wordLen; ++i){
+            for(Map.Entry<String, int[]> entry: wordMap.entrySet()){
+                entry.getValue()[1] = 0;
             }
-            else{
-                dp[i] = -1;
-            }
+            find(s.substring(i), i, wordMap, indexs, wordLen, words.length);
         }
-
-        for(int shift = 0; shift < wordLen; ++shift){
-            Map<Integer, Integer> localDiff = new HashMap<>(diff);
-            for(int i = 0; i < wordLen * dicLen && (i + shift) < s.length(); i += wordLen){
-                id = dp[i + shift];
-                updateDiff(id, localDiff, -1);
-            }
-            
-            for(int start = shift; (start + wordLen * dicLen) <= s.length(); start += wordLen){
-                if(localDiff.isEmpty()){
-                    indices.add(start);
-                }
-                if((start + wordLen * dicLen) >= s.length()){
-                    break;
-                }
-                
-                int addId = dp[start];
-                updateDiff(addId, localDiff, 1);
-                int removeId = dp[start + wordLen * dicLen];
-                updateDiff(removeId, localDiff, -1);
-            }
-        }
-        return indices;
+        return indexs;
     }
-
+ 
     public static void main(String[] args){
         Solution sol = new Solution();
         String s = "barfoothefoobarman"; 
