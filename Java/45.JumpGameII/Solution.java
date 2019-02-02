309e3f5a1ffc2210c9e7622b7288023e9c30ea84
Namo.Lu <chih-hung.lu@columbia.edu>
2019-02-02 11:14:32 -0800

    LeetCode#45 refine

--- Java/45.JumpGameII/Solution.java
+++ Java/45.JumpGameII/Solution.java
@@ -1,9 +1,12 @@
 /* BFS + Dynamic Programming: O(n)
  * 1. dp[i] = the least number of steps can reach position i
- * 2. Have a pointer rightMostIndex to record the right most index which can be reached now
- * 3. When the position i is visited, it must be the shortest path. Because our visiting way is BFS
- * 4. The sooner we visit the position, the shorter the length is
+ * 2. We can prove dp[i] <= dp[j], where i < j
+ * 3. Every step, we get the farrest reachable position "end", and update dp[j] by currStep + 1
  *
+ * ex: nums = {2, 3, 1, 1, 4} 
+ * time[0]: dp{0, max, max, max, max}
+ * time[1]: dp{0, 1, 1, max, max}
+ * time[2]: dp{0, 1, 1, 2, 2} => done
  */          
 
 import java.util.*; // Stack
@@ -11,37 +14,21 @@ import java.util.*; // Stack
 public class Solution {
     public int jump(int[] nums) {
         int size = nums.length;
-        
-        int[] dp = new int[nums.length];
-        for(int i = 1; i < size; ++i){
-            dp[i] = Integer.MAX_VALUE;
-        }
-        
-        int rightMostIndex = 0;
+        int[] dp = new int[size];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
         for(int i = 0; i < size; ++i){
-            if(dp[i] == Integer.MAX_VALUE){
-                return 0;
+            int currStep = dp[i];
+            int end = Math.min(size - 1, i + nums[i]);
+            for(int j = end; j > i && dp[j] == Integer.MAX_VALUE; --j){
+                dp[j] = currStep + 1;
             }
             
-            int currLimit = Math.min(size - 1, i + nums[i]);
-            if(rightMostIndex < currLimit){
-                for(int j = currLimit; j > i; --j){
-                    if(dp[j] == Integer.MAX_VALUE){
-                        dp[j] = dp[i] + 1; 
-                    }
-                    else{
-                        break;
-                    }
-                }
-            }
-            
-            rightMostIndex = Math.max(rightMostIndex, currLimit);
-            if(rightMostIndex == size - 1){
-                return dp[rightMostIndex];
+            if(end == size - 1){
+                return dp[end];
             }
         }
-        
-        return 0;
+        return dp[size - 1];
     }
  
     public static void main(String[] args){
