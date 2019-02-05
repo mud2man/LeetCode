2245f1e19fdc819c69c53f509def54a5c608f48a
Namo.Lu <chih-hung.lu@columbia.edu>
2019-02-04 21:24:27 -0800

    LeetCode#432 update

--- Java/432.AllOneDataStructure/AllOne.java
+++ Java/432.AllOneDataStructure/AllOne.java
@@ -1,17 +1,15 @@
-/* Circular queue and Has: O(1)
+/* Circular queue and Hash: O(1)
  * 1. Have an circular queue with "head", where head is the node stored 0, head.next stored minimum, head.prev stored maximum
- * 2. Have "key2Value" to store key to value map
- * 3. Have "value2Keys" to store key sets with the same value
- * 4. Have "value2Node" to store value to node of circular queue
- * 5. When update circular queue, need to do add, and then delete
- * 6. When deleteNode, need to get the previous node of inserted node
- * 7. When addNode, just delete the node
+ * 2. Have "key2Node" to store key to node of circular queue
+ * 3. When update circular queue, need to do add, and then delete
+ * 4. When deleteNode, need to get the previous node of inserted node
+ * 5. When addNode, just delete the node
  */
 
 import java.util.*;
 
 public class AllOne {
-    private class Node{
+        private class Node{
         int freq;
         Set<String> keys;
         Node next;
@@ -19,29 +17,28 @@ public class AllOne {
         Node(int f){freq = f; keys = new HashSet<>(); next = this; prev = this;}
     }
     Node head;
-    Map<String, Integer> key2Freq;
-    Map<Integer, Node> freq2Node;
+    Map<String, Node> key2Node;
     
     /** Initialize your data structure here. */
     public AllOne() {
         head = new Node(0);
-        key2Freq = new HashMap<>();
-        freq2Node = new HashMap<>();
-        freq2Node.put(0, head);
+        key2Node = new HashMap<>();
     }
     
     private void add(Node prev, int freq, String key){
-        if(!freq2Node.containsKey(freq)){
+        if(prev.next.freq == freq){
+            key2Node.put(key, prev.next);
+        }
+        else{
             Node newNode = new Node(freq);
-            newNode.keys.add(key);
+            key2Node.put(key, newNode);
             Node next = prev.next;
             newNode.prev = prev;
             newNode.next = next;
             prev.next = newNode;
             next.prev = newNode;
-            freq2Node.put(freq, newNode);
         }
-        freq2Node.get(freq).keys.add(key);
+        key2Node.get(key).keys.add(key);
     }
     
     private void delete(Node node, String key){
@@ -49,39 +46,37 @@ public class AllOne {
         if(node.keys.isEmpty()){
             node.prev.next = node.next;
             node.next.prev = node.prev;
-            freq2Node.remove(node.freq);
+        }
+        if(key2Node.get(key) == node){
+            key2Node.remove(key);
         }
     }
     
     /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
     public void inc(String key) {
-        if(!key2Freq.containsKey(key)){
-            key2Freq.put(key, 1);
+        if(!key2Node.containsKey(key)){
             add(head, 1, key);
         }
         else{
-            key2Freq.put(key, key2Freq.get(key) + 1);
-            int freq = key2Freq.get(key);
-            add(freq2Node.get(freq - 1), freq, key);
-            delete(freq2Node.get(freq - 1), key);
+            Node curr = key2Node.get(key);
+            add(curr, curr.freq + 1, key);
+            delete(curr, key);
         }
     }
     
     /** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
     public void dec(String key) {
-        if(!key2Freq.containsKey(key)){
+        if(!key2Node.containsKey(key)){
             return;
         }
     
-        if(key2Freq.get(key) == 1){
-            key2Freq.remove(key);
-            delete(freq2Node.get(1), key);
+        Node curr = key2Node.get(key);
+        if(key2Node.get(key).freq == 1){
+            delete(curr, key);
         }
         else{
-            key2Freq.put(key, key2Freq.get(key) - 1);
-            int freq = key2Freq.get(key);
-            add(freq2Node.get(freq + 1).prev, freq, key);
-            delete(freq2Node.get(freq + 1), key);
+            add((curr.prev.freq < curr.freq - 1)? curr.prev : curr.prev.prev, curr.freq - 1, key);
+            delete(curr, key);
         }
     }
     
