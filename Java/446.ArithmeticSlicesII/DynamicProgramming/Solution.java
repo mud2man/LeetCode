a24421525fe2013ed349fcf5a22e8db11c4e5312
Namo.Lu <chih-hung.lu@columbia.edu>
2019-02-05 22:40:28 -0800

    LeetCode#446 update

--- Java/446.ArithmeticSlicesII/DynamicProgramming/Solution.java
+++ Java/446.ArithmeticSlicesII/DynamicProgramming/Solution.java
@@ -1,6 +1,8 @@
 /* Dynamic Programming: Time:O(n), Space:O(n)
- * 1. dp.get(i).get(diff) is the number of sequence with difference "diff" with length at least 2
- * 2. Traverse the input array A from right end, then update dp and accumulate returned value "num"
+ * 1. dp.get(i).get(diff) is the number of sequence with difference "diff" with length at least 2 ending with A[i]
+ * 2. Traverse the input array A from left, then update dp and accumulate returned value "count"
+ * 3. If dp.get(j).containsKey(diff), we accumulate dp.get(i) with dp.get(j).get(diff) + 1
+ * 4. Otherwise, accumulate dp.get(i) 1
  */
 
 import java.util.*; // Stack
@@ -13,22 +15,23 @@ public class Solution {
         }
         
         int count = 0;
-        for(int i = A.length - 2; i >= 0; --i){
-            int num0 = A[i];
-            for(int j = i + 1; j < A.length; ++j){
-                int num1 = A[j];
+        for(int i = 1; i < A.length; ++i){
+            int num1 = A[i];
+            for(int j = i - 1; j >= 0; --j){
+                int num0 = A[j];
                 long diff = (long)num1 - (long)num0;
-                dp.get(i).putIfAbsent(diff, 0);
                 if(dp.get(j).containsKey(diff)){
                     count += dp.get(j).get(diff);
-                    dp.get(i).put(diff, dp.get(j).get(diff) + 1 + dp.get(i).get(diff));
+                    dp.get(i).putIfAbsent(diff, 0);
+                    dp.get(i).put(diff, dp.get(i).get(diff) + dp.get(j).get(diff) + 1);
                 }
                 else{
-                    dp.get(i).put(diff, 1 + dp.get(i).get(diff));
+                    dp.get(i).putIfAbsent(diff, 0);
+                    dp.get(i).put(diff, dp.get(i).get(diff) + 1);
                 }
             }
         }
-        return count;
+        return count; 
     }
 
     public static void main(String[] args){
