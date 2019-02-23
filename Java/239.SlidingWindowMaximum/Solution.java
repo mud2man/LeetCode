3637ff1d1b34dcb5d99749b08fcfc036bdaeeb86
Namo.Lu <chih-hung.lu@columbia.edu>
2019-02-23 11:26:26 -0800

    LeetCode#239 refine

--- Java/239.SlidingWindowMaximum/Solution.java
+++ Java/239.SlidingWindowMaximum/Solution.java
@@ -1,43 +1,45 @@
-/* Monotonous Stack: Time:O(n), Space:O(n)
- * 1. Have a monotonous stack store in decending order from head to tail, where tail is the latest visited element
- * 2. Becuase the previous element will be replaced by the current bigger element, we will kill them in "update" method
- * 3. When sliding window, we delete the haed of stack only if the out-of-window element is equal to the head 
- * 4. Because all the element at most go through the stack once. So the amortized time complexity is O(n)  
+/* Monotonous Queue: Time:O(n), Space:O(k)
+ * 1. Have a monotonous queue store in increasing order from head to tail in the window, where front is the latest visited element
+ * 2. Becuase the previous element will be replaced by the current bigger element, we will kill them in "insert" method
+ * 3. When sliding window, we delete the tail only if the out-of-window element is equal to it, 
+ *    because it must be tail if its (k - 1) sucessors is not able to replace it in the window
+ * 4. Because all the element at most go through the queue once. So the amortized time complexity is O(n)  
  */
 
 import java.util.*;
 
 public class Solution{
-    private void update(LinkedList<Integer> monotonousStack, int num){
-        while(!monotonousStack.isEmpty() && monotonousStack.peekLast() < num){
-            monotonousStack.pollLast();
+    private void insert(Deque<Integer> monotonousQ, int addNum){
+        while(!monotonousQ.isEmpty() && addNum > monotonousQ.peekFirst()){
+            monotonousQ.pollFirst();
         }
-        monotonousStack.add(num);
+        monotonousQ.addFirst(addNum);
     }
     
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if(nums.length == 0){
-            return new int[]{};
+        if(nums == null || nums.length == 0){
+            return new int[0];
         }
         
-        LinkedList<Integer> monotonousStack = new LinkedList<Integer>();
-        
-        for(int i = 0; i < (k - 1); ++i){
-            update(monotonousStack, nums[i]);
+        Deque<Integer> monotonousQ = new LinkedList<>();
+        for(int i = 0; i < k; ++i){
+            insert(monotonousQ, nums[i]);
         }
         
-        int[] maximums = new int[nums.length - k + 1];
-        for(int i = k - 1; i < nums.length; ++i){
-            update(monotonousStack, nums[i]);
-            maximums[i - k + 1] = monotonousStack.peekFirst();
-            int deleteNum = nums[i - k + 1];
-            if(monotonousStack.peekFirst() == deleteNum){
-                monotonousStack.pollFirst();
+        int[] max = new int[nums.length - k + 1];
+        for(int i = k; i < nums.length; ++i){
+            max[i - k] = monotonousQ.peekLast();
+            int addNum = nums[i];
+            int deleteNum = nums[i - k];
+            if(monotonousQ.peekLast() == deleteNum){
+                monotonousQ.pollLast();
             }
+            insert(monotonousQ, addNum);
         }
-        return maximums;
+        max[max.length - 1] = monotonousQ.peekLast();
+        return max;
     }
-
+ 
     public static void main(String[] args){
         Solution sol = new Solution();
         int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
