e33e2da911eb083db7578b3df910c813ddfe5cc5
Namo.Lu <chih-hung.lu@columbia.edu>
2019-02-27 20:33:39 -0800

    LeetCode#480

---
+++ Java/480.SlidingWindowMedian/Solution.java
@@ -0,0 +1,70 @@
+/* Heap: O(nlogn)
+ * 1. Have a maxHeap to store the smaller half
+ * 2. Have a minHeap to store the bigger half
+ */
+
+import java.util.*;
+
+public class Solution{
+    private class MaxHeapComparator implements Comparator<Integer>{
+        public int compare(Integer x, Integer y){
+            return (x > y)? -1: (x == y)? 0: 1;
+        }
+    }
+    
+    private void update(PriorityQueue<Integer> maxHeap, PriorityQueue<Integer> minHeap, int val, boolean isInsert){
+        if(isInsert){
+            if(maxHeap.isEmpty() || maxHeap.peek() >= val){
+                maxHeap.add(val);
+            }
+            else{
+                minHeap.add(val);
+            }
+        }
+        else{
+            if(maxHeap.peek() >= val){
+                maxHeap.remove(val);
+            }
+            else{
+                minHeap.remove(val);
+            }
+        }
+        
+        if(minHeap.size() > maxHeap.size()){
+            maxHeap.add(minHeap.poll());
+        }
+        if(maxHeap.size() > (minHeap.size() + 1)){
+            minHeap.add(maxHeap.poll());
+        }
+    }
+    
+    public double[] medianSlidingWindow(int[] nums, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(new MaxHeapComparator());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int i = 0; i < k - 1; ++i){
+            update(maxHeap, minHeap, nums[i], true);
+        }
+        
+        double[] medians = new double[nums.length - k + 1];
+        for(int i = k - 1, j = 0; i < nums.length; ++i, ++j){
+            update(maxHeap, minHeap, nums[i], true);
+            if(k % 2 == 1){
+                medians[j] = (double)maxHeap.peek();
+            }
+            else{
+                medians[j] = ((double)maxHeap.peek() + (double)minHeap.peek()) / 2.0;
+            }
+            update(maxHeap, minHeap, nums[i - k + 1], false);
+        }
+        return medians;
+    }
+
+    public static void main(String[] args){
+        Solution sol = new Solution() ;
+        int k = 3;
+        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+        System.out.println("k:" + k);
+        System.out.println("nums:" + Arrays.toString(nums));
+        System.out.println("medians:" + Arrays.toString(sol.medianSlidingWindow(nums, k)));
+    }
+}
