c3f6e0a97e88a0fecd426507e35d1facc175f49a
Namo.Lu <chih-hung.lu@columbia.edu>
2019-03-09 23:19:38 -0800

    LeetCode#329 refine

--- Java/329.LongestIncreasingPathMatrix/Solution.java
+++ Java/329.LongestIncreasingPathMatrix/Solution.java
@@ -1,54 +1,46 @@
 /* Dynamic Programming + DFS: O(n*m)
  * 1. dp[y][x] = the longest increasing path starting from (y, x)
  * 2. If dp[y][x] != 0, which means we already visit (y, x), we will NOT invoke dfs again
+ * 3. And if (y, x) visited, the answer cannot be changed, since the direction to its neighbor is fixed, the path is not overlapped
  * 3. Record maximun length and invoke dfs, where 0 <= y < matrix.length, and 0 <= x < matrix[0].length 
  */
 
 import java.util.*;
 
 public class Solution{
-    //left, up, right, down
-    private int[][] directions = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
-    
-    private boolean isInRange(int[][] matrix, int y, int x){
-        return (y >= 0 && y < matrix.length && x >= 0 && x < matrix[0].length) ? true: false;
+    private int dfs(int[][] matrix, int y, int x, int[][]dp){
+        if(dp[y][x] > 0){
+            return dp[y][x];
+        }
+        
+        dp[y][x] = 1;
+        int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}}; 
+        for(int[] dir: dirs){
+            int nextY = y + dir[0];
+            int nextX = x + dir[1];
+            if(nextY < 0 || nextY >= matrix.length || nextX < 0 || nextX >= matrix[0].length){
+                continue;
+            }
+            if(matrix[y][x] > matrix[nextY][nextX]){
+                dfs(matrix, nextY, nextX, dp);
+                dp[y][x] = Math.max(dp[y][x], 1 + dp[nextY][nextX]);
+            }
+        }
+        return dp[y][x];
     }
     
-    private void dfs(int[][] matrix, int[][] dp, int y, int x){
-        int maxLen = 0;
-        
-        for(int[] direction: directions){
-            int shiftY = y + direction[0];
-            int shiftX = x + direction[1];
-            
-            //increase path
-            if(isInRange(matrix, shiftY, shiftX) && matrix[y][x] < matrix[shiftY][shiftX]){
-                if(dp[shiftY][shiftX] == 0){
-                    dfs(matrix, dp, shiftY, shiftX);
-                }
-                maxLen = Math.max(dp[shiftY][shiftX] + 1, maxLen);    
-            }
-            maxLen = Math.max(maxLen, 1);
-        }
-        dp[y][x] = maxLen;
-    } 
-    
     public int longestIncreasingPath(int[][] matrix) {
-        if(matrix.length == 0){
-            return 0;
-        }
-        
-        int[][] dp = new int[matrix.length][matrix[0].length];
-        int maxLen = 0;
-        for(int y = 0; y < matrix.length; y++){
-            for(int x = 0; x < matrix[0].length; x++){
-                if(dp[y][x] == 0){
-                    dfs(matrix, dp, y, x);
-                    maxLen = Math.max(maxLen, dp[y][x]);
-                }
+        int depth = matrix.length;
+        int width = (depth > 0)? matrix[0].length: 0;
+        int[][] dp = new int[depth][width];
+        int max = 0;
+        for(int y = 0; y < depth; ++y){
+            for(int x = 0; x < width; ++x){
+                dp[y][x] = dfs(matrix, y, x, dp);
+                max = Math.max(max, dp[y][x]);
             }
         }
-        return maxLen;
+        return max;
     }
 
     public static void main(String[] args){
