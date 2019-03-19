00f9491ef30dd234acf407d45c4356851f264dd8
Namo.Lu <chih-hung.lu@columbia.edu>
2019-03-18 23:20:13 -0700

    LeetCode#159 update

--- Java/159.LongestSubstringAtMostTwoDistinctCharacters/Solution.java
+++ Java/159.LongestSubstringAtMostTwoDistinctCharacters/Solution.java
@@ -1,4 +1,4 @@
-/* Sliding window: O(n)
+/* Sliding window: Time:O(n), Space:O(1)
  * 1. Use two pointers to store the head and tail of the string with at most 2 characters
  * 2. Use hashmap to record the number of occurrence of ebery character in between head and tail
  * 3. If the size of hashmap == 3, move head to right until the size is equal to 2
@@ -10,28 +10,13 @@ import java.util.*;
 public class Solution{
     public int lengthOfLongestSubstringTwoDistinct(String s) {
         HashMap<Character, Integer> distribution = new HashMap<Character, Integer>();
-        int head, tail, maxLen, currLen;
-        char c;
-        
-        maxLen = 0;
-        head = 0;
-        tail = 0;
-        currLen = 0;
-        
-        for(tail = 0; tail < s.length(); tail++){
-            c = s.charAt(tail);
-            currLen++;
-            if(distribution.containsKey(c)){
-                distribution.put(c, distribution.get(c) + 1);
-            }
-            else{
-                distribution.put(c, 1);
-            }
-            
-            
+        int maxLen = 0;
+        for(int tail = 0, head = 0; tail < s.length(); tail++){
+            char c = s.charAt(tail);
+            distribution.putIfAbsent(c, 0);
+            distribution.put(c, distribution.get(c) + 1);
             while(distribution.size() == 3){
                 c = s.charAt(head++);
-                currLen--;
                 if(distribution.get(c) == 1){
                     distribution.remove(c); 
                 }
@@ -39,11 +24,11 @@ public class Solution{
                     distribution.put(c, distribution.get(c) - 1);
                 }
             }
-            maxLen = Math.max(currLen, maxLen);
+            maxLen = Math.max(tail - head + 1, maxLen);
         }
         return maxLen;
     }
-   
+  
     public static void main(String[] args){
         Solution sol = new Solution();
         String s = "eceba";
