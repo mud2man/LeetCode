92255d4ae12fe5a437bb246bf72fb88c956b560b
Namo.Lu <chih-hung.lu@columbia.edu>
2019-03-30 00:12:52 -0700

    LeetCode#402 update

--- Java/402.RemoveKDigits/Solution.java
+++ Java/402.RemoveKDigits/Solution.java
@@ -2,7 +2,7 @@
  * 1. Delete sb.charAt(index) if sb.charAt(index + 1) < sb.charAt(index)g
  * 2. Delete the the remaining digits from the back of sb
  * 3. Delete the leading zeors, if they exist
- * 4. We can prove by we can always get a smaller number by deleting the first decending digit, rather than others. So we know the first decending digit must be deleted
+ * 4. We can prove by we can always get a smaller number by deleting the first decending digit, rather than others. So we know the first peak digit must be deleted
  */
 
 import java.util.*; // Stack
