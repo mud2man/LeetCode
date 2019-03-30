b6975870c66fa4f3edd783c759e69bdd0924c04c
Namo.Lu <chih-hung.lu@columbia.edu>
2019-03-30 15:34:07 -0700

    LeetCode#5 update

--- Java/5.LongestPalindromicSubstring/Solution.java
+++ Java/5.LongestPalindromicSubstring/Solution.java
@@ -1,4 +1,5 @@
 /* Manacher's Algorithm: Time:O(n), Space:O(n)
+ * 1. Append '#' between character s.t. ccc -> #c#c#c#, so we only need to take care odd-length case
  * 1. Have a right bound of palindrome, and then traverse t
  * 2. If i > rightBound, then set r[i] = 0. Otherwise, set r[i] = min(rightBound - i, r[2 * c - i])
  * 3. Retrieve palindrome given c and its radius "max"
@@ -9,19 +10,19 @@ import java.util.*;
 public class Solution {
     public String longestPalindrome(String s) {
         StringBuilder t = new StringBuilder("");
-        t.append("*");
+        t.append('#');
         for(int i = 0; i < s.length(); ++i){
             t.append(s.charAt(i));
             t.append('#');
         }
         
-        int[] r = new int[t.length()];
         int c = 1;
         int right = 1;
         int maxRadius = 0;
         int maxC = 1;
-        for(int i = 2; i < t.length(); ++i){
-            r[i] = (right > i)? Math.min(right - i, r[2 * c - i]) : 0;
+        int[] r = new int[t.length()];
+        for(int i = 1; i < t.length(); ++i){
+            r[i] = (i > right)? 0: Math.min(right - i, r[2 * c - i]);
             while(i - r[i] - 1 >= 0 && i + r[i] + 1 < t.length() && t.charAt(i - r[i] - 1) == t.charAt(i + r[i] + 1)){
                 r[i]++;
             }
@@ -30,22 +31,21 @@ public class Solution {
                 right = i + r[i];
                 c = i;
             }
-            
-            if(maxRadius < r[i]){
+            if(r[i] > maxRadius){
                 maxC = i;
                 maxRadius = r[i];
             }
         }
         
-        int start = ((maxC - maxRadius) == 1)? 1: maxC - maxRadius + 1;
+        int start = maxC - maxRadius + 1;
         int end = maxC + maxRadius;
         StringBuilder p = new StringBuilder("");
-        for(int i = start; i <= end && i < t.length(); i += 2){
+        for(int i = start; i < end; i += 2){
             p.append(t.charAt(i));
         }
         return p.toString();
     }
-  
+ 
     public static void main(String[] args){
         Solution sol;
         String s = "babad";
