0570aad1fb6a9b5f7c4d6fd0794b62815532d22e
Namo.Lu <chih-hung.lu@columbia.edu>
2019-03-29 23:22:57 -0700

    LeetCode#402 update

--- Java/402.RemoveKDigits/Solution.java
+++ Java/402.RemoveKDigits/Solution.java
@@ -1,54 +1,47 @@
-/* Greedy: O(n)
+/* Greedy: Time:O(n), Space:O(n)
  * 1. Delete sb.charAt(index) if sb.charAt(index + 1) < sb.charAt(index)g
  * 2. Delete the the remaining digits from the back of sb
  * 3. Delete the leading zeors, if they exist
+ * 4. We can prove by we can always get a smaller number by deleting the first decending digit, rather than others. So we know the first decending digit must be deleted
  */
 
 import java.util.*; // Stack
 
 public class Solution {
     public String removeKdigits(String num, int k) {
-        StringBuffer sb = new StringBuffer(num);
-        
         // Delete sb.charAt(index) if sb.charAt(index + 1) < sb.charAt(index)
-        int index = 0;
-        while(k > 0 && index < (sb.length() - 1)){
-            if(sb.charAt(index + 1) < sb.charAt(index)){
-                sb.deleteCharAt(index);
+        Deque<Character> digits = new LinkedList<>();
+        digits.add('0');
+        for(Character c: num.toCharArray()){
+            while(k > 0 && digits.peekLast() > c){
+                digits.pollLast();
                 --k;
-                index = (index > 0)? index - 1: 0; 
             }
-            else{
-                ++index;
-            }
+            digits.add(c);
         }
-        
+  
         // Delete the the remaining digits from the back of sb
-        while(k > 0 && sb.length() > 0){
-            sb.deleteCharAt(sb.length() - 1);
+        while(k > 0 && !digits.isEmpty()){
+            digits.pollLast();
             --k;
-        }
-        
+        } 
         // Delete the leading zeors, if they exist
-        while(sb.length() > 0 && sb.charAt(0) == '0'){
-            sb.deleteCharAt(0);
+        while(!digits.isEmpty() && digits.peekFirst() == '0'){
+            digits.pollFirst();
         }
-        return (sb.length() == 0)? "0": sb.toString();
+        String reducedNum = "";
+        for(Character c: digits){
+            reducedNum += Character.toString(c);
+        }
+        return (reducedNum.length() == 0)? "0": reducedNum;
     }
  
     public static void main(String[] args){
-        Solution sol;
+        Solution sol = new Solution();
         String num = "1432219";
-        String rNum ;
-        int k;
-
-        sol = new Solution();
-        k = 3;
-
+        String rNum;
+        int k = 3;
         System.out.println("num: " + num);
-
-        rNum = sol.removeKdigits(num, k);
-        
-        System.out.println("rNum: " + rNum);
+        System.out.println("rNum: " + sol.removeKdigits(num, k));
 	}
 }
