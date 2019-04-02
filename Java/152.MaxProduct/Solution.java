fd97dab1a5e9dc6dc0de4a95e2b1e39eaad32f7d
Namo.Lu <chih-hung.lu@columbia.edu>
2019-04-01 20:45:08 -0700

    LeetCode#152

--- Java/152.MaxProduct/Solution.java
+++ Java/152.MaxProduct/Solution.java
@@ -10,30 +10,25 @@ public class Solution {
 	public int maxProduct(int[] nums) {
         int[][] dp = new int[nums.length][2];
         int globalMax = nums[0];
-        int num0, num1, num2, localMax, localMin;
         
         dp[0][0] = nums[0];
         dp[0][1] = nums[0];
         for(int i = 1; i < nums.length; ++i){
-            num0 = nums[i];
-            num1 = nums[i] * dp[i - 1][0];
-            num2 = nums[i] * dp[i - 1][1];
-            localMax = Math.max(num2, Math.max(num0, num1));
-            localMin = Math.min(num2, Math.min(num0, num1));
+            int num0 = nums[i];
+            int num1 = nums[i] * dp[i - 1][0];
+            int num2 = nums[i] * dp[i - 1][1];
+            int localMax = Math.max(num2, Math.max(num0, num1));
+            int localMin = Math.min(num2, Math.min(num0, num1));
             globalMax = Math.max(localMax, globalMax);
             dp[i][0] = localMin;
             dp[i][1] = localMax;
         }
-        return globalMax;
+        return globalMax; 
     }
 
     public static void main(String[] args){
-        Solution sol;
-        int maximum;
+        Solution sol = new Solution();
         int[] nums = {2, 3, -2, 4};
-
-        sol = new Solution();
-        
         System.out.println("nums: " + Arrays.toString(nums));
         System.out.println("maximum product:" + sol.maxProduct(nums));
     }
