9e50adb89f73599a58314043bae698716ce77764
Namo.Lu <chih-hung.lu@columbia.edu>
2019-04-05 23:40:55 -0700

    LeetCode#221 update

--- Java/221.MaximalSquare/Solution.java
+++ Java/221.MaximalSquare/Solution.java
@@ -1,4 +1,4 @@
-/* Dynamic Programming: O(n*m)
+/* Dynamic Programming: Time:O(n*m), Space:O(n)
  * 1. dp[y][x][0] = the maximum length of upward column of '1' connect with (y, x)
  * 2. dp[y][x][1] = the maximum length of left-ward row of '1' connect with (y, x)
  * 3. dp[y][x][2] = the length of biggest square with right-bottom cornet at (y, x)
@@ -13,20 +13,23 @@ public class Solution{
     public int maximalSquare(char[][] matrix) {
         int depth = matrix.length;
         int width = (depth > 0)? matrix[0].length: 0;
-        int[][][] dp = new int[depth][width][3]; //up, left, upLeft
+        int[][][] dp = new int[2][width][3]; //up, left, upLeft
         
         int maxLen = 0;        
         for(int y = 0; y < depth; ++y){
             for(int x = 0; x < width; ++x){
+                for(int i = 0; i < 3; ++i){
+                   dp[y % 2][x][i] = 0; 
+                }
                 if(matrix[y][x] == '1'){
-                    int up = (y > 0)? dp[y - 1][x][0]: 0;
-                    int left = (x > 0)? dp[y][x - 1][1]: 0;
-                    int upLeft = (y > 0 && x > 0)? dp[y - 1][x - 1][2]: 0;
-                    dp[y][x][0] = up + 1;
-                    dp[y][x][1] = left + 1;
-                    int currLen = Math.min(dp[y][x][0], dp[y][x][1]);
+                    int up = (y > 0)? dp[(y - 1) % 2][x][0]: 0;
+                    int left = (x > 0)? dp[y % 2][x - 1][1]: 0;
+                    int upLeft = (y > 0 && x > 0)? dp[(y - 1) % 2][x - 1][2]: 0;
+                    dp[y % 2][x][0] = up + 1;
+                    dp[y % 2][x][1] = left + 1;
+                    int currLen = Math.min(dp[y % 2][x][0], dp[y % 2][x][1]);
                     currLen = Math.min(upLeft + 1, currLen);
-                    dp[y][x][2] = currLen;
+                    dp[y % 2][x][2] = currLen;
                     maxLen = Math.max(maxLen, currLen);
                 }
             }
