bde17f1f5f450b1b0ca54519af1093d9c701abca
Namo.Lu <chih-hung.lu@columbia.edu>
2019-04-05 20:32:22 -0700

    LeetCode#304 update

--- Java/304.RangeSumQuery2D/NumMatrix.java
+++ Java/304.RangeSumQuery2D/NumMatrix.java
@@ -1,57 +1,34 @@
 /* Dynamic Programing: O(n*m)
  * 1. Create a 2D array to store partual sum
  * 2. sumMatrix(i, j) = sumMatrix(i, j-1) + sumMatrix(i-1, j) + x(i, j) - sumMatrix(i-1, j-1)
- * 3. regionSum(row1, col1, row2, col2) = sumMatrix(row2, col2) - sumMatrix(row2, col1 - 1 ) - 
-                                          sumMatrix(row1, col2 - 1) + sumMatrix(row1 - 1, col1 - 1)
+ * 3. regionSum(row1, col1, row2, col2) = sumMatrix(row2, col2) - sumMatrix(row2, col1 - 1 ) - sumMatrix(row1, col2 - 1) + sumMatrix(row1 - 1, col1 - 1)
  */
 
 import java.util.*;
 
 public class NumMatrix{
     private int[][] sumMatrix;
-    
     public NumMatrix(int[][] matrix) {
         if(matrix.length == 0){
             return;
         }
-        
         sumMatrix = new int[matrix.length][matrix[0].length];
-        
         for(int y = 0; y < sumMatrix.length; ++y){
             for(int x = 0; x < sumMatrix[0].length; ++x){
-                int sum = 0;
-                if(y > 0){
-                    sum = sum + sumMatrix[y - 1][x];
-                }
-                
-                if(x > 0){
-                    sum = sum + sumMatrix[y][x - 1];
-                }
-                
-                if(y > 0 && x > 0){
-                    sum  = sum - sumMatrix[y - 1][x - 1];
-                }
-                sumMatrix[y][x] = sum + matrix[y][x];
+                int up = (y > 0)? sumMatrix[y - 1][x]: 0;
+                int left = (x > 0)? sumMatrix[y][x - 1]: 0;
+                int upLeft = (y > 0 && x > 0)? sumMatrix[y - 1][x - 1]: 0;
+                sumMatrix[y][x] = matrix[y][x] + up + left - upLeft;
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = sumMatrix[row2][col2];
-        
-        if(row1 > 0){
-            sum = sum - sumMatrix[row1 - 1][col2];
-        }
-        
-        if(col1 > 0){
-            sum = sum - sumMatrix[row2][col1 - 1];
-        }
-        
-        if(row1 > 0 && col1 > 0){
-            sum = sum + sumMatrix[row1 - 1][col1 - 1];
-        }
-        return sum;
-    } 
+        int up = (row1 > 0)? sumMatrix[row1 - 1][col2]: 0;
+        int left = (col1 > 0)? sumMatrix[row2][col1 - 1]: 0;
+        int upLeft = (row1 > 0 && col1 > 0)? sumMatrix[row1 - 1][col1 - 1]: 0;
+        return sumMatrix[row2][col2] - up - left + upLeft;
+    }
 
     public static void main(String[] args){
         NumMatrix sol;
