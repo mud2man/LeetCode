13b75cb23291dc02dbacd2b775150f3e434b9265
Namo.Lu <chih-hung.lu@columbia.edu>
2019-04-08 22:42:47 -0700

    LeetCode#63 update

--- Java/63.UniquePathsII/Solution.java
+++ Java/63.UniquePathsII/Solution.java
@@ -1,60 +1,36 @@
-/* Dynamic programming: O(n*m),
- * 1. Traverse from dp[y][x] to dp[0][0]
+/* Dynamic programming: Time:O(n*m), Space:O(n*m)
+ * 1. Traverse from dp[0][0], and return the right-bottom corner
  */
 
 import java.util.*;
 
 public class Solution{
 	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int[][] dp;
-        int rowNum;
-        int colNum;
-        int x;
-        int y;
-        
-        rowNum = obstacleGrid.length;
-        colNum = obstacleGrid[0].length;
-        dp = new int[rowNum][colNum];
-        
-        for(y = rowNum - 1; y >= 0; --y){
-            for(x = colNum - 1; x >= 0; --x){
+        int[][] dp = new int [obstacleGrid.length][obstacleGrid[0].length];
+        for(int y = 0; y < obstacleGrid.length; ++y){
+            for(int x = 0; x < obstacleGrid[0].length; ++x){
                 if(obstacleGrid[y][x] == 1){
                     dp[y][x] = 0;
                 }
                 else{
-                    if((y < (rowNum - 1)) && (x < (colNum - 1))){
-                        dp[y][x] = dp[y][x + 1] + dp[y + 1][x];
-                    }
-                    else if((y == (rowNum -1)) && (x < (colNum - 1))){
-                        dp[y][x] = dp[y][x + 1];
-                    }
-                    else if((y < (rowNum - 1)) && (x == (colNum - 1))){
-                        dp[y][x] = dp[y + 1][x];
-                    }
-                    else{
-                        dp[y][x] = 1;;
-                    }
+                    int upCount = (y > 0)? dp[y - 1][x]: 0;
+                    int leftCount = (x > 0)? dp[y][x - 1]: 0;
+                    dp[y][x] = (y == 0 && x == 0)? 1: upCount + leftCount;
                 }
             }
         }
-        return dp[0][0];
+        return dp[obstacleGrid.length - 1][obstacleGrid[0].length - 1]; 
     }
  
     public static void main(String[] args){
-		Solution sol;
-		int pathNum;
-		int i;
+		Solution sol = new Solution();;
 		int[][] obstacleGrid = {{0, 0, 0},
-								{0, 1, 0},
-								{0, 0, 0}};
-
-		sol = new Solution();
-		pathNum = sol.uniquePathsWithObstacles(obstacleGrid);
-		
-		for(i = 0; i < obstacleGrid.length; ++i){
+                                {0, 1, 0},
+                                {0, 0, 0}};
+        
+        for(int i = 0; i < obstacleGrid.length; ++i){
         	System.out.println(Arrays.toString(obstacleGrid[i]));
-		}
-
-        System.out.println("#path: " + pathNum);
+        }
+        System.out.println("#path: " + sol.uniquePathsWithObstacles(obstacleGrid));
 	}
 }
