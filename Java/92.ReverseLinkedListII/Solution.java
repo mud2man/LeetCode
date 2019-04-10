bc45c04d1288b0086cbde6e413c850b11ec6b5ee
Namo.Lu <chih-hung.lu@columbia.edu>
2019-04-09 20:19:03 -0700

    LeetCode#92 update

--- Java/92.ReverseLinkedListII/Solution.java
+++ Java/92.ReverseLinkedListII/Solution.java
@@ -16,63 +16,55 @@ class ListNode {
  
 public class Solution{
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        ListNode tail, rHead, dummy, curr, next, secNext;
-        
-        if(n <= m){
+        if(n == m){
             return head;
         }
-        
-        dummy = new ListNode(0);
+        ListNode dummy = new ListNode(0);
         dummy.next = head;
         
-        //find the starting node
-        tail = dummy;
-        rHead = head;
-        for(int i = 1; i < m; i++){
-            tail = rHead;
-            rHead = rHead.next;
+        // find the first part
+        ListNode firstTail = dummy;
+        ListNode secondTail = head;
+        for(int i = 1; i < m ; ++i){
+            firstTail = secondTail;
+            secondTail = secondTail.next;
         }
         
-        //reverse
-        curr = rHead;
-        next = rHead.next;
-        secNext = rHead.next.next;
-        for(int i = 0; i < (n - m); ++i){
-            next.next = curr;
-            curr = next;
-            next = secNext;
-            secNext = (secNext != null)? secNext.next: null;
+        //reverse the second part
+        ListNode curr = secondTail;
+        ListNode secondHead = (curr != null)? curr.next: null; 
+        ListNode thirdHead = (secondHead != null)? secondHead.next: null;
+        secondHead.next = curr;
+        for(int j = 1; j < n - m; ++j){
+            curr = secondHead;
+            secondHead = thirdHead;
+            thirdHead = (thirdHead != null)? thirdHead.next: null;
+            secondHead.next = curr;
         }
-        
-        //reconnect the reversed sub-list with the original list
-        rHead.next = next;
-        tail.next = curr;
-        
+
+        // link the three parts
+        firstTail.next = secondHead;
+        secondTail.next = thirdHead;
         return dummy.next;
     }
-
+ 
     public static void main(String[] args){
-        Solution sol;
-        ListNode head;
-        ListNode node;
-        int m, n;
-
-        sol = new Solution();
-        head = new ListNode(1);
+        Solution sol = new Solution();
+        ListNode head = new ListNode(1);
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
         
         System.out.println("before reverse: ");
-        node = head;
+        ListNode node = head;
         while(node != null){    
             System.out.print(node.val + "->");
             node = node.next;
         }
         
-        m = 2;
-        n = 4;
+        int m = 2;
+        int n = 4;
         head = sol.reverseBetween(head, m, n);
 
         System.out.println("\nafter reorder between " + m + " and " + n);
