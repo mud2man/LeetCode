48c3ee27cc2016c6777dd4b1b28318fc4a8636e8
Namo.Lu <chih-hung.lu@columbia.edu>
2019-04-11 00:01:48 -0700

    LeetCode#545 update

--- Java/545.BoundaryBinaryTree/Solution.java
+++ Java/545.BoundaryBinaryTree/Solution.java
@@ -15,12 +15,11 @@ class TreeNode {
 
 public class Solution {
     private void getLeftBoundary(TreeNode root, List<Integer> boundary){
-        if(root.left == null && root.right != null){
+        if(root!= null && root.left == null && root.right != null){
             boundary.add(root.val);
             return;
         }
-        
-        while(root.left != null || root.right != null){
+        while(root!= null && (root.left != null || root.right != null)){
             boundary.add(root.val);
             root = (root.left != null)? root.left: root.right;
         }
@@ -40,38 +39,29 @@ public class Solution {
     }
     
     private void getRightBoundary(TreeNode root, List<Integer> boundary){
-        if(root.left != null && root.right == null){
+        if(root!= null && root.left != null && root.right == null){
             return;
         }
-        
-        root = root.right;
-        List<Integer> tmp = new ArrayList<Integer>();
+        root = (root != null)? root.right: null;
+        Deque<Integer> stack = new LinkedList<Integer>();
         while(root != null && (root.left != null || root.right != null)){
-            tmp.add(root.val);
+            stack.add(root.val);
             root = (root.right != null)? root.right: root.left;
         }
-        
-        for(int i = tmp.size() - 1; i >= 0; --i){
-            boundary.add(tmp.get(i));
+        while(!stack.isEmpty()){
+            boundary.add(stack.pollLast());
         }
     }
     
     public List<Integer> boundaryOfBinaryTree(TreeNode root) {
-        if(root == null){
-          return new ArrayList<Integer>();  
-        }
-        
         List<Integer> boundary = new ArrayList<Integer>();
         getLeftBoundary(root, boundary);
         getBottomBoundary(root, boundary);
         getRightBoundary(root, boundary);
         return boundary;
     }
-
+ 
     public static void main(String[] args){
-        TreeNode root;
-        Solution sol;
-        
         /* Generate a input tree
          *     1
          *      \
@@ -79,12 +69,12 @@ public class Solution {
          *      / \
          *     3   4
          */
-        root = new TreeNode(1);
+        TreeNode root = new TreeNode(1);
         root.right = new TreeNode(2);
         root.right.left = new TreeNode(3);
         root.right.right = new TreeNode(4);
 
-        sol = new Solution();
+        Solution sol = new Solution();
         List<Integer> boundary = sol.boundaryOfBinaryTree(root);
         System.out.println("boundary: " + boundary);
     }
