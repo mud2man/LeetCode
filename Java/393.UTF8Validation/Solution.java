a78d59efee30aae7d2e66dd37be563000214a31f
Namo.Lu <chih-hung.lu@columbia.edu>
2019-04-11 21:08:20 -0700

    LeetCode#393 update

--- Java/393.UTF8Validation/Solution.java
+++ Java/393.UTF8Validation/Solution.java
@@ -5,7 +5,6 @@ import java.util.*;
 
 public class Solution {
     private boolean validate(int[] data, int start, int type){
-        start++;
         for(int i = 0; i < type; ++i){
             if(start == data.length || (data[start++] & 0xC0) != 0x80){
                 return false;
@@ -18,44 +17,35 @@ public class Solution {
         int index = 0;
         
         while(index < data.length){
-            int header = data[index];
+            int header = data[index++];
+            int payloadLen;
             if((header & 0x80) == 0x0){
-                if(!validate(data, index, 0)){
-                    return false;
-                }
-                index += 1;
+                payloadLen = 0;
             }
             else if((header & 0xE0) == 0xC0){
-                if(!validate(data, index, 1)){
-                    return false;
-                }
-                index += 2;
+                payloadLen = 1;
             }
             else if((header & 0xF0) == 0xE0){
-                if(!validate(data, index, 2)){
-                    return false;
-                }
-                index += 3;
+                payloadLen = 2;
             }
             else if((header & 0xF8) == 0xF0){
-                if(!validate(data, index, 3)){
-                    return false;
-                }
-                index += 4;
+                payloadLen = 3;
             }
             else{
                 return false;
             }
+            
+            if(!validate(data, index, payloadLen)){
+                return false;
+            }
+            index += payloadLen;
         }
-        
         return true;
     }
 
     public static void main(String[] args){
-        Solution sol;
         int[] data = {197, 130, 1};
-        sol = new Solution();
-
+        Solution sol = new Solution();
         System.out.println("data: " + Arrays.toString(data));
         System.out.println("is valid Utf-8: " + sol.validUtf8(data));
     }
