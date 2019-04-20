b0592680f69c7aebc6360e3e2b2b3788b14ed5c7
Namo.Lu <chih-hung.lu@columbia.edu>
2019-04-19 22:50:07 -0700

    LeetCode#73 update

--- Java/73.SetMatrixZeroes/Solution.java
+++ Java/73.SetMatrixZeroes/Solution.java
@@ -1,8 +1,7 @@
 /* Simple traverse: O(n*m),
  * 1. Check if the first row is zeros
  * 2. Check if the first column is zeros
- * 3. Check if zero among matrix[y][x], where x > 1, y > 1, 
- *      and put the result in matrix[0][x] and matrix[y][0]
+ * 3. Check if zero among matrix[y][x], where x > 1, y > 1, and put the result in matrix[0][x] and matrix[y][0]
  * 4. Reset matrix by matrix[0][x] and matrix[y][0] 
  */
 
@@ -12,53 +11,47 @@ public class Solution{
     public void setZeroes(int[][] matrix) {
         int depth = matrix.length;
         int width = (depth == 0)? 0: matrix[0].length;
-        boolean firstRowZeros = false;
-        boolean firstColumZeros = false;
-        
+        boolean firstRowHasZero = false;
+        boolean firstColumnHasZero = false;
         for(int y = 0; y < depth; ++y){
-            if(matrix[y][0] == 0){
-                firstColumZeros = true;
-                break;
-            }
-        }
-        
-        for(int x = 0; x < width; ++x){
-            if(matrix[0][x] == 0){
-                firstRowZeros = true;
-                break;
-            }
-        }
-        
-        for(int y = 1; y < depth; ++y){
-            for(int x = 1; x < width; ++x){
-                if(matrix[y][x] == 0){
-                    matrix[0][x] = 0;
-                    matrix[y][0] = 0;
+            for(int x = 0; x < width; ++x){
+                if(y == 0){
+                    firstRowHasZero = (matrix[y][x] == 0)? true: firstRowHasZero;
                 }
+                if(x == 0){
+                    firstColumnHasZero = (matrix[y][x] == 0)? true: firstColumnHasZero;
+                }
+                matrix[y][0] = (matrix[y][x] == 0)? 0: matrix[y][0];
+                matrix[0][x] = (matrix[y][x] == 0)? 0: matrix[0][x];
             }
         }
         
-        for(int y = 1; y < depth; ++y){
-            for(int x = 1; x < width; ++x){
-                if(matrix[y][0] == 0 || matrix[0][x] == 0){
+        for(int x = 1; x < width; ++x){
+            if(matrix[0][x] == 0){
+                for(int y = 1; y < depth; ++y){
                     matrix[y][x] = 0;
                 }
             }
         }
-        
-        for(int y = 0; y < depth; ++y){
-            if(firstColumZeros == true){
-                matrix[y][0] = 0;
+        for(int y = 1; y < depth; ++y){
+            if(matrix[y][0] == 0){
+                for(int x = 1; x < width; ++x){
+                    matrix[y][x] = 0;
+                }
             }
         }
-        
-        for(int x = 0; x < width; ++x){
-            if(firstRowZeros == true){
+        if(firstRowHasZero){
+            for(int x = 0; x < width; ++x){
                 matrix[0][x] = 0;
             }
         }
+        if(firstColumnHasZero){
+            for(int y = 0; y < depth; ++y){
+                matrix[y][0] = 0;
+            }
+        }
     }
- 
+  
     public static void main(String[] args){
         Solution sol;
         int pathNum;
