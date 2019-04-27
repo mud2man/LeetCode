0ea3e553a007207b96cf751194d4f287fcc096a4
Namo.Lu <chih-hung.lu@columbia.edu>
2019-04-27 11:03:01 -0700

    LeetCode#114 update

--- Java/114.FlattenBinaryTreeLinkedList/Solution.java
+++ Java/114.FlattenBinaryTreeLinkedList/Solution.java
@@ -1,12 +1,11 @@
-/* Recursive: O(n)
+/* Recursive: Time:O(n), Space:O(h). LeetCode has iteration solution
  * 1. Vist the tree in preOrder and recursively
  */
 
 import java.util.*;
 
 /* Definition for binary tree */
-class TreeNode 
-{
+class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
@@ -15,49 +14,36 @@ class TreeNode
 
 public class Solution {
     public TreeNode preOrder(TreeNode root){
+        if(root == null){
+            return null;
+        }
         TreeNode left = root.left;
         TreeNode right = root.right;
-        TreeNode tail = null;
-        
-        if(left == null && right == null){
-            return root;
-        }
-        
-        if(left != null){
+        TreeNode leftTail = preOrder(root.left);
+        TreeNode rightTail = preOrder(root.right);
+        if(leftTail != null){
+            leftTail.right = right;
             root.right = left;
-            root.left = null;
-            tail = preOrder(left);
         }
-        
-        if(right != null){
-            if(tail != null)
-                tail.right = right;
-            tail = preOrder(right);
-        }
-        
+        TreeNode tail = (rightTail != null)? rightTail: (leftTail != null)? leftTail: root;
+        root.left = null;
         return tail;
     }
     
     public void flatten(TreeNode root) {
-        if(root != null)
-            preOrder(root);
-    }    
-
+        preOrder(root);
+    }
+ 
     public void dump(TreeNode root){
-        if(root == null)
+        if(root == null){
             return;
-
+        }
         System.out.print(root.val + "->");
         dump(root.left);
         dump(root.right);
     }
 
     public static void main(String[] args){
-        List<List<Integer>> list;
-        TreeNode root;
-        Solution sol;
-        int sum;
-
         /* Generate a input tree
          *       5
          *      / \
@@ -68,7 +54,7 @@ public class Solution {
          * 7    2  5   1
          */
         
-        root = new TreeNode(5);
+        TreeNode root = new TreeNode(5);
         root.left = new TreeNode(4);
         root.right = new TreeNode(8);
         root.left.left = new TreeNode(11);
@@ -78,7 +64,7 @@ public class Solution {
         root.left.left.right = new TreeNode(2);
         root.right.right.left = new TreeNode(5);
         root.right.right.right = new TreeNode(1);
-        sol = new Solution();
+        Solution sol = new Solution();
         
         System.out.println("before flatten:");    
         sol.dump(root);
