727e74361dae2adfe5e7a84def52f9d8b21ff434
Namo.Lu <chih-hung.lu@columbia.edu>
2019-05-01 22:35:45 -0700

    LeetCode#298 refine

--- Java/298.BinaryTreeLongestConsecutiveSequence/Solution.java
+++ Java/298.BinaryTreeLongestConsecutiveSequence/Solution.java
@@ -1,7 +1,6 @@
-/* DFS: O(n)
- * 1. Keep the current consecutive number
- * 2. Visit left and right child, if the value of child = the value of parent + 1.
- * 3. Pass the parameter with current length + 1. Otherwise, pass with 1
+/* DFS: Time:O(n), Space:O(n)
+ * 1. return the current consecutive number, either from left or right child
+ * 2. Update longestConsecutiveLength
  */
 
 import java.util.*; // Stack
@@ -15,39 +14,28 @@ class TreeNode {
 }
 
 public class Solution {
-    private void dfs(TreeNode root, int currLen, int[] longestConsecutiveLength){
+    private int dfs(TreeNode root, int[] longestConsecutiveLength){
         if(root == null){
-            return;
+            return 0;
         }    
-        
-        longestConsecutiveLength[0] = Math.max(currLen, longestConsecutiveLength[0]);
-        
-        if(root.left != null && root.left.val == (root.val + 1)){
-            dfs(root.left, currLen + 1, longestConsecutiveLength);
-        }
-        else{
-            dfs(root.left, 1, longestConsecutiveLength);
-        }
-        
-        if(root.right != null && root.right.val == (root.val + 1)){
-            dfs(root.right, currLen + 1, longestConsecutiveLength);
-        }
-        else{
-            dfs(root.right, 1, longestConsecutiveLength);
-        }
+        int leftLength = dfs(root.left, longestConsecutiveLength);
+        int rightLength = dfs(root.right, longestConsecutiveLength);
+        int currentLength = 1;
+        leftLength = (root.left != null && root.left.val == root.val + 1)?  (1 + leftLength): 0;
+        rightLength = (root.right != null && root.right.val == root.val + 1)?  (1 + rightLength): 0;
+        currentLength = Math.max(currentLength, leftLength);
+        currentLength = Math.max(currentLength, rightLength);
+        longestConsecutiveLength[0] = Math.max(longestConsecutiveLength[0], currentLength);
+        return currentLength;
     }
     
     public int longestConsecutive(TreeNode root) {
         int[] longestConsecutiveLength = new int[1];
-        longestConsecutiveLength[0] = 0;
-        dfs(root, 1, longestConsecutiveLength);
+        dfs(root, longestConsecutiveLength);
         return longestConsecutiveLength[0]; 
     }
- 
+  
     public static void main(String[] args){
-        TreeNode root;
-        Solution sol;
-        
         /* Generate a input tree
          *     8
          *    / \
@@ -57,14 +45,12 @@ public class Solution {
          *    / \  /
          *   4   7 13 
          */
-        root = new TreeNode(1);
+        TreeNode root = new TreeNode(1);
         root.right = new TreeNode(3);
         root.right.left = new TreeNode(2);
         root.right.right = new TreeNode(4);
         root.right.right.right = new TreeNode(5);
-
-        sol = new Solution();
-        
+        Solution sol = new Solution();
         System.out.println("longestConsecutiveLength: " + sol.longestConsecutive(root));
     }
 }
