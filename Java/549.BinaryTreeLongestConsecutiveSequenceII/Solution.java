ee464fe58ef268d40b5e52103f3c25929f1420a0
Namo.Lu <chih-hung.lu@columbia.edu>
2019-05-02 23:12:35 -0700

    LeetCode#549 update

--- Java/549.BinaryTreeLongestConsecutiveSequenceII/Solution.java
+++ Java/549.BinaryTreeLongestConsecutiveSequenceII/Solution.java
@@ -17,69 +17,43 @@ class TreeNode {
 }
 
 public class Solution {
-    private int[] postOrder(int[] maxLength, TreeNode root){
+   private int[] postOrder(int[] maxLength, TreeNode root){
         //lengths[0] = max decending length starting from root
         //lengths[1] = max ascending length starting from root 
         int[] lengths = new int[2]; 
-        
         if(root == null){
             return lengths;
         }
-        
-        lengths[0] = 1;
-        lengths[1] = 1;
-        int[] leftLengths = postOrder(maxLength, root.left);
-        int[] rightLengths = postOrder(maxLength, root.right);
-        
-        if(root.left != null){
-            lengths[0] = (root.val == (root.left.val + 1))? Math.max(lengths[0], leftLengths[0] + 1): lengths[0];
-            lengths[1] = (root.val == (root.left.val - 1))? Math.max(lengths[1], leftLengths[1] + 1): lengths[1];
-        }
-        
-        if(root.right != null){
-            lengths[0] = (root.val == (root.right.val + 1))? Math.max(lengths[0], rightLengths[0] + 1): lengths[0];
-            lengths[1] = (root.val == (root.right.val - 1))? Math.max(lengths[1], rightLengths[1] + 1): lengths[1];
-        }
-       
-        maxLength[0] = Math.max(maxLength[0], lengths[0]);
-        maxLength[0] = Math.max(maxLength[0], lengths[1]);
-        
-        if(root.left != null && root.right != null){
-            if((root.left.val == root.val + 1) && (root.right.val == root.val - 1)){
-                int length = leftLengths[1] + rightLengths[0] + 1;
-                maxLength[0] = Math.max(maxLength[0], length);
-            }
-            
-            if((root.left.val == root.val - 1) && (root.right.val == root.val + 1)){
-                int length = leftLengths[0] + rightLengths[1] + 1;
-                maxLength[0] = Math.max(maxLength[0], length);
-            }
-        }
-        
+
+        int[] leftLength = postOrder(maxLength, root.left);
+        leftLength[0] = (root.left != null && root.val == root.left.val + 1)? leftLength[0]: 0;
+        leftLength[1] = (root.left != null && root.val == root.left.val - 1)? leftLength[1]: 0;
+        int[] rightLength = postOrder(maxLength, root.right);
+        rightLength[0] = (root.right != null && root.val == root.right.val + 1)? rightLength[0]: 0;
+        rightLength[1] = (root.right != null && root.val == root.right.val - 1)? rightLength[1]: 0;
+        lengths[0] = Math.max(leftLength[0] + 1, rightLength[0] + 1);
+        lengths[1] = Math.max(leftLength[1] + 1, rightLength[1] + 1);
+        maxLength[0] = Math.max(maxLength[0], (leftLength[0] + rightLength[1] + 1));
+        maxLength[0] = Math.max(maxLength[0], (rightLength[0] + leftLength[1] + 1));
         return lengths;
     }
     
     public int longestConsecutive(TreeNode root) {
-        int[] maxLength = new int[]{0};
+        int[] maxLength = new int[1];
         postOrder(maxLength, root);
-        return maxLength[0]; 
+        return maxLength[0];
     }
-
+ 
     public static void main(String[] args){
-        TreeNode root;
-        Solution sol;
-        
         /* Generate a input tree
          *     2
          *    / \
          *   1   3
          */
-        root = new TreeNode(2);
+        TreeNode root = new TreeNode(2);
         root.left = new TreeNode(1);
         root.right = new TreeNode(3);
-
-        sol = new Solution();
-        
+        Solution sol = new Solution();
         System.out.println("maximum consecutive length: " + sol.longestConsecutive(root));
     }
 }
