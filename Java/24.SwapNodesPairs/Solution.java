8a024eea0ce65f11499258a048e2148c5cc758f2
Namo.Lu <chih-hung.lu@columbia.edu>
2019-05-03 21:01:17 -0700

    LeetCode#24

--- Java/24.SwapNodesPairs/Solution.java
+++ Java/24.SwapNodesPairs/Solution.java
@@ -1,4 +1,4 @@
-/* O(n)
+/* Time:O(n), Space:O(1
  * 1. Use 4 pointers to do pointer exchange
  */
 
@@ -14,43 +14,36 @@ class ListNode {
  
 public class Solution{
     public ListNode swapPairs(ListNode head) {
-        ListNode dummy, w, x, y, z;
-        
-        dummy = new ListNode(0);
+        ListNode dummy = new ListNode(0);
         dummy.next = head;
-        
-        w = dummy;
-        x = head;
-        y = (x != null)? x.next: null;
-        z = (y != null)? y.next: null;
-        while(y != null){
-            y.next = x;
-            w.next = y;
-            x.next = z;
+        ListNode[] window = new ListNode[4];
+        window[0] = dummy;
+        window[1] = head;
+        window[2] = (window[1] != null)? window[1].next: null;
+        window[3] = (window[2] != null)? window[2].next: null;
+        while(window[2] != null){
+            window[2].next = window[1];
+            window[0].next = window[2];
+            window[1].next = window[3];
             
-            w = x;
-            x = z;
-            y = (z != null)? z.next: null;
-            z = (y != null)? y.next: null;
+            window[0] = window[1];
+            window[1] = window[3];
+            window[2] = (window[1] != null)? window[1].next: null;
+            window[3] = (window[2] != null)? window[2].next: null;
         }
-        
         return dummy.next;
     }
 
     public static void main(String[] args){
-        Solution sol;
-        ListNode head;
-        ListNode node;
-
-        sol = new Solution();
-        head = new ListNode(1);
+        Solution sol = new Solution();
+        ListNode head = new ListNode(1);
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
         
         System.out.println("before swap: ");
-        node = head;
+        ListNode node = head;
         while(node != null){    
             System.out.print(node.val + "->");
             node = node.next;
