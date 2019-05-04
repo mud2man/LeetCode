4cf9f98720c3f768aebd54b5b680a6e77bdb414a
Namo.Lu <chih-hung.lu@columbia.edu>
2019-05-03 22:22:53 -0700

    LeetCode#394 update

---
+++ Java/394.DecodeString/Recursive/Solution.java
@@ -0,0 +1,44 @@
+/* Recursive: O(n)
+ * 1. Have a global variable "index" for string traverse
+ * 2. In decode, if first character is not digit, append character until we hit ']' or digit
+ * 3. Otherwise, get the repeat time, and get the payload by calling "decode", then append payload with "times" 
+ */
+
+import java.util.*;
+
+public class Solution{
+    private String decode(int[] index, String s){
+        StringBuilder sb = new StringBuilder("");
+        while(index[0] < s.length() && s.charAt(index[0]) != ']'){
+            if(!Character.isDigit(s.charAt(index[0]))){
+                while(!Character.isDigit(s.charAt(index[0])) && s.charAt(index[0]) != ']'){
+                    sb.append(s.charAt(index[0]++));
+                }
+            }else{
+                int leftIndex = s.indexOf('[', index[0]);
+                int stratIndex = index[0];
+                index[0] = leftIndex + 1;
+                int times = Integer.parseInt(s.substring(stratIndex, leftIndex));
+                String payload = decode(index, s);
+                for(int i = 0; i < times; ++i){
+                    sb.append(payload);
+                }
+            }
+        }
+        index[0]++;
+        return sb.toString();
+    }
+    
+    public String decodeString(String s) {
+        s = 1 + "[" + s + "]";
+        int[] index = {0};
+        return decode(index, s);
+    }
+
+    public static void main(String[] args){
+        String s = "3[a]2[bc]";
+        Solution sol = new Solution();
+        System.out.println("s: " + s);
+        System.out.println("ans: " + sol.decodeString(s));
+    }
+}
