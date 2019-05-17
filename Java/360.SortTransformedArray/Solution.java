8935f20b8a70828ff5b4fea7461207d70e271497
Namo.Lu <chih-hung.lu@columbia.edu>
2019-05-17 18:25:06 +0800

    LeetCode#360 update

--- Java/360.SortTransformedArray/Solution.java
+++ Java/360.SortTransformedArray/Solution.java
@@ -1,7 +1,6 @@
 /* Two pointers: Time:O(n^2), Space:O(1)
  * 1. Have two pointers left and right
- * 2. If (a >= 0), put answer[i] from (size - 1), and compare quadratic value of left and right to determine shift which one
-      like merge sort
+ * 2. If (a >= 0), put answer[i] from (size - 1), and compare quadratic value of left and right to determine shift which one. It's like merge sort
  * 3. Otherwise, put answer[i] from 0
  */
 
@@ -14,55 +13,37 @@ public class Solution{
     
     public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
         int size = nums.length;
-        
         int[] answer = new int[size];
         int left = 0;
         int right = size - 1;
-        
         if(a >= 0){
             for(int i = size - 1; i >= 0; --i){
                 int leftValue = quadraticFunction(nums[left], a, b, c);
                 int rightValue = quadraticFunction(nums[right], a, b, c);
-                if(leftValue > rightValue){
-                    answer[i] = leftValue;
-                    left++;
-                }
-                else{
-                    answer[i] = rightValue;
-                    right--;
-                }
+                answer[i] = (leftValue > rightValue)? leftValue: rightValue;
+                left += (leftValue > rightValue)? 1: 0;
+                right -= (leftValue <= rightValue)? 1: 0;
             }
-        }
-        else{
+        }else{
             for(int i = 0; i < size; ++i){
                 int leftValue = quadraticFunction(nums[left], a, b, c);
                 int rightValue = quadraticFunction(nums[right], a, b, c);
-                if(leftValue < rightValue){
-                    answer[i] = leftValue;
-                    left++;
-                }
-                else{
-                    answer[i] = rightValue;
-                    right--;
-                }
+                answer[i] = (leftValue < rightValue)? leftValue: rightValue;
+                left += (leftValue < rightValue)? 1: 0;
+                right -= (leftValue >= rightValue)? 1: 0;
             }
         }
-        
         return answer;
     }
-
+ 
     public static void main(String[] args){
-        Solution sol;
         int[] nums = {-4, -2, 2, 4};
         int a = 1;
         int b = 3;
         int c = 5;
-
-        sol = new Solution();    
+        Solution sol = new Solution();    
         System.out.println("nums: " + Arrays.toString(nums));
-        System.out.println("a: " + a);
-        System.out.println("b: " + b);
-        System.out.println("c: " + c);
+        System.out.println("a: " + a + ", b: " + b + ", c: " + c);
         System.out.println("answer: " + Arrays.toString(sol.sortTransformedArray(nums, a, b, c)));
     }
 }
