816e0f9a1c5f0cde56a733bd8317b9f1f766b283
Namo.Lu <chih-hung.lu@columbia.edu>
2019-05-27 12:22:59 -0700

    LeetCode#249 updtae

--- Java/249.GroupShiftedStrings/Solution.java
+++ Java/249.GroupShiftedStrings/Solution.java
@@ -9,48 +9,37 @@ public class Solution{
     private String getIdentity(String s){
         char firstChar = s.charAt(0);
         int shift = 26 - (firstChar - 'a');
-        
         StringBuilder identity = new StringBuilder("");
         for(char c: s.toCharArray()){
             char identityChar = (char)((((c - 'a') + shift) % 26) + 'a');           
             identity.append(identityChar);
         }
-        
         return identity.toString();
     }
     
     public List<List<String>> groupStrings(String[] strings) {
         HashMap<String, List<String>> shiftSequencesGroupMap = new HashMap<String, List<String>>();
-        
         for(String s: strings){
             String identity = getIdentity(s);
-            if(!shiftSequencesGroupMap.containsKey(identity)){
-                shiftSequencesGroupMap.put(identity, new ArrayList<String>());
-            }
+            shiftSequencesGroupMap.putIfAbsent(identity, new ArrayList<String>());
             shiftSequencesGroupMap.get(identity).add(s);
         }
         
         List<List<String>> shiftSequencesGroup = new ArrayList<List<String>>();
-        
         for (Map.Entry<String, List<String>> entry : shiftSequencesGroupMap.entrySet()) {
             List<String> shiftSequences = entry.getValue();
             shiftSequencesGroup.add(shiftSequences);
         }
-        
         return shiftSequencesGroup;
     }
-
+ 
     public static void main(String[] args){
         Solution sol = new Solution();
         String[] strings = {"abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"};
-        List<List<String>> shiftedStrings;
-
+        
         System.out.println("strings: " + strings);
-
-        shiftedStrings = sol.groupStrings(strings);
-
         System.out.println("shifted strings: ");
-        for(List<String> strList: shiftedStrings){
+        for(List<String> strList: sol.groupStrings(strings)){
             System.out.println(strList);
         }
     }
