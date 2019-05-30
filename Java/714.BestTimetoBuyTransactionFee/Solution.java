b464f84cc2c9e028c90d62d52e658c9704990678
Namo.Lu <chih-hung.lu@columbia.edu>
2019-05-29 22:05:08 -0700

    LeetCode#714 refine

--- Java/714.BestTimetoBuyTransactionFee/Solution.java
+++ Java/714.BestTimetoBuyTransactionFee/Solution.java
@@ -1,4 +1,4 @@
-/* Dynamaic programming: O(n)
+/* Dynamaic programming: Time:O(n), Space:O(1)
  * 1. Have 2 arrays oneStockDp and noStockDp
  * 2. oneStockDp[i] = the maximum profit with one stock, when reaching the price[i] so far
  * 3. noStockDp[i] = the maximum profit without stock, when reaching the price[i] so far
@@ -15,26 +15,23 @@ import java.util.*;
 
 public class Solution{
     public int maxProfit(int[] prices, int fee) {
-        int[] oneStockDp = new int[prices.length];
-        int[] noStockDp = new int[prices.length];
+        int[] oneStockDp = new int[2];
+        int[] noStockDp = new int[2];
         
         oneStockDp[0] = -prices[0];
         noStockDp[0] = 0;
-        
         for(int i = 1; i < prices.length; ++i){
-            oneStockDp[i] = Math.max(oneStockDp[i - 1], noStockDp[i - 1] - prices[i]);
-            noStockDp[i] = Math.max(noStockDp[i - 1], oneStockDp[i - 1] + prices[i] - fee);
+            oneStockDp[i % 2] = Math.max(oneStockDp[(i - 1) % 2], noStockDp[(i - 1) % 2] - prices[i]);
+            noStockDp[i % 2] = Math.max(noStockDp[(i - 1) % 2], oneStockDp[(i - 1) % 2] + prices[i] - fee);
         }
-        
-        return noStockDp[prices.length - 1];
+        return noStockDp[(prices.length - 1) % 2]; 
     }
 
     public static void main(String[] args){
-        Solution sol;
         int[] prices = {1, 3, 2, 8, 4, 9};
         int fee = 2;
         
-        sol = new Solution();
+        Solution sol = new Solution();
         System.out.println("prices: " + Arrays.toString(prices));
         System.out.println("fee: " + fee);
         System.out.println("maximum profit: " + sol.maxProfit(prices, fee));
