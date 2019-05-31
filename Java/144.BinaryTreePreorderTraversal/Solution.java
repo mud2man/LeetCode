a63492a60bab7f0d5eeea7b95e7884de0279b2c3
Namo.Lu <chih-hung.lu@columbia.edu>
2019-05-30 20:25:01 -0700

    LeetCode#144 update

--- Java/144.BinaryTreePreorderTraversal/Solution.java
+++ Java/144.BinaryTreePreorderTraversal/Solution.java
@@ -1,11 +1,10 @@
-/* Stack: O(n)
+/* Stack: Time:O(n), Space:O(n)
  */
 
 import java.util.*; // Stack
 
 /* Definition for binary tree */
-class TreeNode 
-{
+class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
@@ -14,7 +13,7 @@ class TreeNode
 
 public class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<TreeNode>();
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
         List<Integer> preOrder = new ArrayList<Integer>();
         
         if(root == null){
@@ -22,29 +21,25 @@ public class Solution {
         }
         
         stack.push(root);
-        while(!stack.empty()){
-            TreeNode subRoot = stack.pop();
+        while(!stack.isEmpty()){
+            TreeNode subRoot = stack.pollLast();
             preOrder.add(subRoot.val);
             while(subRoot.left != null){
                 preOrder.add(subRoot.left.val);
                 if(subRoot.right != null){
-                    stack.push(subRoot.right);
+                    stack.add(subRoot.right);
                 }
                 subRoot = subRoot.left;
             }
             
             if(subRoot.right != null){
-                stack.push(subRoot.right);
+                stack.add(subRoot.right);
             }
         }
         return preOrder;
     }
-
+ 
     public static void main(String[] args){
-        List<Integer> preOrder;
-        TreeNode root;
-        Solution sol;
-        
         /* Generate a input tree
          *     3
          *    / \
@@ -52,12 +47,12 @@ public class Solution {
          *       / \
          *      15  7
          */
-        root = new TreeNode(3);
+        TreeNode root = new TreeNode(3);
         root.left = new TreeNode(9);
         root.right = new TreeNode(20);
         root.right.left = new TreeNode(15);
         root.right.right = new TreeNode(7);
-        sol = new Solution();
+        Solution sol = new Solution();
         
         System.out.println("postOrder of tree: ");
         System.out.println(sol.preorderTraversal(root));
