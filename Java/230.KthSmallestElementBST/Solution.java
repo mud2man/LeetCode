8948621dc0bebf16886aae62d95409c2a3a3a176
Namo.Lu <chih-hung.lu@columbia.edu>
2019-05-31 23:46:03 -0700

    LeetCode#230 update

--- Java/230.KthSmallestElementBST/Solution.java
+++ Java/230.KthSmallestElementBST/Solution.java
@@ -6,8 +6,7 @@
 import java.util.*; // Stack
 
 /* Definition for binary tree */
-class TreeNode 
-{
+class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
@@ -15,33 +14,23 @@ class TreeNode
 }
 
 public class Solution {
-    private int inorderTraverse(TreeNode root, int[] count, int k){
+    private TreeNode inorder(TreeNode root, int[] count, int k){
         if(root == null){
-            return 0;
+            return null;
         }
         
-        int left = inorderTraverse(root.left, count, k);
-        if(count[0] == k){
-            return left;
-        }
-        
-        if(k == ++count[0]){
-            return root.val;
-        }
-        
-        return inorderTraverse(root.right, count, k);
+        TreeNode left = inorder(root.left, count, k);
+        count[0]++;
+        return (left != null)? left: ((count[0] == k)? root: inorder(root.right, count, k));
     }
     
     public int kthSmallest(TreeNode root, int k) {
         int[] count = {0};
-        return inorderTraverse(root, count, k); 
+        TreeNode ret = inorder(root, count, k);
+        return ret.val;
     }
 
     public static void main(String[] args){
-        TreeNode root;
-        Solution sol;
-        int k = 3;
-        
         /* Generate a input tree
          *     8
          *    / \
@@ -51,7 +40,7 @@ public class Solution {
          *    / \  /
          *   4   7 13 
          */
-        root = new TreeNode(8);
+        TreeNode root = new TreeNode(8);
         root.left = new TreeNode(3);
         root.right = new TreeNode(10);
         root.left.left = new TreeNode(1);
@@ -60,8 +49,8 @@ public class Solution {
         root.left.right.left = new TreeNode(4);
         root.left.right.right = new TreeNode(7);
         root.right.right.left = new TreeNode(13);
-
-        sol = new Solution();
+        Solution sol = new Solution();
+        int k = 3;
         System.out.println(k + "th smallest: " + sol.kthSmallest(root, k));
     }
 }
