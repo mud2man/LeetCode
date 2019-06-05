6e1a47090bd301c8ddea3bc73748c5f2802d8bcb
Namo.Lu <chih-hung.lu@columbia.edu>
2019-06-04 21:38:38 -0700

    LeetCode#46 update

--- Java/46.Permutations/Solution.java
+++ Java/46.Permutations/Solution.java
@@ -5,16 +5,14 @@
 import java.util.*;
 
 public class Solution{
-    public void helper(LinkedList<Integer> remain, LinkedList<Integer> permutation, LinkedList<List<Integer>> permutations){
-        int i, size;
-        
+    public void helper(Deque<Integer> remain, Deque<Integer> permutation, List<List<Integer>> permutations){
         if(remain.isEmpty()){
             permutations.add(new LinkedList<Integer>(permutation));
             return;
         }
         
-        size = remain.size();
-        for(i = 0; i < size; ++i){
+        int size = remain.size();
+        for(int i = 0; i < size; ++i){
             int num = remain.pollFirst();
             permutation.add(num);
             helper(remain, permutation, permutations);
@@ -24,26 +22,19 @@ public class Solution{
     }
     
     public List<List<Integer>> permute(int[] nums) {
-        LinkedList<Integer> remain, permutation;
-        LinkedList<List<Integer>> permutations;
-        
-        remain = new LinkedList<Integer>();
-        permutation = new LinkedList<Integer>();
-        permutations = new LinkedList<List<Integer>>();
-        
+        Deque<Integer> remain = new LinkedList<Integer>();
+        Deque<Integer> permutation = new LinkedList<Integer>();
+        List<List<Integer>> permutations = new LinkedList<List<Integer>>();
         for(int num: nums){
             remain.add(num);
         }
-        
         helper(remain, permutation, permutations);
         return permutations;
-    }    
-    
+    }
+  
     public static void main(String[] args){
-        Solution sol;
         int[] nums = {1, 2, 3};
-        sol = new Solution();
-        
+        Solution sol = new Solution();
         System.out.println("nums: " + Arrays.toString(nums));    
         System.out.println("permutations: " + sol.permute(nums));    
     }
