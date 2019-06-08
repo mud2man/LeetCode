65b2ca28eeafff1b95a6954365caeff93cef3bb2
Namo.Lu <chih-hung.lu@columbia.edu>
2019-06-08 15:18:24 -0700

    LeetCode#986 update

--- Java/986.IntervalListIntersections/Solution.java
+++ Java/986.IntervalListIntersections/Solution.java
@@ -4,62 +4,52 @@
 
 import java.util.*;
 
-class Interval {
-    int start;
-    int end;
-    Interval() { start = 0; end = 0; }
-    Interval(int s, int e) { start = s; end = e; }
-}
-
 public class Solution{
-    public Interval[] intervalIntersection(Interval[] A, Interval[] B) {
-        List<Interval> intersections = new ArrayList<>();
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        List<int[]> intersections = new ArrayList<>();
         int ptr0 = 0;
         int ptr1 = 0;
         while(ptr0 < A.length && ptr1 < B.length){
-            Interval a = A[ptr0];
-            Interval b = B[ptr1];
+            int[] a = A[ptr0];
+            int[] b = B[ptr1];
             //check if overlap between a and b
-            if(!(a.end < b.start || b.end < a.start)){
-                intersections.add(new Interval(Math.max(a.start, b.start), Math.min(a.end, b.end)));
+            if(!(a[1] < b[0] || b[1] < a[0])){
+                intersections.add(new int[]{Math.max(a[0], b[0]), Math.min(a[1], b[1])});
             }
             
-            if(a.end >= b.end){
+            if(a[1] >= b[1]){
                 ptr1++;    
-            }
-            else{
+            }else{
                 ptr0++;
             }
         }
         
-        Interval[] ret = new Interval[intersections.size()]; 
+        int[][] ret = new int[intersections.size()][2]; 
         for(int i = 0; i < intersections.size(); ++i){
             ret[i] = intersections.get(i);
         }
         return ret;
     }
-   
+ 
     public static void main(String[] args){
         Solution sol = new Solution();
-        Interval[] A = {new Interval(0, 2), new Interval(5, 10),new Interval(13, 23), new Interval(24, 25)};
-        Interval[] B = {new Interval(1, 5), new Interval(8, 12),new Interval(15, 24), new Interval(25, 26)};
-        
+        int[][] A = {{0, 2}, {5, 10}, {13, 23}, {24, 25}};
+        int[][] B = {{1, 5}, {8, 12}, {15, 24}, {25, 26}};      
         System.out.print("A: ");
-        for(Interval i: A){
-            System.out.print("(" + i.start + ", " + i.end + "), ");
+        for(int[] a: A){
+            System.out.print(Arrays.toString(a));
         }
         System.out.println("");
         
-        System.out.print("B: ");
-        for(Interval i: B){
-            System.out.print("(" + i.start + ", " + i.end + "), ");
+        for(int[] b: B){
+            System.out.print(Arrays.toString(b));
         }
         System.out.println("");
         
-        Interval[] C = sol.intervalIntersection(A, B);
+        int[][] C = sol.intervalIntersection(A, B);
         System.out.print("C: ");
-        for(Interval i: C){
-            System.out.print("(" + i.start + ", " + i.end + "), ");
+        for(int[] c: C){
+            System.out.print(Arrays.toString(c));
         }
         System.out.println("");
     }
