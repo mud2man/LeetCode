3f0f85ee6b98faad924a2ebc806f582861089d49
Namo.Lu <chih-hung.lu@columbia.edu>
2019-06-11 19:34:46 -0700

    LeetCode#921

--- Java/921.MinimumAddMakeParenthesesValid/Solution.java
+++ Java/921.MinimumAddMakeParenthesesValid/Solution.java
@@ -1,9 +1,7 @@
-/* Greedy: Time:O(n), Space:O(1). LeetCode has one shot solution
+/* Greedy: Time:O(n), Space:O(1).
  * 1. Have an array "pair" to record the '(' number and ')' number
- * 2. Traverse from left, and treat '(' as producer, and ')' as consumer. The accumulate count with pair[1]
- * 3. Traverse from right, and treat ')' as producer, and '(' as consumer. The accumulate count with pair[0]
- * 4. The first loop is to count how many ')' not be paired
- * 5. The second loop is to count how many '(' not be paired
+ * 2. Traverse from left, and treat '(' as producer, and ')' as consumer. The accumulate count with pair[0] and pair[1]
+ * 3. pair[1] is to count how many ')' not be paired, pair[0] is the count of  how many '(' not be paired
  */
 
 import java.util.*;
@@ -21,34 +19,14 @@ public class Solution{
             else{
                 if(pair[0] > 0){
                     pair[0]--;
-                }
-                else{
+                }else{
                     pair[1]++;
                 }
             }
         }
-        
-        //')':producer, '(':consumer
-        int count = pair[1];
-        pair = new int[2];
-        for(int i = S.length() - 1; i >= 0; --i){
-            char c = S.charAt(i);
-            if(c == ')'){
-                pair[1]++;
-            }
-            else{
-                if(pair[1] > 0){
-                    pair[1]--;
-                }
-                else{
-                    pair[0]++;
-                }
-            }
-        }
-        count += pair[0];
-        return count;
+        return pair[0] + pair[1];
     }
-     
+  
     public static void main(String[] args){
         Solution sol = new Solution();
         String S = "()))((";
