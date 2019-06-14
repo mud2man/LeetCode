9ef6ece740a682cb525fd23464cd46bd20e9a513
Namo.Lu <chih-hung.lu@columbia.edu>
2019-06-13 19:10:52 -0700

    LeetCode#137 update

--- Java/137.SingleNumberII/Solution.java
+++ Java/137.SingleNumberII/Solution.java
@@ -7,19 +7,16 @@ import java.util.*; // Stack
 
 public class Solution {
     public int singleNumber(int[] nums) {
-        int single = 0;
-        int[] digit = new int [32];
+        int[] bitCount = new int[32];
         for(int num: nums){
-            for(int i = 0; i < 32; ++i){
-                digit[i] += ((num & (1 << i)) != 0)? 1: 0;
+            for(int i = 0, base = 1; i < 32; ++i, base *= 2){
+                bitCount[i] += ((num & base) != 0)? 1: 0;
             }
         }
         
-        for(int j = 0, base = 1; j < 32; ++j){
-            if(digit[j] % 3 != 0){
-                single = single + base;
-            }
-            base = base << 1;
+        int single = 0;
+        for(int i = 0, base = 1; i < 32; ++i, base *= 2){
+            single += (bitCount[i] % 3 == 1)? base: 0;
         }
         return single;
     }
