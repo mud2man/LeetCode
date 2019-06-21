80b0328db02426e21dceda20505d5432fbc9c864
Namo.Lu <chih-hung.lu@columbia.edu>
2019-06-20 22:08:21 -0700

    LeetCode#315 update

--- Java/315.CountSmallerNumbersAfterSelf/MergeSort/Solution.java
+++ Java/315.CountSmallerNumbersAfterSelf/MergeSort/Solution.java
@@ -1,70 +1,55 @@
 /* Merge Sort: Time:O(nlogn), Space:O(n)
- * 1. Transform nums to nodes, where node[0] = nums[i], node[1] = i, nodes[2] = count of smaller numbers after self
- * 2. Use merge sort to accumulate nodes[i][2], in the case of start <= i <= mid < j <= end
- * 3. Transform nodes by the given information index = nodes[i][1]
+ * 1. Transform nums to pairs, where pair[0] = nums[i], pair[1] = i,
+ * 2. Use merge sort to accumulate counts[i], in the case of start <= i <= mid < j <= end
  */
 
 import java.util.*;
 
 public class Solution{
-    private void mergeSort(int[][] nodes, int start, int end){
+    private List<int[]> mergeSort(List<int[]> pairs, int start, int end, List<Integer> counts){
+        List<int[]> mergedPairs = new ArrayList<>();
         if(start >= end){
-            return;
+            if(start == end){
+              mergedPairs.add(pairs.get(start));  
+            }
+            return mergedPairs;
         }
-        
         int mid = (start + end) / 2;
-        mergeSort(nodes, start, mid);
-        mergeSort(nodes, mid + 1, end);
-        Deque<int[]> queue = new LinkedList<>();
-        int i = start;
-        int j = mid + 1;
-        int count = 0;
-        while(i <= mid || j <= end){
-            int[] left = (i <= mid)? nodes[i]: null;
-            int[] right = (j <= end)? nodes[j]: null;
-            if(left != null && right != null){
-                if(left[0] > right[0]){
-                    count++;
-                    j++;
-                    queue.add(right);
-                }
-                else{
-                    i++;
-                    left[2] += count;
-                    queue.add(left);
-                }
-            }
-            else if(left == null){
-                j++;
-                queue.add(right);
-            }
-            else{
-                i++;
-                left[2] += count;
-                queue.add(left);
+        List<int[]> leftPairs = mergeSort(pairs, start, mid, counts);
+        List<int[]> rightPairs = mergeSort(pairs, mid + 1, end, counts);
+        for(int i = 0, leftIdx = 0, rightIdx = 0; i < end - start + 1; ++i){
+            int[] leftPair = (leftIdx < leftPairs.size())? leftPairs.get(leftIdx): null;
+            int[] rightPair = (rightIdx < rightPairs.size())? rightPairs.get(rightIdx): null;
+            if(leftPair == null){
+                mergedPairs.add(rightPair);
+                rightIdx++;
+            }else if(rightPair == null){
+                mergedPairs.add(leftPair);
+                leftIdx++;
+            }else if(leftPair[0] > rightPair[0]){
+                int count = rightPairs.size() - rightIdx;
+                int idx = leftPair[1];
+                counts.set(idx, counts.get(idx) + count);
+                mergedPairs.add(leftPair);
+                leftIdx++;
+            }else{
+                mergedPairs.add(rightPair);
+                rightIdx++;
             }
         }
-        
-        for(int k = start; k <= end; ++k){
-            nodes[k] = queue.pollFirst();
-        }
+        return mergedPairs;
     }
     
     public List<Integer> countSmaller(int[] nums) {
-        int[][] nodes = new int[nums.length][3];
+        List<int[]> pairs = new ArrayList<>();
         List<Integer> counts = new ArrayList<>();
         for(int i = 0; i < nums.length; ++i){
-            nodes[i][0] = nums[i];
-            nodes[i][1] = i;
+            pairs.add(new int[]{nums[i], i});
             counts.add(0);
         }
-        mergeSort(nodes, 0, nodes.length - 1);
-        for(int[] node: nodes){
-            counts.set(node[1], node[2]);
-        }
+        mergeSort(pairs, 0, pairs.size() - 1, counts);
         return counts;
-    }
-
+    } 
     public static void main(String[] args){
         int[] nums = {5, 2, 6, 1};
         Solution sol = new Solution();
