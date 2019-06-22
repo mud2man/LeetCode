31ef59407e3af3fd0f0a409d32708dce28124eb2
Namo.Lu <chih-hung.lu@columbia.edu>
2019-06-22 15:50:13 -0700

    LeetCode#324 update

--- Java/324.WiggleSortII/Solution.java
+++ Java/324.WiggleSortII/Solution.java
@@ -1,4 +1,4 @@
-/* Select: Time:O(n), Space:O(n). LeetCode hase O(n) + O(1) solution
+/* Select: Time:O(n), Space:O(n). LeetCode hase O(n) + O(1) solution, but it's too tricky to do it
  * 1. Apply selectKth to find the median, s.t. array => l0, l1, m2, m3, s4, s5
  * 2. Interleave the array with the two half, s.t. array => m3, l0, s4, l1, s5, m2
  */
@@ -6,71 +6,53 @@
 import java.util.*;
 
 public class Solution{
-    private void swap(int[] nums, int index0, int index1){
-        int tmp = nums[index0];
-        nums[index0] = nums[index1];
-        nums[index1] = tmp;
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
     
-    private void selectKth(int[] nums, int start, int end, int medianIndex){
-        if(medianIndex < start || medianIndex > end ){
-            return;
-        }
-        else{
-            int ptr0 = start -1;
-            int ptr1 = start -1;
-            int target = nums[end];
-            for(int i = start; i <= end; ++i){
-                if(target == nums[i]){
-                    swap(nums, ++ptr1, i);
-                }
-                else if(target < nums[i]){
-                    swap(nums, ++ptr1, i);
-                    swap(nums, ++ptr0, ptr1);
-                }
-                else{
+    private int selectKthLargest(int[] nums, int k, int start, int end){
+        while(start < end){
+            int biggerTailIdx = start - 1;
+            int equalTailIdx = start - 1;
+            int pivot = nums[end];
+            for(int i = start; i < end; ++i){
+                int num = nums[i];
+                if(num > pivot){
+                    swap(nums, i, ++equalTailIdx);
+                    swap(nums, equalTailIdx, ++biggerTailIdx);
+                }else if(num == pivot){
+                    swap(nums, i, ++equalTailIdx);
+                }else{
                     continue;
                 }
             }
-
-            if(medianIndex <= ptr0){
-                selectKth(nums, start, ptr0, medianIndex);
-            }
-            else if(medianIndex > ptr1){
-                selectKth(nums, ptr1 + 1, end, medianIndex);
-            }
-            else{
-                return;
+            swap(nums, ++equalTailIdx, end);
+            if(equalTailIdx == k){
+                return nums[k];
+            }else if(equalTailIdx > k){
+                end = equalTailIdx - 1;
+            }else{
+                start = equalTailIdx + 1;
             }
         }
+        return nums[start];
     }
     
     public void wiggleSort(int[] nums) {
-        int size = nums.length;
-        int medianIndex = size / 2;
-        
-        selectKth(nums, 0 , size - 1, medianIndex);
-        System.out.println(Arrays.toString(nums));
-        
-        int[] tmp = new int[size];
-        System.arraycopy(nums, 0, tmp, 0, size);
-        
-        int index = 0;
-        for(int i = 1; i < size; i += 2){
-            nums[i] = tmp[index++];
+        int median = selectKthLargest(nums, (nums.length - 1) / 2, 0, nums.length - 1);
+        int[] temp = new int[nums.length];
+        for(int i = 0, j = 0, k = nums.length / 2; i < temp.length; ++i){
+            temp[i] =(i % 2 == 0)? nums[k++]: nums[j++];
         }
-        
-        for(int i = 0; i < size; i += 2){
-            nums[i] = tmp[index++];
-        }
+        System.arraycopy(temp, 0, nums, 0, nums.length);
     }
-
+ 
     public static void main(String[] args){
-        Solution sol;
         int[] nums = {1, 5, 1, 1, 6, 4};
-
         System.out.println("nums @ before: " + Arrays.toString(nums));
-        sol = new Solution();    
+        Solution sol = new Solution();    
         sol.wiggleSort(nums);
         System.out.println("nums @ after: " + Arrays.toString(nums));
     }
