7da1c4473494df88deb2fa0d4ad9f7fe348566c3
Namo.Lu <chih-hung.lu@columbia.edu>
2019-06-22 20:02:06 -0700

    LeetCode#317 refine

--- Java/317.ShortestDistanceAllBuildings/Solution.java
+++ Java/317.ShortestDistanceAllBuildings/Solution.java
@@ -1,18 +1,16 @@
 /* Time:O(n*m*k), Space:O(n*m), where n is row#, m is column#, k is building#
- * 1. Have 2D array passed[y][x] to record the number of building can be reached from the position (y, x)
+ * 1. Decrease grid[y][x] if it can be reached, and we only step on the position with grid[y][x] == hitCount
  * 2. Have 2D array distance[y][x] to record the distance to reach all buildings from the position (y, x)
- * 3. Use BFS from all buildings, and record how many buildings can be reached.
- * 4. If reachCount != targetNumber from building A, we return -1, because there doesn't exist a position can reach A and others
- * 5. Get the minimum distance from the 2D array "distance"
+ * 3. Use BFS from all buildings
+ * 4. Get the minimum distance from the 2D array "distance", with grid[y][x] == -k
  */         
 
 import java.util.*;
 
 public class Solution {
-    private void bfs(int[][] grid, int startY, int startX, int[][] hitCount, int[][] distance){
+    private void bfs(int[][] grid, int startY, int startX, int[][] distance, int hitCount){
         int depth = grid.length;
         int width = grid[0].length;
-        boolean[][] visited = new boolean[depth][width];
         Deque<int[]> queue = new LinkedList<>();
         queue.add(new int[]{startY, startX});
         int radius = 0;
@@ -25,10 +23,9 @@ public class Solution {
                 for(int[] shift: shifts){
                     int y = curr[0] + shift[0];
                     int x = curr[1] + shift[1];
-                    if(y >= 0 && y < depth && x >= 0 && x < width && grid[y][x] == 0 && !visited[y][x]){
+                    if(y >= 0 && y < depth && x >= 0 && x < width && grid[y][x] == hitCount){
                         distance[y][x] += radius;
-                        hitCount[y][x]++;
-                        visited[y][x] = true;
+                        grid[y][x]--;
                         queue.add(new int[]{y, x});
                     }
                 }
@@ -40,12 +37,11 @@ public class Solution {
         int depth = grid.length;
         int width = grid[0].length;
         int[][] distance = new int[depth][width];
-        int[][] hitCount = new int[depth][width];
         int k = 0;
         for(int y = 0; y < depth; ++y){
             for(int x = 0; x < width; ++x){
                 if(grid[y][x] == 1){
-                    bfs(grid, y, x, hitCount, distance);
+                    bfs(grid, y, x, distance, -k);
                     ++k;
                 }
             }
@@ -54,7 +50,7 @@ public class Solution {
         int min = Integer.MAX_VALUE;
         for(int y = 0; y < depth; ++y){
             for(int x = 0; x < width; ++x){
-                if(hitCount[y][x] == k){
+                if(grid[y][x] == -k){
                     min = Math.min(min, distance[y][x]); 
                 }
             }
