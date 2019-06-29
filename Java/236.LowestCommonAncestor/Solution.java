fcceef5d535cdc1f8087bd8b94f8bf1f33a1c25d
Namo.Lu <chih-hung.lu@columbia.edu>
2019-06-28 18:09:03 -0700

    LeetCode#236 update

--- Java/236.LowestCommonAncestor/Solution.java
+++ Java/236.LowestCommonAncestor/Solution.java
@@ -19,22 +19,21 @@ public class Solution {
         if(root == null){
             return null;
         }
-        
-        if(root == p){
-            return p;
+        else if(root == p || root == q){
+            return root;
         }
-        else if(root == q){
-            return q;
+        
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if(left != null && right != null){
+            return root;
+        }
+        else if(left == null){
+            return right;
         }
         else{
-            TreeNode left = lowestCommonAncestor(root.left, p, q);
-            TreeNode right = lowestCommonAncestor(root.right, p, q);
-            
-            if(left != null && right != null){
-                return root;
-            }
-            
-            return (left != null)? left: right;
+            return left;
         }
     }
  
