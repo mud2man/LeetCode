199d6a0ee59859ae3e7a538346d5944cc94a5629
Namo.Lu <chih-hung.lu@columbia.edu>
2019-06-28 21:44:21 -0700

    LeetCode#410 update

--- Java/410.SplitArrayLargestSum/DynamicProgramming/Solution.java
+++ Java/410.SplitArrayLargestSum/DynamicProgramming/Solution.java
@@ -17,45 +17,33 @@ public class Solution {
         int depth = m + 1;
         int[][] dp = new int[depth][width];
         
-        int sum = 0;
-        for(int x = 0; x < width; ++x){
+        for(int x = 0, sum = 0; x < width; ++x){
             sum += nums[x];
             dp[1][x] = sum;
         }
         
         for(int y = 2; y < depth; ++y){
-            int x;
             int max = nums[0];
-            for(x = 0; x < y; ++x){
+            for(int x = 0; x < y; ++x){
                 max = Math.max(max, nums[x]);   
             }
-            
             dp[y][y - 1] = max;
-            for(x = y; x < width; ++x){
-                int lastSum = 0;
+            for(int x = y; x < width; ++x){
                 int MinimumMaxSum = Integer.MAX_VALUE;
-                for(int z = x; z >= (y - 1); --z){
+                for(int z = x, lastSum = 0; z >= (y - 1); --z){
                     lastSum += nums[z];
-                    if(z > 0){
-                        MinimumMaxSum = Math.min(MinimumMaxSum, Math.max(lastSum, dp[y - 1][z - 1]));
-                    }
-                    else{
-                        MinimumMaxSum = Math.min(MinimumMaxSum, lastSum);
-                    }
+                    MinimumMaxSum = Math.min(MinimumMaxSum, Math.max(lastSum, dp[y - 1][z - 1]));
                 }
                 dp[y][x] = MinimumMaxSum;
             }
         }
-        
         return dp[depth - 1][width - 1];
     }
-
+ 
     public static void main(String[] args){
-        Solution sol;
         int[] nums = {7, 2, 5, 10, 8};
         int m = 2;
-        
-        sol = new Solution();
+        Solution sol = new Solution();
         System.out.println("nums:" + Arrays.toString(nums));
         System.out.println("2:" + m);
         System.out.println("minimum largest sum: " + sol.splitArray(nums, m));
