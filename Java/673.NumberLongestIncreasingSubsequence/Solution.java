7bab12b2549f9a7535d5764faf7cd72606165179
Namo.Lu <chih-hung.lu@columbia.edu>
2019-07-13 14:28:24 -0700

    LeetCode#673 refine

--- Java/673.NumberLongestIncreasingSubsequence/Solution.java
+++ Java/673.NumberLongestIncreasingSubsequence/Solution.java
@@ -1,55 +1,46 @@
 /* Dynamic Programming: Time:O(n^2), Space: O(n)
- * 1. dp[i] store the node, where dp[i].length = the maximum length of subsequence ends with nums[i], dp[i].count = the number of 
-      subsequence with length dp[i].length
- * 2. In each loop, traverse from 0 to (i - 1), and update dp[i] when nums[i] > nums[j]
- * 3. Finally, accumulate the count of all node in dp, where dp[i].length == maxLength
+ * 1. num2LengthAndCount[i][0] = the max length of subsequence ending with nums[i], the count of the longest subsequence ending with nums[i]
+ * 2. In each loop, traverse from 0 to (i - 1), and update maxLength when nums[tail] > nums[i]
+ * 3. Traverse again to accumulate num2LengthAndCount[tail][1], when nums[i] < nums[tail] and num2LengthAndCount[i][0] == maxLength
+ * 4. Finally, accumulate the count of all node in num2LengthAndCount, where num2LengthAndCount[i][0] == maxLength
  * 
  * ex: nums = {1, 3, 5, 4, 7}
- *     dp[] = {{1, 1}, {2, 1}, {3, 1}, {3, 1}, {4, 2}}
+ *     num2LengthAndCount[] = {{1, 1}, {2, 1}, {3, 1}, {3, 1}, {4, 2}}
  */         
 
 import java.util.*;
 
 public class Solution {
-    private class Node{
-        int length;
-        int count;
-        Node(int l, int c){length = l; count = c;}
-    }
-    
     public int findNumberOfLIS(int[] nums) {
-        int maxLength = 0;
-        Node[] dp = new Node[nums.length];
-        
-        for(int i = 0; i < nums.length; ++i){
-            Node node = new Node(1, 1);
-            for(int j = 0; j < i; ++j){
-                if(nums[i] > nums[j]){
-                    if(node.length == (dp[j].length + 1)){
-                        node.count += dp[j].count;
-                    }
-                    else if(node.length <= dp[j].length){
-                        node.length = dp[j].length + 1;
-                        node.count = dp[j].count;
-                    }
-                    else{
-                        continue;
-                    }
+        int[][] num2LengthAndCount = new int[nums.length][2]; //{length, count}
+        int golbalMaxLength = 0;
+        for(int tail = 0; tail < nums.length; ++tail){
+            int maxLength = 0;
+            for(int i = tail - 1; i >= 0; --i){
+                if(nums[i] < nums[tail]){
+                    maxLength = Math.max(maxLength, num2LengthAndCount[i][0]);
                 }
             }
-                     
-            maxLength = Math.max(maxLength, node.length);
-            dp[i] = node;
+            num2LengthAndCount[tail][0] = maxLength + 1;
+            if(maxLength > 0){
+                for(int i = tail - 1; i >= 0; --i){
+                    if(nums[i] < nums[tail]) {
+                        num2LengthAndCount[tail][1] += (num2LengthAndCount[i][0] == maxLength)? num2LengthAndCount[i][1]: 0;
+                    }
+                }
+            }else{
+                num2LengthAndCount[tail][1] = 1;
+            }
+            golbalMaxLength = Math.max(golbalMaxLength, num2LengthAndCount[tail][0]);
         }
         
         int count = 0;
-        for(Node node: dp){
-            count += (node.length == maxLength)? node.count: 0;
+        for(int i = 0; i < nums.length; ++i){
+            count += (num2LengthAndCount[i][0] == golbalMaxLength)? num2LengthAndCount[i][1]: 0;
         }
-        
         return count;
     }
- 
+  
     public static void main(String[] args){
         Solution sol;
         int[] nums = {1, 3, 5, 4, 7};
