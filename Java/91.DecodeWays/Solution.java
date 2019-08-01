0b5364dea8650f3e0868ad72a1a1a811cf477b24
Namo.Lu <chih-hung.lu@columbia.edu>
2019-07-31 21:30:26 -0700

    LeetCode#91 refine

--- Java/91.DecodeWays/Solution.java
+++ Java/91.DecodeWays/Solution.java
@@ -1,4 +1,4 @@
-/* Dynamic programming: O(n)
+/* Dynamic programming: Time:O(n), Space:O(1)
  * 1. dp[i] = the number of decode way from 0-th character to i-th character
  * 2. if s.charAt(i) != '0', dp[i] += dp[i - 1]
  * 3. if s.charAt(i - 1) != '0' && s.substring(i - 1, i + 1) >= 26, dp[i] += dp[i - 2]
@@ -16,31 +16,26 @@ public class Solution {
             return 0;
         }
         
-        int[] dp = new int [s.length()];
+        int[] dp = new int [3];
         dp[0] = 1;
         for(int i = 1; i < s.length(); ++i){
-            dp[i] = 0;
+            dp[i % 3] = 0;
             //single digit
             if(s.charAt(i) != '0'){
-                dp[i] += dp[i - 1];
+                dp[i % 3] += dp[(i - 1) % 3];
             }
-            
             //double digit
             int num = Integer.parseInt(s.substring(i - 1, i + 1));
             if(num <= 26 && s.charAt(i - 1) != '0'){
-                dp[i] += (i - 2 >= 0)? dp[i - 2]: 1;
+                dp[i % 3] += (i - 2 >= 0)? dp[(i - 2) % 3]: 1;
             }
         }
-        return dp[s.length() - 1];
+        return dp[(s.length() - 1) % 3];
     }
- 
+  
     public static void main(String[] args){
-        Solution sol;
-        String s;
-        
-        s = "1234";
-        sol = new Solution();
-
+        Solution sol = new Solution();
+        String s = "1234";
         System.out.println("s: " + s);
         System.out.println("number of Decode Ways: " + sol.numDecodings(s));
     }
