d4ed70a93689d5cc4f5de704cbe6028e5384b2a3
Namo.Lu <chih-hung.lu@columbia.edu>
2019-08-01 22:55:37 -0700

    LeetCode#10 refine

--- Java/10.RegularExpressionMatching/Solution.java
+++ Java/10.RegularExpressionMatching/Solution.java
@@ -3,7 +3,7 @@
  * 2. Initialize dp[0][x], and dp[y][0]
  * 3. If charP == '.' or charP == charS, dp[y][x] = dp[y - 1][x - 1]
  * 4. If charP == '*', and (charP != charS and charP != '.'), dp[y][x] = dp[y - 2][x] 
- * 5. If charP == '*', and (charP == charS or charP != '.'), dp[y][x] = dp[y][x - 1](map more than once) | dp[y - 1][x - 1](map once) | dp[y - 2][x](map zero)
+ * 5. If charP == '*', and (charP == charS or charP != '.'), dp[y][x] = dp[y][x - 1](map ore or more) | dp[y - 2][x](map zero)
  * 6. Otherwise, dp[y][x] = false
  * 
  * ex: s = "aab", p = "c*a*b"
@@ -21,52 +21,42 @@ import java.util.*;
 
 public class Solution{
     public boolean isMatch(String s, String p) {
-        int depth = p.length() + 1;
-        int width = s.length() + 1;
-        boolean[][] dp = new  boolean[depth][width];
-        
+        boolean[][] dp = new boolean[p.length() + 1][s.length() + 1];
         dp[0][0] = true;
-        for(int x = 1; x < width; ++x){
+        for(int x = 1; x < dp[0].length; ++x){
             dp[0][x] = false;
         }
         
-        for(int y = 1; y < depth; ++y){
+        for(int y = 1; y < dp.length; ++y){
             dp[y][0] = (p.charAt(y - 1) != '*')? false: dp[y - 2][0];
         }
         
-        for(int y = 1; y < depth; ++y){
-            for(int x = 1; x < width; ++x){
+        for(int y = 1; y < dp.length; ++y){
+            for(int x = 1; x < dp[0].length; ++x){
                 char charS = s.charAt(x - 1);
                 char charP = p.charAt(y - 1);
-                
                 if(charP == '*'){
-                    charP = p.charAt(y - 2);
-                    if(charP != charS && charP != '.'){
+                    char prevCharP = p.charAt(y - 2);
+                    if(prevCharP != charS && prevCharP != '.'){
                         dp[y][x] = dp[y - 2][x];
+                    }else{
+                        //use one or more previous p | remove previous p
+                        dp[y][x] = dp[y][x - 1] | dp[y - 2][x]; 
                     }
-                    else{
-                        //not use p | use more than 1 p | use 1 p | remove previous p
-                        dp[y][x] = dp[y - 1][x] | dp[y][x - 1] | dp[y - 1][x - 1] | dp[y - 2][x]; 
-                    }
-                }
-                else if(charP == '.' || charP == charS){
+                }else if(charP == '.' || charP == charS){
                     dp[y][x] = dp[y - 1][x - 1];
-                }
-                else{
+                }else{
                     dp[y][x] = false;
                 }
             }
         }
-        
-        return dp[depth - 1][width - 1];
+        return dp[dp.length - 1][dp[0].length - 1];
     }
  
     public static void main(String[] args){
-        Solution sol;
         String s = "aab";
         String p = "c*a*b"; 
-        
-        sol = new Solution();
+        Solution sol = new Solution();
         System.out.println("s: " + s);
         System.out.println("p: " + p);
         System.out.println("is match: " + sol.isMatch(s, p));
