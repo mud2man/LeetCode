84033639a3b9045e8bce26283e335d9efb7286df
Namo.Lu <chih-hung.lu@columbia.edu>
2019-08-06 21:34:55 -0700

    LeetCode#632 update

--- Java/632.SmallestRange/Solution.java
+++ Java/632.SmallestRange/Solution.java
@@ -1,8 +1,9 @@
 /* Slide window + min heap: Time:O(nlogm), Space:O(m), where m is the nums's size
- * 1. Have min heaps "candidates", and "window"
- * 2. Have "values" to record the value in "window", where values[i] is the number from nums[i]
- * 3. Everytime, pick the smallest number "top" from candidates, and update range[1] 
- * 4. Update values[i] with "top", and shift range[0] to right by "window.peek()"
+ * 1. Have a min heap "candidates"
+ * 2. In the begining, put the first num from each list
+ * 3. Everytime, take out the smallest number "top" from candidates, and put in its successor
+ * 4. Update range[0/1]
+ * 5. Repeat step3/4 as long as the candidates's size is equal to K
  */
 
 import java.util.*;
@@ -24,40 +25,31 @@ public class Solution{
     public int[] smallestRange(List<List<Integer>> nums) {
         int length = nums.size();
         int[] range = {Integer.MAX_VALUE, Integer.MIN_VALUE};
-        int[] values = new int[length];
         PriorityQueue<Cell> candidates = new PriorityQueue<>(new MinHeapComparator());
-        PriorityQueue<Integer> window = new PriorityQueue<>();
         
         for(int i = 0; i < length; ++i){
             int num = nums.get(i).get(0);
-            values[i] = num;
             range[0] = Math.min(range[0], num);
             range[1] = Math.max(range[1], num);
-            window.add(num);
-            if(nums.get(i).size() > 1){
-                candidates.add(new Cell(nums.get(i).get(1), i, 1));
-            }
+            candidates.add(new Cell(num, i, 0));
         }
         
         int[] minRange = {range[0], range[1]};
-        while(!candidates.isEmpty()){
+        while(candidates.size() == length){
             Cell top = candidates.poll();
-            if(nums.get(top.outerIdx).size() > top.innerIdx + 1){
-                candidates.add(new Cell(nums.get(top.outerIdx).get(top.innerIdx + 1), top.outerIdx, top.innerIdx + 1));
+            if(top.innerIdx == nums.get(top.outerIdx).size() - 1 || candidates.isEmpty()){
+                break;
             }
-            
-            window.add(top.num);
-            range[1] = (top.num > range[1])? top.num: range[1];
-            window.remove(values[top.outerIdx]);
-            values[top.outerIdx] = top.num;
-            range[0] = window.peek();
-
+            range[0] = candidates.peek().num;
+            Cell next = new Cell(nums.get(top.outerIdx).get(top.innerIdx + 1), top.outerIdx, top.innerIdx + 1);
+            candidates.add(next);
+            range[1] = Math.max(range[1], next.num);
+            range[0] = Math.min(range[0], next.num);
             if(range[1] - range[0] < minRange[1] - minRange[0]){
                 minRange[0] = range[0];
                 minRange[1] = range[1];
             }
         }
-        
         return minRange;
     }
 
