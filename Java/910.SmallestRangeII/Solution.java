4bbc328bc16540497646089eaec61d5ebb2b6de6
Namo.Lu <chih-hung.lu@columbia.edu>
2019-09-11 22:43:42 -0700

    LeetCode#910 with proof

--- Java/910.SmallestRangeII/Solution.java
+++ Java/910.SmallestRangeII/Solution.java
@@ -2,6 +2,11 @@
  * 1. Decrease A by K, and sort
  * 2. Separete A to 2 parts "left" and "right" with adding 2 * K to all value in "left", while keeping "right" unchanged
  * 3. Get the range of "left" and "right", and update the global range
+ *
+ * Proof: After sorting, A[0] <= A[1] <= ... <= A[A.length - 1] 
+ *        If A[i] is the peak with the minimum range, then all the left neighbors of A[i] should be added with K since it can make them closer to the peak.
+ *        And it will only get the better answer. We can claim A[i + 1] must be decreased by K, since A[i] is the peak. So as all number on the right side. 
+ *        So we can traverse from left of A, and increase A[i] with 2 * K
  */
 
 import java.util.*; // Stack
