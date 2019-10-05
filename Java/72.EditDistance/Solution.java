19497d55c8c33050daa8a8e9cba3b1659c8816ea
Namo.Lu <chih-hung.lu@columbia.edu>
2019-10-04 21:24:46 -0700

    LeetCode#72

--- Java/72.EditDistance/Solution.java
+++ Java/72.EditDistance/Solution.java
@@ -1,60 +1,35 @@
 /* Dynamic programming: O(n*m)
- * 1. Have a 2D array, dp[y][x] = the minimum steps of tarnsfer word1.substring(y) to word2.substring(x);
- * 2. If y==0 / x==0
- *   2.1. If word1.charAt(y)!=word2.charAt(x), then dp[y][x]=dp[y][x - 1] + 1 / dp[y - 1][x] + 1;
- *   2.2  If word1.charAt(y)==word2.charAt(x)
- *       2.2.1 If dp[y][x - 1]==x / dp[y - 1][x]==y (the current character never overlap), then dp[y][x]=dp[y][x - 1] / dp[y - 1][x]
- *       2.2.2 Else, dp[y][x] = dp[y][x - 1] + 1 / dp[y][x] = dp[y - 1][x] + 1
- * 3. Otherwise
- *   3.1 If word1Char==word2Char, dp[y][x]=min(dp[y - 1][x] + 1, dp[y][x - 1] + 1, dp[y - 1][x - 1])
- *   3.2 Else, dp[y][x]=min(dp[y - 1][x] + 1, dp[y][x - 1] + 1, dp[y - 1][x - 1] + 1)
+ * 1. Transform word1 to xword1, and eord2 to xword2
+ * 1. Have a 2D array, dp[y][x] = the minimum steps of tarnsfer word1.substring(y + 1) to word2.substring(x + 1);
  * 
- * ex: word1=ab, word2=bac
- * dp[][] = [[1, 1, 2],
- *           [1, 2, 2]]
+ * ex: word1=ros, word2=horse => word1=xros, word2=xhorse
+ * dp[][] = [[0, 1, 2, 3, 4, 5],
+ *           [1, 1, 2, 2, 3, 4],
+ *           [2, 2, 1, 2, 3, 4],
+ *           [3, 3, 2, 3, 2, 3]]
  */
 
 import java.util.*;
 
 public class Solution{
-        public int minDistance(String word1, String word2) {
-        if(word1.length() == 0 || word2.length() == 0){
-            return Math.max(word1.length(), word2.length());
-        }
-        
-        int depth = word1.length();
-        int width = word2.length();
+    public int minDistance(String word1, String word2) {
+        int depth = word1.length() + 1;
+        int width = word2.length() + 1;
         int[][] dp = new int[depth][width];
         for(int y = 0; y < depth; ++y){
             for(int x = 0; x < width; ++x){
-                if(y == 0 && x == 0){
-                    if(word1.charAt(y) == word2.charAt(x)){
-                        dp[y][x] = 0;
-                    }
-                    else{
-                        dp[y][x] = 1;
-                    }
-                }
-                else if(y == 0){
-                    if(word1.charAt(y) == word2.charAt(x)){
-                        dp[y][x] = x;
-                    }
-                    else{
-                        dp[y][x] = dp[y][x - 1] + 1;
-                    }
-                }
-                else if(x == 0){
-                    if(word1.charAt(y) == word2.charAt(x)){
-                        dp[y][x] = y;
-                    }
-                    else{
-                        dp[y][x] = dp[y - 1][x] + 1;
-                    }
-                }
-                else{
-                    dp[y][x] = (word1.charAt(y) == word2.charAt(x))? dp[y - 1][x - 1]: dp[y - 1][x - 1] + 1;
-                    dp[y][x] = Math.min(dp[y][x], dp[y][x - 1] + 1);
-                    dp[y][x] = Math.min(dp[y][x], dp[y - 1][x] + 1); 
+                if(x == 0 && y == 0){
+                    dp[y][x] = 0;
+                }else if(x == 0){
+                    dp[y][x] = y;
+                }else if(y == 0){
+                    dp[y][x] = x;
+                }else{
+                    char word1Char = word1.charAt(y - 1);
+                    char word2Char = word2.charAt(x - 1);
+                    dp[y][x] = (word1Char == word2Char)? dp[y - 1][x - 1]: dp[y - 1][x - 1] + 1; //replce
+                    dp[y][x] = Math.min(dp[y][x], dp[y - 1][x] + 1); // delete word1Char
+                    dp[y][x] = Math.min(dp[y][x], dp[y][x - 1] + 1); // add word2Char
                 }
             }
         }
@@ -62,12 +37,9 @@ public class Solution{
     }
   
     public static void main(String[] args){
-        Solution sol;
-        String word1 = "ab";
-        String word2 = "bac";
-        
-        sol = new Solution();
-        
+        String word1 = "ros";
+        String word2 = "horse";
+        Solution sol = new Solution();
         System.out.println("word1: " + word1);
         System.out.println("word2: " + word2);
         System.out.println("minimum distance: " + sol.minDistance(word1, word2));
