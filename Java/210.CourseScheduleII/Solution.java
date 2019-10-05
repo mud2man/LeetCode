de4f79125f27849ae2066538cf392e01ddf8c723
Namo.Lu <chih-hung.lu@columbia.edu>
2019-10-05 13:45:47 -0700

    LeetCode210 update

--- Java/210.CourseScheduleII/Solution.java
+++ Java/210.CourseScheduleII/Solution.java
@@ -10,19 +10,15 @@ import java.util.*; // Stack
 public class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] inDegrees = new int[numCourses];
-        List<List<Integer>> adjecentList = new ArrayList<List<Integer>>();
-        LinkedList<Integer> roots = new LinkedList<Integer>();
+        Map<Integer, List<Integer>> adjecentList = new HashMap<Integer, List<Integer>>();
+        Deque<Integer> roots = new LinkedList<Integer>();
         List<Integer> order = new ArrayList<Integer>();
-        
-        for(int i = 0; i < numCourses; ++i){
-            adjecentList.add(new ArrayList<Integer>());
-        }
         
         for(int[] prerequisite: prerequisites){
             int source = prerequisite[1];
             int target = prerequisite[0];
             inDegrees[target]++;
-            adjecentList.get(source).add(target);
+            adjecentList.computeIfAbsent(source, key -> new ArrayList<>()).add(target);
         }
         
         for(int i = 0; i < numCourses; ++i){
@@ -36,6 +32,9 @@ public class Solution {
             for(int i = 0; i < size; ++i){
                 int source = roots.pollFirst();
                 order.add(source);
+                if(!adjecentList.containsKey(source)){
+                    continue;
+                }
                 for(Integer target: adjecentList.get(source)){
                     if(--inDegrees[Integer.valueOf(target)] == 0){
                         roots.add(target);
@@ -46,18 +45,12 @@ public class Solution {
         
         if(order.size() == numCourses){
             return order.stream().mapToInt(i->i).toArray();
-        }
-        else{
+        }else{
             return new int[0];
         }
     }
- 
+  
     public static void main(String[] args){
-        final int numCourses = 5;
-        Solution sol;
-        int[][] prerequisites;
-        boolean canfinish; 
-
         /*    Relationship graph
          *         0
          *        / \
@@ -70,11 +63,9 @@ public class Solution {
          *     @ @
          *      4
          */
-        prerequisites = new int [][]{
-            {1, 0}, {2, 0}, {3, 1}, {4, 3}
-        };
-
-        sol = new Solution();
+        int numCourses = 5;
+        int[][] prerequisites = {{1, 0}, {2, 0}, {3, 1}, {4, 3}};
+        Solution sol = new Solution();
         int[] order = sol.findOrder(numCourses, prerequisites);
         System.out.println("order: " + Arrays.toString(order));
     }
