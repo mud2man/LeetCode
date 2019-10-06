270dcd24c030b0d7c8e4cb13ebc6c12b736dfdc1
Namo.Lu <chih-hung.lu@columbia.edu>
2019-10-06 13:13:35 -0700

    LeetCode#573 update

--- Java/573.SquirrelSimulation/Solution.java
+++ Java/573.SquirrelSimulation/Solution.java
@@ -1,29 +1,27 @@
 /* Hash: Time:O(n) Space:O(1)
- * 1. Becuase the minimum distace = sum(treeToNut[i]*2) + min(squirrelToNut[i] - treeToNut[i]), where 0 <= i <= (length - 1)
- * 2. We can iterate all nuts to compute distance = sum(treeToNut[i]*2), and update minDiff = min(squirrelToNut[i] - treeToNut[i])
- * 3. Therefore, the answer is distance + minDiff
+ * 1. Becuase the minimum distace = sum(treeToNut[i]*2) - max(treeToNut[i] - squirrelToNut[i]), where 0 <= i <= (length - 1)
+ * 2. We can iterate all nuts to compute distance = sum(treeToNut[i]*2), and update maxDiff = max(treeToNut[i] - squirrelToNut[i])
+ * 3. Therefore, the answer is distance - maxDiff
  */
 
 import java.util.*;
 
 public class Solution{
     public int minDistance(int height, int width, int[] tree, int[] squirrel, int[][] nuts) {
-        int minDiff = Integer.MAX_VALUE;
+        int maxDiff = Integer.MIN_VALUE;
         int distance = 0;
-        
         for(int[] nut: nuts){
             int treeToNut = Math.abs(nut[0] - tree[0]);
             treeToNut += Math.abs(nut[1] - tree[1]);
             distance += treeToNut * 2;
             int squirrelToNut = Math.abs(nut[0] - squirrel[0]);
             squirrelToNut += Math.abs(nut[1] - squirrel[1]);
-            int diff = squirrelToNut - treeToNut;
-            minDiff = Math.min(minDiff, diff);  
+            int diff = treeToNut - squirrelToNut;
+            maxDiff = Math.max(maxDiff, diff);  
         }
-        
-        return distance + minDiff;
+        return distance - maxDiff;
     }
-
+ 
     public static void main(String[] args){
         int height = 5;
         int width = 7;
