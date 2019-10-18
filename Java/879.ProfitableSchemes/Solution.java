050785e850b144973f65b6bdcf961e19bdd0e4e3
Namo.Lu <chih-hung.lu@columbia.edu>
2019-10-18 09:39:45 +0800

    LeetCode#879 refine

--- Java/879.ProfitableSchemes/Solution.java
+++ Java/879.ProfitableSchemes/Solution.java
@@ -1,8 +1,9 @@
 /* Dynamic programming: Time:O(N*P*G), Space:O(N*P*G), where N is group#
  * 1. dp[z][y][x] = the schemes of making x profit using y people with considering group[0, 1, ... z] and profit[0, 1, ... z]
  * 2. dp[z][y][P] is especially the schemes for P profit or larger of using y people
- * 3. dp[z][y][x] = dp[z - 1][y][x](not include profit[z]) + dp[z - 1][y - group[z - 1]][x - profit[z - 1]](include profit[z])
- * 4. dp[z][y][P] = dp[z - 1][y][P] + SumOf(dp[z - 1][prevY][x]), where Math.max(0, P - profit[z - 1]) <= x <= P 
+ * 3. If not select (group[z], profit[z]) = (g, p), we accumulate dp[z][y][x] to dp[z + 1][y][x]
+ * 4. If select (group[z], profit[z]) = (g, p), we accumulate dp[z][y][x] to dp[z + 1][y + g][x + p]
+ * 5. For each traversal, we consider both cases of selecting and no-slecting
  */
 
 import java.util.*;
@@ -12,25 +13,21 @@ public class Solution{
         int[][][] dp = new int[group.length + 1][G + 1][P + 1];
         int base = 1_000_000_007;
         dp[0][0][0] = 1;
-        for(int z = 1; z < group.length + 1; ++z){
+        for(int z = 0; z < group.length; ++z){
+            int g = group[z];
+            int p = profit[z];
             for(int y = 0; y < G + 1; ++y){
-                //caculate the schemes with profit < P
-                for(int x = 0; x < P; ++x){
-                    dp[z][y][x] = dp[z - 1][y][x];
-                    int prevX = x - profit[z - 1];
-                    int prevY = y - group[z - 1];
-                    dp[z][y][x] += (prevX >= 0 && prevY >= 0)? dp[z - 1][prevY][prevX]: 0;
-                    dp[z][y][x] = dp[z][y][x] % base;
-                }
-                
-                //caculate the schemes with profit >= P
-                dp[z][y][P] = dp[z - 1][y][P];
-                int prevY = y - group[z - 1];
-                if(prevY >= 0){
-                    int prevX = Math.max(0, P - profit[z - 1]);
-                    for(int x = prevX; x < P + 1; ++x){
-                        dp[z][y][P] += dp[z - 1][prevY][x];
-                        dp[z][y][P] = dp[z][y][P] % base;
+                for(int x = 0; x < P + 1; ++x){
+                    //not select (g, p)
+                    dp[z + 1][y][x] = (dp[z + 1][y][x] + dp[z][y][x]) % base;
+                    //select (g, p)
+                    if(y + g <= G){
+                        if(x + p >= P){
+                            dp[z + 1][y + g][P] = (dp[z][y][x] + dp[z + 1][y + g][P]) % base;
+                        }else{
+                            dp[z + 1][y + g][x + p] = (dp[z][y][x] + dp[z + 1][y + g][x + p]) % base;
+                        }
+                        
                     }
                 }
             }
@@ -38,9 +35,8 @@ public class Solution{
         
         int schemes = 0;
         for(int y = 0; y < G + 1; ++y){
-            schemes += dp[group.length][y][P];
-            schemes = schemes % base;
-        }
+            schemes = (schemes + dp[group.length][y][P]) % base;
+        }  
         return schemes;
     }
   
