0fb43903d43cbbf38043dbd24b45cf527f9b1dcd
Namo.Lu <chih-hung.lu@columbia.edu>
2019-11-04 21:49:59 -0800

    LeetCode#698 update

--- Java/698.PartitionKEqualSumSubsets/DynamicProgramming/Solution.java
+++ Java/698.PartitionKEqualSumSubsets/DynamicProgramming/Solution.java
@@ -1,8 +1,9 @@
 /* Dynamic programming: Time:O(n*2^n) Space:O(2^n)
- * 1. state = used status of nums, e.g.state of {6(used, 5(used), 5(unused), 4(used)} = 2'b1101
- * 2. dp[state] = if the state of nums can fill the bucket which the last selected num didn't overflow the bucket
- * 3. Since if nums can fill k buckets, we can find the last selected num which not overflow the last bucket, we can deduced theory from the end to start
- * 4. dp[state] = dp[prevState0] | dp[prevState1] | ...., where the the last selected num <= room
+ * 1. We can say we put the number into bucket in a greedy way, and we iterate all possible "lastSelect". So we search all permutation
+ * 2. state = used status of nums, e.g.state of {6(used, 5(used), 5(unused), 4(used)} = 2'b1101
+ * 3. dp[state] = if the state of nums can fill the bucket which the last selected num didn't overflow the bucket
+ * 4. Since if nums can fill k buckets, we can find the last selected num which not overflow the last bucket, we can deduced theory from the end to start
+ * 5. dp[state] = dp[prevState0] | dp[prevState1] | ...., where the the last selected num <= room
  */
 
 import java.util.*;
@@ -28,7 +29,7 @@ public class Solution{
             }
             
             int lastSelect = 1;
-            int room = (subSum - 1) % bucketSize + 1;
+            int room = (subSum - 1) % bucketSize + 1; //consider the edge case of subSum = bucketSize * m
             for(int i = 0; i < len; ++i, lastSelect = lastSelect << 1){
                 if((lastSelect & state) != 0 && nums[i] <= room){
                     int prevState = state & ~lastSelect;
