9dc5bb14867d4d48fcac2150aa2d2fcd093cf280
Namo.Lu <chih-hung.lu@columbia.edu>
2019-11-05 19:18:55 -0800

    LeetCode#380 update

--- Java/380.InsertDeleteGetRandom/IndexMap/RandomizedSet.java
+++ Java/380.InsertDeleteGetRandom/IndexMap/RandomizedSet.java
@@ -9,23 +9,23 @@ import java.util.*;
 
 public class RandomizedSet {
     int size;
-    Map<Integer, Integer> indexMap;
-    Map<Integer, Integer> reverseIndexMap;
+    Map<Integer, Integer> index2Num;
+    Map<Integer, Integer> num2Index;
     Random rand;
     
     /** Initialize your data structure here. */
     public RandomizedSet() {
         size = 0;
-        indexMap = new HashMap<>();
-        reverseIndexMap = new HashMap<>();
+        index2Num = new HashMap<>();
+        num2Index = new HashMap<>();
         rand = new Random();
     }
     
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public boolean insert(int val) {
-        if(!reverseIndexMap.containsKey(val)){
-            indexMap.put(size, val);
-            reverseIndexMap.put(val, size++);
+        if(!num2Index.containsKey(val)){
+            index2Num.put(size, val);
+            num2Index.put(val, size++);
             return true;
         }else{
             return false;
@@ -34,13 +34,13 @@ public class RandomizedSet {
     
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
-        if(reverseIndexMap.containsKey(val)){
-            int lastVal = indexMap.get(--size);
-            int removeIndex = reverseIndexMap.get(val);
-            indexMap.put(removeIndex, lastVal);
-            reverseIndexMap.put(lastVal, removeIndex);
-            reverseIndexMap.remove(val);
-            indexMap.remove(size);
+        if(num2Index.containsKey(val)){
+            int lastVal = index2Num.get(--size);
+            int removeIndex = num2Index.get(val);
+            index2Num.put(removeIndex, lastVal);
+            num2Index.put(lastVal, removeIndex);
+            num2Index.remove(val);
+            index2Num.remove(size);
             return true;
         }else{
             return false;
@@ -50,14 +50,12 @@ public class RandomizedSet {
     /** Get a random element from the set. */
     public int getRandom() {
         int randomIndex = rand.nextInt(size);
-        return indexMap.get(randomIndex);
+        return index2Num.get(randomIndex);
     }
-   
+  
     public static void main(String[] args){
-        int val;
         RandomizedSet obj = new RandomizedSet();
-        
-        val = 1;
+        int val = 1;
         System.out.println("Insert " + val + " :" + obj.insert(val));
         
         val = 2;
