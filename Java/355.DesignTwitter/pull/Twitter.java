0cf3a4a2f9d03b66b9daa8afe14c31b1e349245c
Namo.Lu <chih-hung.lu@columbia.edu>
2019-11-06 22:05:11 -0800

    LeetCode#355 update

--- Java/355.DesignTwitter/pull/Twitter.java
+++ Java/355.DesignTwitter/pull/Twitter.java
@@ -1,9 +1,8 @@
 /* Pull model: Time:O(1), Space:O(n)
- * 1. Have "user2Tweets" to record the user->tweets, and keep up to 10 tweets
+ * 1. Have "user2Posts" to record the user->tweets, and keep up to 10 tweets
  * 2. Have "user2Followees" to record user->followees
  * 3. Have "timeStamp" in Tweet, and sorting by it
  * 4. Output a list of tweets acording followees and user2Tweets with decending order of timeStamp 
- * 5. Each user need to follow herself, and cannot be unfollowed
  */
 
 import java.util.stream.Collectors;
@@ -11,42 +10,50 @@ import java.util.*;
 import java.math.*;
 
 public class Twitter{
-    private class Tweet{
-        int id;
-        Integer timeStamp;
-        Tweet(int i, int t){id = i; timeStamp = t;}
-    }
-    Map<Integer, Deque<Tweet>> user2Tweets;
+    int time;
     Map<Integer, Set<Integer>> user2Followees;
-    int timeStamp;
+    Map<Integer, Deque<int[]>> user2Posts;
+    
     /** Initialize your data structure here. */
     public Twitter() {
-        user2Tweets = new HashMap<>();
+        time = 0;
         user2Followees = new HashMap<>();
-        timeStamp = 0;
+        user2Posts = new HashMap<>();
     }
     
     /** Compose a new tweet. */
     public void postTweet(int userId, int tweetId) {
-        user2Tweets.computeIfAbsent(userId, key -> new LinkedList<>()).add(new Tweet(tweetId, timeStamp++));
-        user2Followees.computeIfAbsent(userId, key -> new HashSet<>()).add(userId);
-        if(user2Tweets.get(userId).size() > 10){
-            user2Tweets.get(userId).pollFirst();
+        user2Posts.computeIfAbsent(userId, key -> new LinkedList<>()).add(new int[]{tweetId, time++});
+        Deque<int[]> posts = user2Posts.get(userId);
+        if(posts.size() > 10){
+            posts.pollFirst();
         }
     }
     
     /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
     public List<Integer> getNewsFeed(int userId) {
-        List<Tweet> news = new ArrayList<>();
-        Set<Integer> followees = user2Followees.getOrDefault(userId, new HashSet<>());
+        Set<Integer> followees = new HashSet<>();
+        followees.add(userId);
+        followees.addAll(user2Followees.getOrDefault(userId, new HashSet<>()));
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((x, y) -> x[1] - y[1]);
         for(int followee: followees){
-            news.addAll(user2Tweets.getOrDefault(followee, new LinkedList<>()));
-            Collections.sort(news, (x, y) -> {return y.timeStamp.compareTo(x.timeStamp);});
-            if(news.size() > 10) {
-                news = news.subList(0, 10);
+            Deque<int[]> posts = user2Posts.getOrDefault(followee, new LinkedList<>());
+            for(int[] post: posts){
+                if(minHeap.size() < 10){
+                    minHeap.add(post);
+                }else{
+                    if(minHeap.peek()[1] < post[1]){
+                        minHeap.poll();
+                        minHeap.add(post);
+                    }
+                }
             }
         }
-        return news.stream().map(tweet -> tweet.id).collect(Collectors.toList());
+        LinkedList<Integer> newsFeed = new LinkedList<>();
+        while(!minHeap.isEmpty()){
+            newsFeed.addFirst(minHeap.poll()[0]);
+        }
+        return newsFeed;
     }
     
     /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
@@ -56,11 +63,9 @@ public class Twitter{
     
     /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
     public void unfollow(int followerId, int followeeId) {
-        if(user2Followees.containsKey(followerId) && followerId != followeeId){
-            user2Followees.get(followerId).remove(followeeId);
-        }
+        user2Followees.computeIfAbsent(followerId, key -> new HashSet<>()).remove(followeeId);
     }
- 
+  
     public static void main(String[] args){
         int userId0;
         int userId1;
