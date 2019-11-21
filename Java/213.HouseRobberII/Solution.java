80f93f2f75eefa1617382a7b9b89f8017132bb01
Namo.Lu <chih-hung.lu@columbia.edu>
2019-11-20 22:55:07 -0800

    LeetCode#213 update

--- Java/213.HouseRobberII/Solution.java
+++ Java/213.HouseRobberII/Solution.java
@@ -7,39 +7,30 @@
 import java.util.*;
 
 public class Solution {
-    public int rob(int[] nums) {
-        int[] dpFirstTaken = new int[nums.length];
-        int[] dpFirstNotTaken = new int[nums.length];
-        
+    public int rob(int[] nums) {    
         if(nums.length == 0){
             return 0;
         }
-        
         if(nums.length == 1){
             return nums[0];
         }
         
+        int[] dpFirstTaken = new int[nums.length];
+        int[] dpFirstNotTaken = new int[nums.length];
         dpFirstTaken[0] = nums[0];
         dpFirstTaken[1] = nums[0];
-        for(int i = 2; i < (nums.length - 1); ++i){
-            dpFirstTaken[i] = Math.max(dpFirstTaken[i - 1], dpFirstTaken[i - 2] + nums[i]);
-        }
-        
         dpFirstNotTaken[0] = 0;
         dpFirstNotTaken[1] = nums[1];
         for(int i = 2; i < nums.length; ++i){
+            dpFirstTaken[i] = Math.max(dpFirstTaken[i - 1], dpFirstTaken[i - 2] + nums[i]);
             dpFirstNotTaken[i] = Math.max(dpFirstNotTaken[i - 1], dpFirstNotTaken[i - 2] + nums[i]);
         }
-        
         return Math.max(dpFirstTaken[nums.length - 2], dpFirstNotTaken[nums.length - 1]);
     }
-
+ 
     public static void main(String[] args){
-        Solution sol;
         int[] nums = {3, 2, 1, 5, 6, 4};
-        
-        sol = new Solution();
-
+        Solution sol = new Solution();
         System.out.println("nums: " + Arrays.toString(nums));
         System.out.println("The maximum money: " + sol.rob(nums));
     }
