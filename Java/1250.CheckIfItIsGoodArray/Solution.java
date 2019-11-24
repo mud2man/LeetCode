d45eed2f0699179eb38bc644412e98e921a0acca
Namo.Lu <chih-hung.lu@columbia.edu>
2019-11-23 19:15:09 -0800

    LeetCode#1250 update

--- Java/1250.CheckIfItIsGoodArray/Solution.java
+++ Java/1250.CheckIfItIsGoodArray/Solution.java
@@ -1,25 +1,25 @@
 /* Math: Time:O(n), Space:O(1)
  * 1. We can reach the goal if we get the GCD among a set of numbers, which is equal to 1
- * 2. Keep update the GCD by split phase divide nums[i] and gcd 
+ * 2. Keep update the GCD by euclidean algorithm  nums[i] and gcd 
  */
 
 import java.util.*; // Stack
 
 public class Solution {
-    private int splitPhaseDivide(int num0, int num1){
+    private int euclideanAlgorithm(int num0, int num1){
         int small = Math.min(num0, num1);
         int big = Math.max(num0, num1);
         if(small == big || small == 0){
             return big;
         }else{
-            return splitPhaseDivide(small, big % small);
+            return euclideanAlgorithm(small, big % small);
         }
     }
     
     public boolean isGoodArray(int[] nums) {
         int gcd = nums[0];
         for(int i = 1; i < nums.length; ++i){
-            gcd = splitPhaseDivide(gcd, nums[i]);
+            gcd = euclideanAlgorithm(gcd, nums[i]);
         }
         return (gcd == 1);
     }
