aa75a6b8d1e79e27f1bcd9638286ef11d0da4e01
Namo.Lu <chih-hung.lu@columbia.edu>
2019-11-24 00:58:38 -0800

    LeetCode#17 update

--- Java/17.LetterCombinationsPhoneNumber/Solution.java
+++ Java/17.LetterCombinationsPhoneNumber/Solution.java
@@ -4,35 +4,29 @@
 import java.util.*;
 
 public class Solution {
-    void helper(int idx, String digits, String subString, String[] bottoms, List<String> combinations){
-        char bottom;
-        
+    private void backtrack(int idx, String digits, String subString, String[] bottoms, List<String> combinations){
         if(subString.length() == digits.length()){
-            combinations.add(subString);
+            if(digits.length() > 0){
+                combinations.add(subString);
+            }
             return;
         }
-        
-        bottom = digits.charAt(idx);
+        char bottom = digits.charAt(idx);
         for(char c:  bottoms[bottom - '2'].toCharArray()){
-            helper(idx + 1, digits, subString + Character.toString(c), bottoms, combinations);
+            backtrack(idx + 1, digits, subString + Character.toString(c), bottoms, combinations);
         }
     }
     
     public List<String> letterCombinations(String digits) {
         List<String> combinations = new ArrayList<String>();
         String[] bottoms = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        
-        if(digits.length() > 0)
-            helper(0, digits, "", bottoms, combinations);
-            
+        backtrack(0, digits, "", bottoms, combinations);    
         return combinations;
     }
- 
+  
     public static void main(String[] args){
-        Solution sol;
+        Solution sol = new Solution();
         String digits = "23";
-        sol = new Solution();
-
         System.out.println("digits: " + digits);
         System.out.println("combinations: " + sol.letterCombinations(digits));
     }
