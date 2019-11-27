c6f639dc5551108180b3e387daca8edcdf90ca6c
Namo.Lu <chih-hung.lu@columbia.edu>
2019-11-26 22:30:40 -0800

    LeetCode#295 update

--- Java/295.FindMedianDataStream/MedianFinder.java
+++ Java/295.FindMedianDataStream/MedianFinder.java
@@ -1,9 +1,8 @@
 /* Heap: O(logn)
  * 1. Have a maxHeap to store the smaller half
  * 2. Have a minHeap to store the bigger half
- * 3. When insert, if maxHeap.size() == minHeap.size(), put the num to maxHeap if num <= maxHeap.peek()
- * 4. If maxHeap.size() > minHeap.size(), add num to maxHeap and poll the top and add it to minHeap, which make two heap have the same size
- * 5. Otherwise, vice versa
+ * 3. When insert, if maxHeap.size() <= minHeap.size(), add num to minHeap and poll top of minHeap and add it to maxHeap
+ * 4. Otherwise, add num to maxHeap and poll the top and add it to minHeap, which make two heap have the same size
  *
  * For follow up, use bucket, where bucket[i] is the count of number i. We can iterate from i = 0 to 100 to accumulatet the count. It's O(1) solution
  */
@@ -12,43 +11,31 @@ import java.util.*;
 
 public class MedianFinder{
     PriorityQueue<Integer> maxHeap;
-    PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> minHeap;     
     /** initialize your data structure here. */
     public MedianFinder() {
-        maxHeap = new PriorityQueue<>((x, y) -> (y - x));
-        minHeap = new PriorityQueue<>(); 
+        maxHeap = new PriorityQueue<Integer>((x, y) -> (y - x));
+        minHeap = new PriorityQueue<Integer>();
     }
     
     public void addNum(int num) {
-        if(maxHeap.isEmpty()){
-            maxHeap.add(num);
-        }else if(maxHeap.size() == minHeap.size()){
-            if(num <= maxHeap.peek()){
-                maxHeap.add(num);
-            }else{
-                minHeap.add(num);
-            }
+        if(maxHeap.size() <= minHeap.size()){
+            minHeap.add(num);
+            maxHeap.add(minHeap.poll());
         }else if(maxHeap.size() > minHeap.size()){
             maxHeap.add(num);
             minHeap.add(maxHeap.poll());
-        }else{
-            minHeap.add(num);
-            maxHeap.add(minHeap.poll());
         }
     }
     
     public double findMedian() {
-        if((maxHeap.size() + minHeap.size()) % 2 == 0){
-            return (double)(maxHeap.peek() + minHeap.peek()) / 2.0;
+        if(maxHeap.size() == minHeap.size()){
+            return ((double)maxHeap.peek() + (double)minHeap.peek()) / 2.0;
         }else{
-            if(maxHeap.size() > minHeap.size()){
-                return (double)(maxHeap.peek());
-            }else{
-                return (double)(minHeap.peek());
-            }
+            return (maxHeap.size() > minHeap.size())? (double)maxHeap.peek(): (double)minHeap.peek();
         }
     }
- 
+  
     public static void main(String[] args){
         MedianFinder sol = new MedianFinder();
         int num = 1;
