88c798e25830461b5e649add553445de6907f04f
Namo.Lu <chih-hung.lu@columbia.edu>
2019-11-28 22:46:06 -0800

    LeetCode#269 update

--- Java/269.AlienDictionary/Solution.java
+++ Java/269.AlienDictionary/Solution.java
@@ -1,57 +1,47 @@
 /* Topological sort: Time:O(n), Space: O(n), where n is number of characters of string array
  * 1. Add edges from all the continuous two words. e.g., t->f for "wrt" and "wrf"
- * 2. Use topological sort  to find the order
+ * 2. Use topological sort to find the order
  */         
 
 import java.util.*;
 
 public class Solution {
-    private void findEdge(String parent, String child, Map<Integer, Set<Integer>> ajdacencyList, int[] indegree){
-        int i;
-        for(i = 0; i < Math.min(parent.length(), child.length()); ++i){
-            int source = parent.charAt(i) - 'a';
-            int target = child.charAt(i) - 'a';
+    private void findEdge(String parent, String child, Map<Character, Set<Character>> ajdacencyList, int[] indegree){
+        for(int i = 0; i < Math.min(parent.length(), child.length()); ++i){
+            char source = parent.charAt(i);
+            char target = child.charAt(i);
             if(source != target){
-                ajdacencyList.putIfAbsent(source, new HashSet<Integer>());
+                ajdacencyList.putIfAbsent(source, new HashSet<>());
                 if(!ajdacencyList.get(source).contains(target)){
                     ajdacencyList.get(source).add(target);
-                    indegree[source] = (indegree[source] == -1)? 0: indegree[source];
-                    indegree[target] = (indegree[target] == -1)? 1: indegree[target] + 1;
+                    indegree[source - 'a'] = (indegree[source - 'a'] == -1)? 0: indegree[source - 'a'];
+                    indegree[target - 'a'] = (indegree[target - 'a'] == -1)? 1: indegree[target - 'a'] + 1;
                 }
                 break;
             }
-            else{
-                indegree[source] = (indegree[source] == -1)? 0: indegree[source];
-            }
         }
-        for(int j = i; j < child.length(); ++j){
+        for(int j = 0; j < child.length(); ++j){
             indegree[child.charAt(j) - 'a'] = (indegree[child.charAt(j) - 'a'] == -1)? 0: indegree[child.charAt(j) - 'a'];
         }
     }
     
     public String alienOrder(String[] words) {
-        Map<Integer, Set<Integer>> ajdacencyList = new HashMap<Integer, Set<Integer>>();
+        Map<Character, Set<Character>> ajdacencyList = new HashMap<>();
         int[] indegree = new int[26];
-        
-        if(words.length == 1){
-            return words[0];
-        }
-
-        Arrays.fill(indegree, -1); 
+        Arrays.fill(indegree, -1);
         for(int i = 0; i < words[0].length(); ++i){
             indegree[words[0].charAt(i) - 'a'] = 0;
         }
- 
         for(int i = 0; i < (words.length - 1); ++i){
             findEdge(words[i], words[i + 1], ajdacencyList, indegree);
         }
         
         // find nodes with indegree 0 as sources
-        LinkedList<Integer> queue = new LinkedList<Integer>();
+        Deque<Character> queue = new LinkedList<>();
         int nodeCount = 0;
         for(int i = 0; i < indegree.length; ++i){
             if(indegree[i] == 0){
-                queue.add(i);
+                queue.add((char)(i + 'a'));
             }
             nodeCount = (indegree[i] != -1)? nodeCount + 1: nodeCount;
         }
@@ -60,25 +50,23 @@ public class Solution {
         StringBuilder order = new StringBuilder("");
         int visitedCount = 0;
         while(!queue.isEmpty()){
-            int currentNode = queue.pollFirst();
+            char currentNode = queue.pollFirst();
             visitedCount++;
-            order.append((char)('a' + currentNode));
-            Set<Integer> children = (ajdacencyList.containsKey(currentNode))? ajdacencyList.get(currentNode): new HashSet();
-            for(int child: children){
-                indegree[child]--;
-                if(indegree[child] == 0){
+            order.append(currentNode);
+            Set<Character> children = ajdacencyList.getOrDefault(currentNode, new HashSet<>());
+            for(char child: children){
+                indegree[child - 'a']--;
+                if(indegree[child - 'a'] == 0){
                     queue.add(child);
                 }
             }
         }
         return (visitedCount == nodeCount)? order.toString(): "";
     }
-
+ 
     public static void main(String[] args){
-        Solution sol;
         String[] words = {"wrt", "wrf", "er", "ett", "rftt"};
-        
-        sol = new Solution();
+        Solution sol = new Solution();
         System.out.println("words:" + Arrays.toString(words));
         System.out.println("order:" + sol.alienOrder(words));
     }
