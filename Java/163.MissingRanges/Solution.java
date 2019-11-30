fac81addfe036535924a859d3ebba4bfd4968f8c
Namo.Lu <chih-hung.lu@columbia.edu>
2019-11-30 10:51:02 -0800

    LeetCode#163 update

--- Java/163.MissingRanges/Solution.java
+++ Java/163.MissingRanges/Solution.java
@@ -13,7 +13,6 @@ public class Solution{
     public List<String> findMissingRanges(int[] nums, int lower, int upper) {
         List<String> ranges  = new ArrayList<String>();
         long prev = (long)lower - 1;
-        long ub = (long)upper;
         for(int num: nums){
             if(prev != (long)num - 1 && prev < num){
                 ranges.add(getRange(prev + 1, (long)num - 1));
@@ -28,12 +27,11 @@ public class Solution{
     }
  
     public static void main(String[] args){
-        Solution sol;
         int[] nums = {0, 1, 3, 50, 75};
         int lower = 0;
         int upper = 99;
 
-        sol = new Solution();    
+        Solution sol = new Solution();    
         System.out.println("nums: " + Arrays.toString(nums));
         System.out.println("lower: " + lower);
         System.out.println("upper: " + upper);
