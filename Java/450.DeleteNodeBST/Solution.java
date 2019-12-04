3b03ca021d3278a9e09386a8ad75bb44042e2c7b
Namo.Lu <chih-hung.lu@columbia.edu>
2019-12-03 23:20:22 -0800

    LeetCode#450 update

--- Java/450.DeleteNodeBST/Solution.java
+++ Java/450.DeleteNodeBST/Solution.java
@@ -18,13 +18,11 @@ public class Solution {
         private void search(TreeNode[] fartherSon, int key){
         if(fartherSon[1] == null || fartherSon[1].val == key){
             return;
-        }
-        else if(fartherSon[1].val > key){
+        }else if(fartherSon[1].val > key){
             fartherSon[0] = fartherSon[1];
             fartherSon[1] = fartherSon[1].left;
             search(fartherSon, key);
-        }
-        else{
+        }else{
             fartherSon[0] = fartherSon[1];
             fartherSon[1] = fartherSon[1].right;
             search(fartherSon, key);
@@ -39,22 +37,18 @@ public class Solution {
         if(fartherSon[1].right == null){
             if(fartherSon[0].left == fartherSon[1]){
                 fartherSon[0].left = fartherSon[1].left;
-            }
-            else{
+            }else{
                 fartherSon[0].right = fartherSon[1].left;
             }
-        }
-        else{
+        }else{
             if(fartherSon[1].right.left == null){
                 if(fartherSon[1] == fartherSon[0].left){
                     fartherSon[0].left = fartherSon[1].right;
-                }
-                else{
+                }else{
                     fartherSon[0].right = fartherSon[1].right;
                 }
                 fartherSon[1].right.left = fartherSon[1].left;
-            }
-            else{
+            }else{
                 TreeNode ptr0 = fartherSon[1].right;
                 TreeNode ptr1 = ptr0.left;
                 while(ptr1.left != null){
@@ -79,21 +73,14 @@ public class Solution {
     public void inorder(TreeNode root){
         if(root == null){
             return;
-        }
-        else{
+        }else{
             inorder(root.left);
             System.out.print(root.val + ", ");
             inorder(root.right);
         }
     }
  
-    public static void main(String[] args)
-    {
-        List<List<Integer>> list;
-        TreeNode root;
-        Solution sol;
-        int key;
-        
+    public static void main(String[] args) {
         /* Generate a input tree
          *     3
          *    / \
@@ -101,13 +88,13 @@ public class Solution {
          *       / \
          *      15  7
          */
-        root = new TreeNode(3);
+        TreeNode root = new TreeNode(3);
         root.left = new TreeNode(9);
         root.right = new TreeNode(20);
         root.right.left = new TreeNode(15);
         root.right.right = new TreeNode(7);
-        key = 20;
-        sol = new Solution();
+        int key = 20;
+        Solution sol = new Solution();
         
         System.out.println("before delete " + key);
         sol.inorder(root);
