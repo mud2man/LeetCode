481da0ec8f486239c06e17d02fee81ec0327b2c4
Namo.Lu <chih-hung.lu@columbia.edu>
2019-12-08 21:07:21 -0800

    LeetCode#1167 update

--- Java/1167.MinimumCostConnectSticks/Solution.java
+++ Java/1167.MinimumCostConnectSticks/Solution.java
@@ -1,6 +1,11 @@
-/* Greedy: Time:O(n), Space:O(1). We need to prove greedy algo can be optimal
+/* Greedy: Time:O(nlogn), Space:O(1)
  * 1. Pick the 2 minimum stickes, and do merge, then get the cost in each round
  * 2. Repeat the step1 until sticks# < 2
+ *
+ * Proof:
+ * 1. If the optimal solution has the smallest was mergered to elementX on stage Y. 
+ * 2. We can get the smaller of equivalent cost by replacing the one of the merged two element
+ * 3. Based on this, we cna replacing the other element by the second smallest element with the smaller of equivalent cost
  */
 
 import java.util.*;
