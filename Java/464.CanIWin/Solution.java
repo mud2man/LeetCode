43e918ce1da0ee932b6ae81178ca8f4127e8cb04
Namo.Lu <chih-hung.lu@columbia.edu>
2019-12-12 17:17:52 -0800

    LeetCode464 update

--- Java/464.CanIWin/Solution.java
+++ Java/464.CanIWin/Solution.java
@@ -1,13 +1,13 @@
 /* Devide and counquer: O(2^n), while O(n!) without memory
  * 1. Record all the taken steps into an array, and caculate the identifier by 2's weight
- * 2. Keep records of lose set "lose" and win set "win" to  avoid duplicated searching
+ * 2. Keep records of win/lose to "cache" avoid duplicated searching
  * 3. Any one of step can make opponant cannot win, then I win
  */
 
 import java.util.*; // Stack
 
 public class Solution {
-   private int getHashCode(boolean[] used){
+    private int getHashCode(boolean[] used){
         int hashCode = 0;
         for(int i = 1, weight = 1; i < used.length; ++i){
             hashCode += used[i]? weight: 0;
@@ -16,34 +16,27 @@ public class Solution {
         return hashCode;
     }
     
-    private boolean helper(boolean[] used, int remain, Set<Integer> win, Set<Integer> lose){
+    private boolean helper(boolean[] used, int remain, Map<Integer, Boolean> cache){
         int hashCode = getHashCode(used);
         if(remain <= 0){
-            lose.add(hashCode);
+            cache.put(hashCode, false);
             return false;
-        }
-        if(win.contains(hashCode)){
-            return true;
-        }
-        if(lose.contains(hashCode)){
-            return false;
-        }
-        
-        for(int i = 1; i < used.length; ++i){
-            if(used[i] == false){
-                // my move
-                used[i] = true;
-                int nextRemain = remain - i;
-                // opponant's move
-                if(!helper(used, nextRemain, win, lose)){
-                    win.add(hashCode);
+        }else if(cache.containsKey(hashCode)){
+            return cache.get(hashCode);
+        }else{
+            for(int i = 1; i < used.length; ++i){
+                if(used[i] == false){
+                    used[i] = true;
+                    if(!helper(used, remain - i, cache)){
+                        cache.put(hashCode, true);
+                        used[i] = false;
+                        return true;
+                    }
                     used[i] = false;
-                    return true;
                 }
-                used[i] = false;
             }
         }
-        lose.add(hashCode);
+        cache.put(hashCode, false);
         return false;
     }
     
@@ -55,10 +48,8 @@ public class Solution {
         if(sum < desiredTotal){
             return false;
         }
-        boolean[] used = new boolean[maxChoosableInteger + 1];
-        Set<Integer> win = new HashSet<>();
-        Set<Integer> lose = new HashSet<>();
-        return helper(used, desiredTotal, win, lose);
+        Map<Integer, Boolean> cache = new HashMap<>();
+        return helper(new boolean[maxChoosableInteger + 1], desiredTotal, cache);
     }
  
     public static void main(String[] args){
