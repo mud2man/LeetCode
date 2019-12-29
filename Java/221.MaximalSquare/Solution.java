aa61a0a254c2c6205b06674ad6538e1860a43844
Namo.Lu <chih-hung.lu@columbia.edu>
2019-12-28 22:53:48 -0800

    LeetCode#221 update

--- Java/221.MaximalSquare/Solution.java
+++ Java/221.MaximalSquare/Solution.java
@@ -1,9 +1,7 @@
 /* Dynamic Programming: Time:O(n*m), Space:O(n)
- * 1. dp[y][x][0] = the maximum length of upward column of '1' connect with (y, x)
- * 2. dp[y][x][1] = the maximum length of left-ward row of '1' connect with (y, x)
- * 3. dp[y][x][2] = the length of biggest square with right-bottom cornet at (y, x)
- * 4. In each round, update dp[y][x][0], dp[y][x][1] and dp[y][x][2]
- * 5. Also, update 'maxLen' by max(maxLen, dp[y][x][2]) 
+ * 1. dp[y][x] = the length of biggest square with right-bottom cornet at (y, x)
+ * 2. In each round, update dp[y][x] by min(dp[y - 1][x], dp[y][x - 1], dp[y - 1][x - 1]) + 1
+ * 3. Also, update 'max' by max(max, dp[y][x]) 
  */
 
 import java.util.*;
@@ -13,28 +11,22 @@ public class Solution{
     public int maximalSquare(char[][] matrix) {
         int depth = matrix.length;
         int width = (depth > 0)? matrix[0].length: 0;
-        int[][][] dp = new int[2][width][3]; //up, left, upLeft
-        
-        int maxLen = 0;        
+        int[][] dp = new int[depth][width];
+        int max = 0;
         for(int y = 0; y < depth; ++y){
             for(int x = 0; x < width; ++x){
-                for(int i = 0; i < 3; ++i){
-                   dp[y % 2][x][i] = 0; 
-                }
                 if(matrix[y][x] == '1'){
-                    int up = (y > 0)? dp[(y - 1) % 2][x][0]: 0;
-                    int left = (x > 0)? dp[y % 2][x - 1][1]: 0;
-                    int upLeft = (y > 0 && x > 0)? dp[(y - 1) % 2][x - 1][2]: 0;
-                    dp[y % 2][x][0] = up + 1;
-                    dp[y % 2][x][1] = left + 1;
-                    dp[y % 2][x][2] = Math.min(upLeft + 1, Math.min(dp[y % 2][x][0], dp[y % 2][x][1]));
-                    maxLen = Math.max(maxLen, dp[y % 2][x][2]);
+                    int topSquare = (y > 0)? dp[y - 1][x]: 0;
+                    int leftSquare = (x > 0)? dp[y][x - 1]: 0;
+                    int topLeftSquare = (y > 0 && x > 0)? dp[y - 1][x - 1]: 0;
+                    dp[y][x] = Math.min(topLeftSquare, Math.min(topSquare, leftSquare)) + 1;
+                    max = Math.max(max, dp[y][x]);
                 }
             }
         }
-        return (maxLen * maxLen);
+        return max * max;
     }
- 
+  
     public static void main(String[] args){
         Solution sol = new Solution();
         char[][] matrix = {{'1', '0', '1', '0', '0'},
