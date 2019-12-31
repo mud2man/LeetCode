4535b0d07134f1273c02e14b52b465b55b12509f
Namo.Lu <chih-hung.lu@columbia.edu>
2019-12-30 21:18:31 -0800

    LeetCode#755 update

--- Java/755.PourWater/Solution.java
+++ Java/755.PourWater/Solution.java
@@ -1,57 +1,37 @@
 /* Time:O(n*m), Space:O(n), where n is the length of heights, m is V
- * 1. Move the drop to left if it eventually drop, and invoke "helper" with the index of dropped
- * 2. If drop[0] == 0, which means the drop used. Then, return
- * 3. Otherwise, move drop to right if it eventually drop
- * 4. If drop[0] == 1, whcih means the drop cannot be used, so just rise at the current index
+ * 1. Move the drop to left as long as heights[i] <= heights[idx]
+ * 2. If idx != K, increase heights[idx] and continue
+ * 3. Otherwise, move the drop to right as long as heights[i] <= heights[idx]
+ * 4. Increase heights[idx]
  */
 
 import java.util.*;
 
 public class Solution{
-    private void helper(int[] heights, int index, int[] drop){
-        int nextIndex = index - 1;
-        while(nextIndex >= 0){
-            if(heights[nextIndex] < heights[index]){
-                helper(heights, nextIndex, drop);
-                break;
-            }
-            else if (heights[nextIndex] > heights[index]){
-                break;
-            }
-            nextIndex--;
-        }
-        
-        if(drop[0] == 0){
-            return;
-        }
-        
-        nextIndex = index + 1;
-        while(nextIndex < heights.length){
-            if(heights[nextIndex] < heights[index]){
-                helper(heights, nextIndex, drop);
-                break;
-            }
-            else if (heights[nextIndex] > heights[index]){
-                break;
-            }
-            nextIndex++;
-        }
-        
-        if(drop[0] == 1){
-            heights[index]++;
-            drop[0]--;
-        }
-    }
-    
     public int[] pourWater(int[] heights, int V, int K) {
-        int[] drop = new int[1];
-        for(int i = 0; i < V; ++i){
-            drop[0] = 1;
-            helper(heights, K, drop);
+        while(V-- > 0){
+            int idx = K;
+            //left
+            for(int i = idx - 1; i >= 0 && heights[i] <= heights[idx]; --i){
+                if(heights[idx] > heights[i]){
+                    idx = i;
+                }
+            }
+            if(idx != K){
+                heights[idx]++;
+                continue;
+            }
+            //right
+            for(int i = idx + 1; i < heights.length && heights[i] <= heights[idx]; ++i){
+                if(heights[idx] > heights[i]){
+                    idx = i;
+                }
+            }
+            heights[idx]++;
         }
         return heights;
     }
- 
+
     public static void main(String[] args){
         Solution sol = new Solution();
         int[] heights = {2, 1, 1, 2, 1, 2, 2};
