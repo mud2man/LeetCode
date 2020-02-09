ce038f26e14a9bb6577cf0ba13ddf75eb1bb2d27
Namo.Lu <chih-hung.lu@columbia.edu>
2020-02-08 21:46:28 -0800

    LeetCode#140 update

--- Java/140.WordBreakII/TrieTree/Solution.java
+++ Java/140.WordBreakII/TrieTree/Solution.java
@@ -1,7 +1,8 @@
-/* TrieTree + Backtrace + Memorization: Time:O(n*k), where n is the length of s, k is number of sentence
- * 1. Have a trie tree to store the dictionary
- * 2. Have a hash map, where key is index, and value is the string set wich can be assembled from substring(idx) 
- * 3. Call helper recursively, and return answer directly if hashmap has the answer
+/* TrieTree + Backtrace: Time:O(max(n * m, k)), Space:O(n * m), where n is length of s, m is max length of words, and k is number of sentence
+ * 1. Have a trie tree to store words backwardly
+ * 2. Have "idx2Words", where key is index and value is the word list wich can cover a substring ending at s.charAt(idx) and idx2Words.containsKey(idx - word.length() - 1)
+ * 3. The second condition of above is to ensure we skip cases which cannot find word list to cover s.substring(0, idx - word.length)
+ * 4. The we use backtrack to collect all possible solutions
  */
 
 import java.util.*;
@@ -10,83 +11,62 @@ import java.util.*;
 public class Solution{
     private class TrieNode{
         String word;
-        TrieNode[] child;
-        TrieNode(){word = ""; child = new TrieNode[96];}
+        TrieNode[] children;
+        TrieNode(){word = ""; children = new TrieNode[26];}
     }
+    TrieNode tree = new TrieNode();
     
-    TrieNode tree;
-    
-    private void insert(TrieNode root, String word){
-        for(int i = 0; i < word.length(); ++i){
+    private void insert(String word){
+        TrieNode node = tree;
+        for(int i = word.length() - 1; i >= 0; --i){
             char c = word.charAt(i);
-            if(root.child[c - ' '] == null){
-                root.child[c - ' '] = new TrieNode();
-            }
-            root = root.child[c - ' '];
+            node.children[c - 'a'] = (node.children[c - 'a'] == null)? new TrieNode(): node.children[c - 'a'];
+            node = node.children[c - 'a'];
         }
-        root.word = word;
+        node.word = word;
     } 
     
-    private List<String> search(String s, int idx, TrieNode tree){
-        List<String> words = new ArrayList<String>();
-        for(int i = idx; i < s.length(); ++i){
+    private void search(String s, int end, Map<Integer, List<String>> idx2Words){
+        TrieNode node = tree;
+        for(int i = end; i >= 0 && node != null; --i){
             char c = s.charAt(i);
-            if(tree.child[c - ' '] != null){
-                tree = tree.child[c - ' '];
-                if(tree.word.length() > 0){
-                    words.add(tree.word);
-                }
-            }
-            else{
-                break;
+            node = node.children[c - 'a'];
+            if(node != null && node.word.length() > 0 && idx2Words.containsKey(i - 1)){
+                idx2Words.computeIfAbsent(end, key -> new ArrayList<>()).add(node.word);
             }
         }
-        return words;
     }
     
-    private List<String> helper(String s, int idx, TrieNode tree, Map<Integer, List<String>> map){
-        if(idx == s.length()){
-            return new ArrayList<String>();
+    private void backtarck(String s, int idx, Map<Integer, List<String>> idx2Words, List<String> sentences, String path){
+        if(idx == -1){
+            sentences.add(path.substring(1));
+            return;
         }
-        
-        if(map.containsKey(idx)){
-            return map.get(idx);
-        }
-        
-        List<String> words = search(s, idx, tree);
-        List<String> ret = new ArrayList<String>();
+        List<String> words = idx2Words.getOrDefault(idx, new ArrayList<>());
         for(String word: words){
-            List<String> sentences = helper(s, idx + word.length(), tree, map);
-            if((idx + word.length()) == s.length()){
-                ret.add(word);
-                continue;
-            }
-            if(sentences == null){
-                continue;
-            }
-            for(String sentence: sentences){
-                ret.add(word + " " + sentence);
-            }
+            backtarck(s, idx - word.length(), idx2Words, sentences, " " + word + path);
         }
-        map.put(idx, (ret.size() == 0)? null: ret); 
-        return map.get(idx);
     }
     
     public List<String> wordBreak(String s, List<String> wordDict) {
-        tree = new TrieNode();
         for(String word: wordDict){
-            insert(tree, word);
+            insert(word);
         }
-        List<String> ret = helper(s, 0, tree, new HashMap<Integer, List<String>>());
-        return (ret == null)? new ArrayList<String>(): ret;
+        Map<Integer, List<String>> idx2Words = new HashMap<>();
+        idx2Words.put(-1, Collections.singletonList(""));
+        for(int end = 0; end < s.length(); ++end){
+            search(s, end, idx2Words);
+        }
+        List<String> sentences = new ArrayList<>();
+        backtarck(s, s.length() - 1, idx2Words, sentences, "");
+        return sentences;
     }
-
+ 
     public static void main(String[] args){
         Solution sol = new Solution();
         String s = "catsanddog";
         List<String> wordDict = new ArrayList<String>();
         wordDict.addAll(Arrays.asList("cat", "cats", "and", "sand", "dog")); 
-        
         System.out.println("s: " + s);
         System.out.println("wordDict: " + wordDict);
         System.out.println("sentences: " + sol.wordBreak(s, wordDict));
