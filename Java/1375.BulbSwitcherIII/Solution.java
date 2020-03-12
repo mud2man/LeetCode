e4dc8c5b849e6f6d7921a6c05b7d8987cfdbb065
Namo.Lu <chih-hung.lu@columbia.edu>
2020-03-11 20:20:05 -0700

    LeetCode#1375 update

--- Java/1375.BulbSwitcherIII/Solution.java
+++ Java/1375.BulbSwitcherIII/Solution.java
@@ -1,28 +1,17 @@
-/* Time:O(n), Space:O(n). However, leetcode has Space-O(1) solution
- * 1. Have an array "bulbs" to record the status of each bulb with the status 0:off, 1:yellow, 2:blue 
- * 2. In each round, we turn the bulb to blue if isLeftBlue is true, and keep change status as long as bulbs[idx] == 1
- * 3. We accumulate allBlueCount as yellowCount == 0
+/* Time:O(n), Space:O(1).
+ * 1. Have a variable "rightestSeenBulb" to remember the seen bulb with the biggest index
+ * 2. Accumulate "allBlueCount" if all bulbs at left of rightestSeenBulb were turned on
  */
 
 import java.util.*;
 
 public class Solution {
     public int numTimesAllBlue(int[] light) {
-        int[] bulbs = new int[light.length]; //0:off, 1:yellow, 2:blue;
-        int yellowCount = 0;
         int allBlueCount = 0;
-        for(int onIndex: light){
-            onIndex--;
-            boolean isLeftBlue =(onIndex == 0)? true: (bulbs[onIndex - 1] == 2);
-            bulbs[onIndex] = 1;
-            yellowCount++;
-            int idx = onIndex;
-            while(isLeftBlue && idx < bulbs.length && bulbs[idx] == 1){
-                yellowCount--;
-                bulbs[idx] = 2;
-                idx++;
-                allBlueCount +=(yellowCount == 0)? 1: 0;
-            }
+        int rightestSeenBulb = 0;
+        for(int idx = 0; idx < light.length; ++idx){
+            rightestSeenBulb = Math.max(rightestSeenBulb, light[idx]);
+            allBlueCount +=(rightestSeenBulb == idx + 1)? 1: 0;
         }
         return allBlueCount;
     }
