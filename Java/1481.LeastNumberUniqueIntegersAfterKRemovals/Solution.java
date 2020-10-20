80d52ce38dd1989237401802ea2f403c8e19731c
Namo.Lu <chih-hung.lu@columbia.edu>
2020-10-19 22:27:53 -0700

    LeetCode#1481

---
+++ Java/1481.LeastNumberUniqueIntegersAfterKRemovals/Solution.java
@@ -0,0 +1,37 @@
+/* Greedy: Time:O(nlogn), Space:O(n)
+ * 1. Construct num2Count and sort the counts using increasing order
+ * 2. Subtract k with counts.get(i) until k <= 0
+ */     
+
+import java.util.*; // Stack
+
+public class Solution {
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        Map<Integer, Integer> num2Count = new HashMap<>();
+        for(int num: arr){
+            num2Count.put(num, num2Count.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> counts = new ArrayList<>(num2Count.values());
+        Collections.sort(counts);
+        System.out.println(counts);
+        for(int i = 0; i < counts.size(); ++i){
+            if(k == 0){
+                return counts.size() - i;
+            }else if(k < 0){
+                return counts.size() - i + 1;
+            }else{
+                k -= counts.get(i);
+            }
+        }
+        return (k >= 0)? 0: 1;
+    } 
+    
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        int[] arr = {5, 5, 4};
+        int k = 1;
+        System.out.println("arr:" + Arrays.toString(arr));
+        System.out.println("k:" + k);
+        System.out.println("least number of uniques:" + sol.findLeastNumOfUniqueInts(arr, k));
+    }
+}
